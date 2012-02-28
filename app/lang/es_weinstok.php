d2961bc860968dfe2af136f1ff6b6af8735f6d1c
smoers <smoers@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-02-28 13:30:42 +0000

    Subir cambios del trunk.
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@4797 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/lang/es_weinstok.php
+++ app/lang/es_weinstok.php
@@ -3,11 +3,12 @@
 	require_once dirname(__FILE__) . '/../conf.php';
 	require_once dirname(__FILE__) . '/es.php';
 	
-	$_LANG['ROL/RUT'] = 'CÉDULA';
-	$_LANG['RUT/NIT'] = 'CÉDULA';
-	$_LANG['RUT'] = 'CÉDULA';
+	$_LANG['ROL/RUT'] = 'Cédula';
+	$_LANG['RUT/NIT'] = 'Cédula';
+	$_LANG['RUT'] = 'Cédula';
 	$_LANG['Rut'] = 'Cédula';
 	$_LANG['RUT personal'] = 'Cédula';
 	$_LANG['RUT Proveedor'] = 'Cédula Proveedor';
-	
+	$_LANG['Hrs. Tarificadas'] = "Horas";
+	$_LANG['Total Trabajos'] = "Total";
 ?>
