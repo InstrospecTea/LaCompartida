a01f76fae32361b80aaf98a8c35f013951457cc7
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-03-28 10:57:55 +0000

    comenta la linea donde me enviaba mail
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@6211 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/interfaces/tarifas_xls.php
+++ app/interfaces/tarifas_xls.php
@@ -52,7 +52,7 @@
     else:
         $querytarifas="SELECT   id_tarifa, glosa_tarifa FROM tarifa where id_tarifa=".$id_tarifa_edicion;
     endif;
-    mail('ffigueroa@lemontech.cl','Querytarifa',$querytarifas);
+   // mail('ffigueroa@lemontech.cl','Querytarifa',$querytarifas);
     $resptarifas = mysql_query($querytarifas, $sesion->dbh) or Utiles::errorSQL($query_tarifas,__FILE__,__LINE__,$sesion->dbh);
     while($hojas=mysql_fetch_array( $resptarifas )):
 	
