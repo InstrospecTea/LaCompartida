e3533028c1223620bf0ad095d9fd3cf3afa1ba5e
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-04-11 14:00:48 +0000

    faltaba utiles app
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@6661 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/interfaces/cron_ingreso_datos_demo.php
+++ app/interfaces/cron_ingreso_datos_demo.php
@@ -4,6 +4,8 @@
 	require_once Conf::ServerDir().'/../fw/classes/Pagina.php';
 	require_once Conf::ServerDir().'/../fw/classes/Utiles.php';
 	require_once Conf::ServerDir().'/../app/classes/Debug.php';
+		require_once Conf::ServerDir() . '/../app/classes/UtilesApp.php';
+
 	require_once Conf::ServerDir().'/classes/Cobro.php';
 /*
 Valores necesarios:
