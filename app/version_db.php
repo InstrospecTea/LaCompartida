9bda7326a2d31fbe67fbf9a7a1086002ad87b99b
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-05-14 14:50:48 +0000

    informa version DB en pantalla de inicio
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@7877 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/version_db.php
+++ app/version_db.php
@@ -1,21 +1,27 @@
 <?php
 
-require_once dirname(__FILE__).'/../fw/classes/Sesion.php';
+require_once dirname(__FILE__) . '/../fw/classes/Sesion.php';
 
 $sesion = new Sesion(null, true);
 
-$resultado=mysql_query("select max(version) as version from version_db",$sesion->dbh);
+$resultado = mysql_query("SELECT MAX(version) AS version FROM version_db", $sesion->dbh);
 
-if(!$resultado) {
-    if(file_exists('version.php')) include_once('version.php');
+if (!$resultado) {
+	if (file_exists('version.php')) {
+		include_once('version.php');
+	}
 } else {
-    $valor=mysql_fetch_row($resultado);
-    
-    if (count($valor)==0) {
-    if(file_exists('version.php')) include_once('version.php');    
-    } else {
-    $VERSION = $valor[0] ; if( $_GET['show'] == 1 ) echo 'Ver. '.$VERSION; 
-    }
-} 
+	$valor = mysql_fetch_row($resultado);
 
-?>
+	if (count($valor) == 0) {
+		if (file_exists('version.php')) {
+			include_once('version.php');
+		}
+	} else {
+		$VERSION = $valor[0];
+		if ($_GET['show'] == 1) {
+			echo 'Ver. ' . $VERSION;
+		}
+	}
+}
+if(!defined('VERSIONDB')) define('VERSIONDB',$VERSION);
