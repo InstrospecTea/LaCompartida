dbc86035c0390fe180db394116a895a420e23caa
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-05-17 17:21:31 +0000

    ultimaobservacion
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@7984 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/classes/Observacion.php
+++ app/classes/Observacion.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require_once dirname(__FILE__).'/../conf.php';
 require_once Conf::ServerDir().'/../fw/classes/Lista.php';
 require_once Conf::ServerDir().'/../fw/classes/Objeto.php';
@@ -23,6 +23,18 @@ class Observacion extends Objeto
         $resp = mysql_query($query, $this->sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__,$this->sesion->dbh);
         return true;
 	}
+        function UltimaObservacion($id_cobro=0) {
+            if(!$id_cobro && !$this->Loaded())
+		{
+			$this->error = __("Debe cargar un historial");
+                        return false;
+		} else {
+                    if(!$id_cobro) $id_cobro=$this->fields['id_cobro'];
+                }
+               $query = "SELECT * FROM cobro_historial WHERE id_cobro = '".$id_cobro."' order by fecha DESC limit 0,1";
+        $resp = mysql_query($query, $this->sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__,$this->sesion->dbh);
+        return mysql_fetch_array($resp);   
+        }
 }
 
 class ListaObservaciones extends Lista
@@ -32,3 +44,5 @@ class ListaObservaciones extends Lista
         $this->Lista($sesion, 'Observacion', $params, $query);
     }
 }
+
+?>
