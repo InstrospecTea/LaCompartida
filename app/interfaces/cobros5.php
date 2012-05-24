316c20ea90a0b4c8182d46142f36a8c4668ad5e6
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-05-24 15:39:29 +0000

    mejora al historial y flujo de cobro para tag produccion
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@8073 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/interfaces/cobros5.php
+++ app/interfaces/cobros5.php
@@ -169,7 +169,7 @@
 		$cobro->Edit('codigo_idioma',$lang);
 		$cobro->Edit('se_esta_cobrando',$se_esta_cobrando);
                 $cobro->Edit("opc_ver_columna_cobrable",$opc_ver_columna_cobrable); 
-		$cobro->Write();//Se guarda porque despues se necesita para recalcular los datos del cobro
+		@$cobro->Write();//Se guarda porque despues se necesita para recalcular los datos del cobro
 		################### DESCUENTOS #####################
 		if($tipo_descuento == 'PORCENTAJE')
 		{
