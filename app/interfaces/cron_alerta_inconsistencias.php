d0b3d5728ff1da57bbcfbed1a1a2e1f8ba4d61e8
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-05-31 13:03:26 +0000

    alerta inconsistencias
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@8151 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/interfaces/cron_alerta_inconsistencias.php
+++ app/interfaces/cron_alerta_inconsistencias.php
@@ -45,7 +45,7 @@
 	$resp = mysql_query($query,$sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__,$sesion->dbh);
 	
 	$enviar = false;
-	$mensaje = "Se han observado inconistencias en los siguientes cobros del cliente ".Conf::dbUser().":<br/><br/>";
+	$mensaje = "Se han observado inconsistencias en los siguientes cobros del cliente ".Conf::dbUser().":<br/><br/>";
 	while( list($id_cobro,$monto_thh,$sumatoria) = mysql_fetch_array($resp) ) {
 		$mensaje .= "Cobro: $id_cobro   Monto_thh: $monto_thh   Sumatoria según horas: $sumatoria <br/>";
 		$enviar = true;
@@ -53,4 +53,67 @@
 	if( $enviar ) {
 		Utiles::Insertar($sesion, "Inconsistencia datos ".Conf::dbUser(), $mensaje, "ffigueroa@lemontech.cl,gtigre@lemontech.cl", "Soporte");
 	}
+	
+	// Revisiones de inconsistencías entre documentos de pago y facturas de pago
+	$querys=array();
+	$querys[]="drop table if exists netres;";
+ $querys[]="create table netres as 
+    select c.id_cobro , 
+    fc.id_documento iddoccobro,
+    ccfm_cobro.id_factura id_factura_cobro,
+    ccfm_cobro.monto_bruto cobrototal_factura,  
+    ccfm_pago.monto_bruto pagototal_factura,  
+
+     ccfm_pago.id_factura_pago  factura_pago, 
+    fp.monto as monto_pago_factura, fp.id_neteo_documento_adelanto, dp.id_documento as iddocpago
+
+    from cobro c  
+    LEFT JOIN factura_cobro fc on fc.id_cobro=c.id_cobro
+    LEFT JOIN cta_cte_fact_mvto AS ccfm_cobro ON ccfm_cobro.id_factura = fc.id_factura 
+    left JOIN cta_cte_fact_mvto_neteo AS ccfmn on ccfmn.id_mvto_deuda = ccfm_cobro.id_cta_cte_mvto
+    left JOIN cta_cte_fact_mvto AS ccfm_pago on  ccfmn.id_mvto_pago = ccfm_pago.id_cta_cte_mvto 
+    left join factura_pago fp ON fp.id_factura_pago = ccfm_pago.id_factura_pago 
+    left join documento dp on dp.id_factura_pago = fp.id_factura_pago and dp.tipo_doc!='N';";
+    
+
+
+$querys[]="ALTER TABLE  `netres` ADD INDEX (  `id_cobro` );";
+$querys[]="ALTER TABLE  `netres` ADD INDEX (  `iddoccobro` ); ";
+
+ $querys[]="drop table if exists netdos;";
+$querys[]="create table netdos as
+SELECT nd.id_neteo_documento, dc.monto monto_cobro,
+nd.id_documento_cobro iddoccobro,
+(nd.valor_cobro_honorarios + nd.valor_cobro_gastos) as cobrototal,
+(nd.valor_pago_honorarios+nd.valor_pago_gastos) as pagototal,
+nd.id_documento_pago iddocpago,
+-1*dp.monto as monto_pago,
+dc.id_cobro id_cobro, dp.id_factura_pago, if(dp.numero_doc='',dp.numero_operacion,dp.numero_doc) as numero_documento_pago
+FROM  cobro join documento dc on dc.id_cobro=cobro.id_cobro 
+right join neteo_documento nd on dc.id_documento=nd.id_documento_cobro and dc.tipo_doc='N'
+left join documento dp ON dp.id_documento=nd.id_documento_pago and dp.tipo_doc!='N';";
+
+
+ 
+$querys[]="ALTER TABLE  `netdos` ADD INDEX (  `id_cobro` );";
+$querys[]="ALTER TABLE  `netdos` ADD INDEX (  `iddoccobro` );";
+	foreach ($querys as $q) mysql_query($q, $sesion->dbh);
+$query ="select netdos.iddoccobro, netdos.iddocpago, netdos.id_cobro from netdos left join netres on netdos.id_cobro=netres.id_cobro
+and netdos.iddoccobro=netres.iddoccobro where netres.id_cobro is null;";
+	$resp = mysql_query($query,$sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__,$sesion->dbh);
+	
+	$enviar = false;
+	$mensaje = "Se han observado inconistencias en los siguientes documentos  de pago del cliente ".Conf::dbUser().":<br/><br/>";
+	while( list($iddoccobro, $iddocpago, $id_cobro) = mysql_fetch_array($resp) ) {
+		$mensaje .= "Documento Cobro: $iddoccobro Cobro: $id_cobro  Documento Pago: $iddocpago <br/>";
+		$enviar = true;
+	}
+	if( $enviar ) {
+		Utiles::Insertar($sesion, "Inconsistencia datos ".Conf::dbUser(), $mensaje, "ffigueroa@lemontech.cl,gtigre@lemontech.cl", "Soporte");
+	}
+	
+	
+	
+	
+	
 ?>
