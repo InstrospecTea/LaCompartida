dbed44cce71b022e842437aa8c1e73c6bf02d3b6
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-06-08 22:29:53 +0000

    modrnizr
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@8289 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/templates/default/headers_nuevo.php
+++ app/templates/default/headers_nuevo.php
@@ -16,7 +16,8 @@ define('HEADERLOADED',1);
 <head>
    
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
 	<script language="JavaScript" type="text/javascript">
         var _sf_startpt=(new Date()).getTime();
         var DivLoading='<div class="divloading">&nbsp;</div>';
@@ -34,6 +35,8 @@ define('HEADERLOADED',1);
 	<title><?php echo Conf::AppName()?> - <?php echo  $this->titulo ?></title>
 	<!-- <?php echo Conf::TimestampDeployCSS()?> -->
 	<link rel="stylesheet" type="text/css" href="https://estaticos.thetimebilling.com/templates/default/css/deploy/all.1226330411_nuevo.css" />
+	
+	
 	<!--[if IE]>
 	<link rel="stylesheet" type="text/css" href="https://estaticos.thetimebilling.com/templates/default/css/css_ie_only.css" />
 	<![endif]-->
@@ -44,8 +47,10 @@ define('HEADERLOADED',1);
         <link rel="shortcut icon" href="https://estaticos.thetimebilling.com/favicon.ico" />
 <link rel="stylesheet" type="text/css" href="https://static.thetimebilling.com/jquery-ui.css" />
 	
+
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
- 	
+ 		<script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.0.6-development-only.js"></script>
+
         <script language="JavaScript" type="text/javascript" src="https://estaticos.thetimebilling.com/templates/default/css/deploy/all.1234370043.js"></script>
         <script language="JavaScript" type="text/javascript" src="https://estaticos.thetimebilling.com/templates/default/css/deploy/resize_iframe.js"></script>
 		<script language="JavaScript" type="text/javascript" src="https://estaticos.thetimebilling.com/jshashtable-2.1.js"></script>
@@ -60,7 +65,6 @@ define('HEADERLOADED',1);
 .non_popup {background: url(https://estaticos.thetimebilling.com/templates/default/img/fondo_degradado2.gif) repeat-x;}
 .campoactivo {cursor:pointer;border:1px solid #EEE;width:200px;}
 .alignleft {text-align:left;font-size:10px;}
-
 #zenbox_tab {overflow: hidden; border: 0 none !important;}
 #mainttb {background:white;padding: 30px 0 5px ;width: 960px;height: 100%;margin: -10px auto 10px; border:0 none;border-top:5px #42A62B;}
 .titulo_sec {padding:0 30px 5px; height:35px;background-color: #FFFFFF;text-align:left;font-size: 14px;    font-weight: bold;}
@@ -75,6 +79,22 @@ define('HEADERLOADED',1);
 *:first-child+html .clearfix { zoom: 1; } /* IE7 */
 .vpx {display:block;height:5px;content:' ';}
 .loadingbar {color:transparent;background: url('https://static.thetimebilling.com/images/loading_bar.gif') 0 0; -webkit-appearance: none;}
+.droplinetabs a {margin-bottom:-3px !important;font-size:11px !important;} 
+.droplinetabs ul > li {display:inline-block !important;margin-left:-3px;}
+.droplinetabs ul li a {display:inline-block !important;text-decoration:none;}
+.barra_fija, #fd_menu_grey {width:960px;border-bottom-left-radius:0;border-bottom-right-radius:0;height:28px !important;overflow:hidden;}
+.droplinetabs UL LI UL A:hover {padding-bottom:7px !important;display:block;background:#119011 !important;text-decoration:none !important;}
+.droplinetabs UL LI UL {margin-left:4px;}
+.droplinetabs ul li ul li a {padding-right:11px !important;padding-left:8px !important}
+.mini_input {height: 12px; margin: -1px 0 0 -1px;font-size:10px;}
+.updown {position: relative;margin: -2px 0 0 0px;left: 260px;top: -20px;}
+ #colleft { width:950px;}
+ #cajafacturas  { width:950px;margin-left:-3px;}
+#cajafacturas td {font-size:8pt !important; border-collapse:collapse;border:1px solid #F9F9F9;}
+#cobro6colderecha {    float:left;margin: 5px -10px 0 35px;width:230px;;background-color: white;}
+#colmask {width:955px;overflow:hidden;clear:both;margin:auto;}
+#historial {margin:5px 0 0 -10px;width:700px;float:left;}
+
 </style>
 
 
