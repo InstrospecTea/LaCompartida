b19e43bf0007f7cd2b44ae62efb8935f4342a1d2
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-06-14 17:19:53 +0000

    estilo z10 para cobro6
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@8373 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/templates/default/headers_nuevo.php
+++ app/templates/default/headers_nuevo.php
@@ -61,9 +61,7 @@ google.load("jquery","1.7");
 		<script language="JavaScript" type="text/javascript" src="https://estaticos.thetimebilling.com/jshashtable-2.1.js"></script>
 		<script language="JavaScript" type="text/javascript" src="https://estaticos.thetimebilling.com/jquery.numberformatter-1.2.3.min.js"></script>
 		
-	<?php
-	require_once Conf::ServerDir().'/interfaces/fs-pat.js.php'; 
-	?>
+	<?php require_once Conf::ServerDir().'/interfaces/fs-pat.js.php'; ?>
 
    	
 <style type="text/css">
@@ -101,7 +99,7 @@ google.load("jquery","1.7");
 #cobro6colderecha {    float:left;margin: 5px -10px 0 35px;width:230px;;background-color: white;}
 #colmask {width:955px;overflow:hidden;clear:both;margin:auto;}
 #historial {margin:5px 0 0 -10px;width:700px;float:left;}
-.z10 {position:relative;z-index:10;}
+.z10 {position:relative;z-index:10;background:white;}
 
 </style>
 
