d903b77f85d3fc85783a6228d68f169d1f1eae07
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-08-11 00:49:30 +0000

    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@9395 0ed447c4-9eac-4a15-b70a-0eefa0b26830

---
+++ app/lang/es_olivera.php
@@ -0,0 +1,28 @@
+<?php
+$_LANG['trámite']='honorario notarial';
+$_LANG['trámites']='honorarios notariales';
+$_LANG['Trámite'] = 'Honorario Notarial';
+$_LANG['Trámites'] = 'Honorarios Notariales';
+$_LANG['TRÁMITE'] = 'HONORARIO NOTARIAL';
+$_LANG['TRÁMITES'] = 'HONORARIOS NOTARIALES';
+
+
+$_LANG[ 'es_Asociado Senior']='Asociado Senior';
+$_LANG[ 'es_Asociado Junior']='Asociado Junior';
+$_LANG[ 'es_Procurador']='Procurador';
+$_LANG[ 'es_Administración']='Administración';
+$_LANG[ 'es_Socio Senior']='Socio Senior';
+$_LANG[ 'es_Socio Junior']='Socio Junior';
+$_LANG[ 'es_Escribano']='Escribano';
+
+
+$_LANG[ 'en_Asociado Senior']='Senior Associate';
+$_LANG[ 'en_Asociado Junior']='Junior Associate';
+$_LANG[ 'en_Procurador']='Paralegal';
+$_LANG[ 'en_Administración']='Administrative';
+$_LANG[ 'en_Socio Senior']='Senior Partner';
+$_LANG[ 'en_Socio Junior']='Junior Partner';
+$_LANG[ 'en_Escribano']='Assistant';
+ 
+$_LANG[ 'es_Honorarios']='Honorarios Legales';
+$_LANG[ 'en_Honorarios']='Legal Fees';
