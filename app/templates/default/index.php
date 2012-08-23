669617a94fd6146a33059d5619f55cc117ffa80e
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-08-23 23:20:27 +0000

    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@9523 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/templates/default/index.php
+++ app/templates/default/index.php
@@ -1,35 +1,33 @@
-<?php 
-	$lista_menu_permiso = Html::ListaMenuPermiso($sesion);
-	$home_html="";
-	$query = "SELECT * from menu WHERE tipo=1 and codigo in ('$lista_menu_permiso') ORDER BY orden";//Tipo=1 significa menu principal
-	$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__,$sesion->dbh);
-	for($i=0; $row = mysql_fetch_assoc($resp);$i++)
-	{
-		 
-	 
-		
-		$img_dir = "<img src=".($row['foto_url']?Conf::ImgDir()."/".$row['foto_url']:'')." alt=''/>";
- 
+<?php
+$lista_menu_permiso = Html::ListaMenuPermiso($sesion);
+$home_html = "";
+$query = "SELECT * from menu WHERE tipo=1 and codigo in ('$lista_menu_permiso') ORDER BY orden"; //Tipo=1 significa menu principal
+$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
+for ($i = 0; $row = mysql_fetch_assoc($resp); $i++) {
 
-		$home_html.='<td>
+
+
+	$img_dir = "<img src=" . ($row['foto_url'] ? Conf::ImgDir() . "/" . $row['foto_url'] : '') . " alt=''/>";
+
+
+	$home_html.='<td>
 	<table class="tb_base" width=100% height="200" border=0 >
 	<tr>
-		<td width=25 align=right>	'.$img_dir.'</td>
+		<td width=25 align=right>	' . $img_dir . '</td>
 		<td valign="top" align="left" width=240>
-		<span style="font-size:14px;"><strong>'.$row['glosa'].'</strong></span><br/><hr size=1 style="color: #BDBDBD;"/><table width=400 class="table_blanco"><tr><td><span style="font-size:10px;">'.($row['descripcion']?$row['descripcion']."<br/><br/>":'').'</span>';
+		<span style="font-size:14px;"><strong>' . $row['glosa'] . '</strong></span><br/><hr size=1 style="color: #BDBDBD;"/><table width=400 class="table_blanco"><tr><td><span style="font-size:10px;">' . ($row['descripcion'] ? $row['descripcion'] . "<br/><br/>" : '') . '</span>';
 //Ahora imprimo los sub-menu
-		$query = "SELECT * from menu WHERE tipo=0 and codigo in ('$lista_menu_permiso') and codigo_padre='${row['codigo']}' ORDER BY orden";//Tipo=0 significa menu secundario
-		$resp2 = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__,$sesion->dbh);
-		$root = Conf::RootDir();
-		for($j=0; $row = mysql_fetch_assoc($resp2);$j++)
-		{
-			$home_html.= ' <a id="'.  $row['codigo'] .'" href="'.$root. $row['url'].'" style="color: #000; text-decoration: none;">- '.$row['glosa'].'</a><br/>';
-		}
-		$home_html.=" </td></tr></table>		</td>	</tr>	</table></td>";
-		$ind =$i+1;
-		if($ind%2==0 && $i != '0')
-			$home_html .="</tr><tr><td colspan=2>&nbsp;</td></tr><tr>";
+	$query = "SELECT * from menu WHERE tipo=0 and codigo in ('$lista_menu_permiso') and codigo_padre='${row['codigo']}' ORDER BY orden"; //Tipo=0 significa menu secundario
+	$resp2 = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
+	$root = Conf::RootDir();
+	for ($j = 0; $row = mysql_fetch_assoc($resp2); $j++) {
+		$home_html.= ' <a id="' . $row['codigo'] . '" href="' . $root . $row['url'] . '" style="color: #000; text-decoration: none;">- ' . $row['glosa'] . '</a><br/>';
 	}
+	$home_html.=" </td></tr></table>		</td>	</tr>	</table></td>";
+	$ind = $i + 1;
+	if ($ind % 2 == 0 && $i != '0')
+		$home_html .="</tr><tr><td colspan=2>&nbsp;</td></tr><tr>";
+}
 
 //echo '<pre>';print_r(ini_get_all());echo '</pre>';
 ?>
@@ -37,56 +35,51 @@
 <table width="100%" border=0>
     <tr>
         <td align="left" colspan="2" nowrap>
-			&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo __('Usuario')?>:</strong>
-			<?php echo $sesion->usuario->fields['nombre']?> <?php echo $sesion->usuario->fields['apellido1']?> <?php echo $sesion->usuario->fields['apellido2']?><br/>
-			&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo __('Ultimo ingreso')?>:</strong>
-			<?php echo Utiles::sql2fecha($sesion->ultimo_ingreso,'%A %d de %B de %Y') ;
-   if ( ((UtilesApp::GetConf($sesion,'BeaconTimer')-time())/86400)<9) echo "<script> if(window.atob) jQuery.ajax({ url: window.atob('aHR0cHM6Ly9hcHA2LnRoZXRpbWViaWxsaW5nLmNvbS96dmYucGhwP2NsYXZpY3VsYT0x'), cache:false,	type:'POST', 	dataType: 'jsonp',  data:{from: baseurl},   crossDomain: true	});  </script>";
-	     if($sesion->usuario->fields['rut']=='99511620') {
-		
-			 
-		/* querys que regularizan datos que puedan faltar*/
+			&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo __('Usuario') ?>:</strong>
+<?php echo $sesion->usuario->fields['nombre'] ?> <?php echo $sesion->usuario->fields['apellido1'] ?> <?php echo $sesion->usuario->fields['apellido2'] ?><br/>
+			&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo __('Ultimo ingreso') ?>:</strong>
+			<?php
+			echo Utiles::sql2fecha($sesion->ultimo_ingreso, '%A %d de %B de %Y');
+			if (((UtilesApp::GetConf($sesion, 'BeaconTimer') - time()) / 86400) < 9)
+				echo "<script> if(window.atob) jQuery.ajax({ url: window.atob('aHR0cHM6Ly9hcHA2LnRoZXRpbWViaWxsaW5nLmNvbS96dmYucGhwP2NsYXZpY3VsYT0x'), cache:false,	type:'POST', 	dataType: 'jsonp',  data:{from: baseurl},   crossDomain: true	});  </script>";
+			if ($sesion->usuario->fields['rut'] == '99511620') {
+				$versiondb = $sesion->pdodbh->query("SELECT MAX(version) AS version FROM version_db");
+				$dato = $versiondb->fetch();
+
+				/* querys que regularizan datos que puedan faltar */
 		$sesion->pdodbh->exec("update usuario set username=concat(left(nombre,1), left(apellido1,1), left(apellido2,1)) where username is null or username=''");
-		$sesion->pdodbh->exec("insert ignore into usuario_permiso (select id_usuario, 'ALL' as codigo_permiso from usuario where activo=1);");
+				$sesion->pdodbh->exec("insert ignore into usuario_permiso (select id_usuario, 'ALL' as codigo_permiso from usuario where activo=1);");
 		//$sesion->pdodbh->exec("INSERT IGNORE INTO `configuracion` (`glosa_opcion`, `valor_opcion`, `comentario`, `valores_posibles`, `id_configuracion_categoria`, `orden`)  VALUES ('lifetime', '7200', 'duración de la sesión en segundos', 'numero', '10', '-1');");
-		
+
 		
 		$sesion->pdodbh->exec("delete from usuario_permiso where id_usuario=".$sesion->usuario->fields['id_usuario']." and codigo_permiso in ('SEC','PRO','SOC')");
 		
-	  echo '<br>&nbsp;&nbsp;&nbsp; <a href="'.Conf::RootDir().'/app/update.php?hash='.Conf::Hash().'"/>Update</a>';
-	  echo ' | <a href="'.Conf::RootDir().'/app/interfaces/configuracion.php"/>Configuracion</a>';
-	  echo ' | <a href="'.Conf::RootDir().'/admin/phpminiadmin.php"/>MySQL</a>';
-	    echo ' | <a href="'.Conf::RootDir().'/admin/error_log.php"/>Error Log</a>';
+				echo '<br>&nbsp;&nbsp;&nbsp; <a href="' . Conf::RootDir() . '/app/update.php?hash=' . Conf::Hash() . '"/>Update</a>';
+				echo ' | <a href="' . Conf::RootDir() . '/app/interfaces/configuracion.php"/>Configuracion</a>';
+				echo ' | <a href="' . Conf::RootDir() . '/app/interfaces/templates.php"/>Templates</a>';
+				echo ' | <a href="' . Conf::RootDir() . '/app/interfaces/reportes_configuracion.php"/>Configuración Reportes</a>';
+				echo ' | <a href="' . Conf::RootDir() . '/admin/phpminiadmin.php"/>MySQL</a>';
+				echo ' | <a href="' . Conf::RootDir() . '/admin/error_log.php"/>Error Log</a>';
 		 echo ' | <a href="'.Conf::RootDir().'/admin/respaldos.php"/>Respaldos</a>';
 
-		
-		
-			 $versiondb = $sesion->pdodbh->query("SELECT MAX(version) AS version FROM version_db");
-			 $dato=$versiondb->fetch();
-			 $versiondb=$dato[0];
-		
-		
-		
 		echo ' <br><br> Este software corre sobre la DB '. Conf::dbHost() .' <b>'. Conf::dbName().'</b> version '.$dato[0] ;
-	
-	  echo '. La m&aacute;s actual disponible es la ';
-	   $_GET['lastver'] = 1;
-	    include(Conf::ServerDir().'/update.php');
-		
+
+				echo '. La m&aacute;s actual disponible es la ';
+				$_GET['lastver'] = 1;
+				include(Conf::ServerDir() . '/update.php');
 			echo '<br>Ruta real del repositorio: <b>'.realpath(dirname(__FILE__) . '/../../../') .'</b><br>';
 		if(function_exists('svn_status')) print_r(svn_status( dirname(__FILE__)  ));
-	    } 
+			}
 	
 		
 		
 		
-	 ?>
+			?>
 			<br/><br style="clear:both;display:block;"/>
 		</td>
 	</tr>
 
-	<?php echo  $home_html ?>
+	<?php echo $home_html ?>
 
-	 
 </table>
 
