32929b156cb9b377d17ea26ea682662e48533653
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-08-27 22:19:30 +0000

    corrige error de merge
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@9541 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/templates/default/bottom_nuevo.php
+++ app/templates/default/bottom_nuevo.php
@@ -29,11 +29,11 @@ $Slim=Slim::getInstance('default',true);
     });
     
  
-jQuery.ajax({async: false,cache:false,type: "GET", url:'https://static.thetimebilling.com/bottom.js?v2', dataType: 'script' });
+jQuery.ajax({async: false,cache:false,type: "GET", url:'https://static.thetimebilling.com/js/bottom.js', dataType: 'script' });
 
  
 
- function downloadJSAtOnload() {
+ 
    
 var color=['#B4D3B5','#E7EABB','#FFCAD8','#FFD2C4','#FFE9A6','#A4BE81','#BEFFA8','#A8FFBE','#E0E0E0','#BEF','#DD9','#A4BBFF','#CDA9FE','#FDDC9F','#BABC5C','#CCF','#EAADAA','#AD9F69','#CABCE0','#EB9C63','#45989C','#C0ACD7','#AFCC91','#FFB0B0','#BDAD75','#E8E471','#DC7E7E','#BDD76F','#A0A5D6','#AACCAC','#A4BBFF','#ECD7CA','#D5C9AC','#EDBFA9','#FFEBAE','#8E9C43','#68BB77','#85B188','#DAB6B6','#429D99','#FC9','#E7CEFF','#69C','#FFEBAE','#D2009E','#D9C6FF','#3DAB9A','#AB6573','#C1CDBC','#99CC66','#9999CC','#99CC99','#CCFFCC','#CCFF99','#FFFF66','#F2F587','#55A6AA','#75808A','#C7826D','#C6DEAD','#C6DEDA','#AEAE00','#3F7C7C','#FFCCF2','#92CB45','#8D8E82','#BDEE73','#CCC','#996','#CC6','#699','#F2F2F2','#E9BBBA','#8C4646','#C8EEFD','#C5DEFA','#D1D8D7','#FF99FF','#FC9','#3C7B91','#5EA6BD','#FEEEBC','#CAC793','#FEB1B4','#CC6633','#CCC','#66CC66','#66FF66'];
 function s2c(s){
