58475cd080ec85746b132efa5cb66b19fc546b1e
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-08-28 22:20:44 +0000

    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@9560 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/templates/default/bottom_nuevo.php
+++ app/templates/default/bottom_nuevo.php
@@ -1,110 +1,55 @@
 
+        <script type="text/javascript">window.jQuery || document.write('<script src="//static.thetimebilling.com/js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
+        <script type="text/javascript" src="//static.thetimebilling.com/js/plugins.js"></script>
+        <script type="text/javascript" src="//static.thetimebilling.com/js/main.js"></script>
+		
+		
+        
 <div style="clear:both;display:block;">&nbsp;</div>
 
-    <div id="ultimocontenedor" style="clear:both;height:70px; width:130px;margin:40px auto 5px ;text-align:center;">
-            <img src="<?php echo Conf::ImgDir()?>/logo_bottom.jpg" width="125" height="37" style="padding:15px 15px 0;float:left;" />&nbsp;
-    <div id="DigiCertClickID_iIR9fwBQ" style="float:right;" >&nbsp;</div>
-    </div>
 
-    
-</div>
-<div id="dialogomodal" style="display:none;text-align:center" > </div> 
-<div id="dialog-confirm" style="display:none;" ></div>  
-<div id="lttooltip"></div>
+<?php  
+$Slim=Slim::getInstance('default',true);
+	if($popup==true) {
+		$Slim->applyHook('hook_footer_popup');
+	} else {
+		$Slim->applyHook('hook_footer');
+		echo '<script type="text/javascript" src="//static.thetimebilling.com/js/footer.js"></script>';
+		echo '<div id="ultimocontenedor" style="clear:both;height:70px; width:130px;margin:40px auto 5px ;text-align:center;">
+				<img src="//static.thetimebilling.com/images/logo_bottom.jpg" width="125" height="37" style="padding:15px 15px 0;float:left;" />&nbsp;
+			<div id="DigiCertClickID_iIR9fwBQ" style="float:right;" >&nbsp;</div>
+			</div>';
+	}
+ ?>	
 <script type="text/javascript">
-/* <![CDATA[   */
-    jQuery.ajax({async: true,cache:true, type: "GET", url: 'https://static.thetimebilling.com/fw/js/droplinemenu.js'	, 
-	dataType: 'script',
+
+    jQuery.ajax({async: true,cache:true, type: "GET", url: "//static.thetimebilling.com/js/droplinemenu.js"	,
+	dataType: "script",
 	complete: function() {
         droplinemenu.buildmenu("droplinetabs1");
 	}
     });
-    
+jQuery.ajax({async: false,cache:true,type: "GET", url:"//static.thetimebilling.com/js/bottom.js", dataType: "script" });
+
  
-jQuery.ajax({async: false,cache:false,type: "GET", url:'https://static.thetimebilling.com/bottom.js?v2', dataType: 'script' });
 
-<?php   $Slim=Slim::getInstance('default',true);  
-$Slim->applyHook('hook_cobro_factura_pago'); ?>
-
- function downloadJSAtOnload() {
    
-   
-    jQuery("#zenboxcss").attr({rel:  "stylesheet", type: "text/css", href: "https://assets.zendesk.com/external/zenbox/v2.4/zenbox.css" });
-     
-
-
-    
-  if (_sf_async_config.pathseguro!==undefined) {
-         if (jQuery('#DigiCertClickID_iIR9fwBQ').length>0) {
-                jQuery('#ultimocontenedor').css({'width':'335px'});
-                if ("https:" == document.location.protocol) {
-                   __dcid.push(["DigiCertClickID_iIR9fwBQ", "3", "s", "black", "iIR9fwBQ"]);(function(){var cid=document.createElement("script");cid.type="text/javascript";cid.async=true;cid.src=("https:" === document.location.protocol ? "https://" : "http://")+"seal.digicert.com/seals/cascade/seal.min.js";var s = document.getElementsByTagName("script");var ls = s[(s.length - 1)];ls.parentNode.insertBefore(cid, ls.nextSibling);}());
-                } else {
-                         jQuery('#DigiCertClickID_iIR9fwBQ').html('<a style="border:0;text-decoration:none;" href="'+_sf_async_config.pathseguro+'"><img src="https://static.thetimebilling.com/images/no_ssl_cifrado.png" style="text-decoration:none;vertical-align:top;border: 0 none;margin-top:0;position:relative;top:0;right:0;" /></a>');
-                }
-            }
-    }
-    if (!Modernizr.borderradius) {
-    jQuery.ajax({async: false,cache:true, type: "GET", url: 'https://static.thetimebilling.com/fw/js/curvycorners.js', 
-	dataType: 'script',
-	complete: function() {
-		  var settings = {
-		      tl: { radius: 5 },
-		      tr: { radius: 5 },
-		      bl: { radius: 5 },
-		      br: { radius: 5 },
-		      antiAlias: true
-		    }
-            var divObj = document.getElementById("fd_menu_grey");
-            curvyCorners(settings, divObj); 
-	    }
-        });            
-	  }
-
-   jQuery("#zenboxcss").load(function() {
-			jQuery.ajax({async: true,cache:true, type: "GET", url: 'https://assets.zendesk.com/external/zenbox/v2.4/zenbox.js', 
-		dataType: 'script'})
-		.fail(function() {
-		jQuery.ajax({async: false,cache:true, type: "GET", url: 'https:/static.thetimebilling.com/zenbox.js'});  
-		}).always(function() {
-
-			if (typeof(window.Zenbox) !== "undefined") {
-				Zenbox.init({
-					dropboxID:   "20042787",
-					url:         "http://soporte.thetimebilling.com",
-					tabID:       "support", 
-					tabImageURL: "https://static.thetimebilling.com/templates/default/img/tag_soporte3.png",
-					tabColor:    "transparent",
-					tabPosition: "Right"
-					});
-				}          		
-				
-
-		});
-	});
- }
-
-if (window.addEventListener)
- window.addEventListener("load", downloadJSAtOnload, false);
- else if (window.attachEvent)
- window.attachEvent("onload", downloadJSAtOnload);
- else window.onload = downloadJSAtOnload;
-        
-/* ]]> */
+var color=['#B4D3B5','#E7EABB','#FFCAD8','#FFD2C4','#FFE9A6','#A4BE81','#BEFFA8','#A8FFBE','#E0E0E0','#BEF','#DD9','#A4BBFF','#CDA9FE','#FDDC9F','#BABC5C','#CCF','#EAADAA','#AD9F69','#CABCE0','#EB9C63','#45989C','#C0ACD7','#AFCC91','#FFB0B0','#BDAD75','#E8E471','#DC7E7E','#BDD76F','#A0A5D6','#AACCAC','#A4BBFF','#ECD7CA','#D5C9AC','#EDBFA9','#FFEBAE','#8E9C43','#68BB77','#85B188','#DAB6B6','#429D99','#FC9','#E7CEFF','#69C','#FFEBAE','#D2009E','#D9C6FF','#3DAB9A','#AB6573','#C1CDBC','#99CC66','#9999CC','#99CC99','#CCFFCC','#CCFF99','#FFFF66','#F2F587','#55A6AA','#75808A','#C7826D','#C6DEAD','#C6DEDA','#AEAE00','#3F7C7C','#FFCCF2','#92CB45','#8D8E82','#BDEE73','#CCC','#996','#CC6','#699','#F2F2F2','#E9BBBA','#8C4646','#C8EEFD','#C5DEFA','#D1D8D7','#FF99FF','#FC9','#3C7B91','#5EA6BD','#FEEEBC','#CAC793','#FEB1B4','#CC6633','#CCC','#66CC66','#66FF66'];
+function s2c(s){
+		if(!s) return;
+		
+		var c = 0;
+		var max = 90;
+		for(var i=0; i<s.length; i++){
+		 c=(c+s.charCodeAt(i))%max;
+		}
+		return color[c];
+		}
+		
 </script>
-
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-21661196-2']);
-_gaq.push(['_setDomainName', 'none']);
-_gaq.push(['_setAllowLinker', true]);
-_gaq.push(['_trackPageview']);
-
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-</script>
+</div>
+<div id="dialogomodal" style="display:none;text-align:center" > </div>
+<div id="dialog-confirm" style="display:none;" ></div>
+<div id="lttooltip"></div>
 </body>
 </html>
