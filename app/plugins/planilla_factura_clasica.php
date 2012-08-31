b0846008c1ca938869ce77e63528358db9284f52
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-08-31 17:33:41 +0000

    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@9597 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/plugins/planilla_factura_clasica.php
+++ app/plugins/planilla_factura_clasica.php
@@ -6,7 +6,7 @@ $Slim->hook('hook_factura_inicio', 'Descarga_Planilla_Clasica');
 $Slim->hook('hook_factura_fin', 'Ofrece_Planilla_Clasica');
 
 function Ofrece_Planilla_Clasica() {
-	 echo '<input type="button" value="'. __('Descargar Excel') .' Formato Antiguo" class="btn" name="boton_excel" onclick="BuscarFacturas(this.form, \'exportar_excel\')">';
+	// echo '<input type="button" value="'. __('Descargar Excel') .' Formato Antiguo" class="btn" name="boton_excel" onclick="BuscarFacturas(this.form, \'exportar_excel\')">';
 }
 
 function Descarga_Planilla_Clasica() {
