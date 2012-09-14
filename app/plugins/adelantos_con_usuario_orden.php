c5dc360bcd373d9a7dd1011d01e7684e43de8a5c
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-09-14 19:58:42 +0000

    version definitiva plugin adelantos con usuario orden
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@9708 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/plugins/adelantos_con_usuario_orden.php
+++ app/plugins/adelantos_con_usuario_orden.php
@@ -9,12 +9,7 @@ $Slim->hook('hook_guardar_documento_pago', 'Guardar_Adelantos_Con_Usuario_Orden'
 function Ingresar_Adelantos_Con_Usuario_Orden() {
 	global $documento, $sesion, $adelanto,$id_usuario_orden, $id_usuario_ingresa;
 
-		if (!array_key_exists('id_usuario_ingresa',$documento->fields)) {
-		$sesion->pdodbh->exec("ALTER TABLE `documento` ADD  `id_usuario_ingresa` INT( 11 ) NULL DEFAULT NULL AFTER `id_documento`; ALTER TABLE `documento` ADD CONSTRAINT  FOREIGN KEY (`id_usuario_ingresa`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE;");
-	}
-	if (!array_key_exists('id_usuario_orden',$documento->fields)) {
-		$sesion->pdodbh->exec("ALTER TABLE `documento` ADD `id_usuario_orden` INT( 11 ) NULL DEFAULT NULL AFTER `id_usuario_ingresa` ; ALTER TABLE `documento` ADD CONSTRAINT   FOREIGN KEY (`id_usuario_orden`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE;");
-	}
+
 	if (!empty($id_usuario_ingresa)) $documento->Edit('id_usuario_ingresa', $id_usuario_ingresa);
 	if(!empty($id_usuario_orden)) $documento->Edit('id_usuario_orden', $id_usuario_orden);
 
@@ -35,13 +30,17 @@ function Guardar_Adelantos_Con_Usuario_Orden() {
  global $sesion, $documento, $id_usuario_orden, $id_usuario_ingresa;
 	
 
-	if (!array_key_exists('id_usuario_ingresa',$documento->fields)) {
+	if (array_key_exists('id_usuario_ingresa',$documento->fields)) {
+		if (!empty($id_usuario_ingresa)) $documento->Edit('id_usuario_ingresa', $id_usuario_ingresa);
+	} else {
 		$sesion->pdodbh->exec("ALTER TABLE `documento` ADD  `id_usuario_ingresa` INT( 11 ) NULL DEFAULT NULL AFTER `id_documento`; ALTER TABLE `documento` ADD CONSTRAINT  FOREIGN KEY (`id_usuario_ingresa`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE;");
 	}
-	if (!array_key_exists('id_usuario_orden',$documento->fields)) {
+	if (array_key_exists('id_usuario_orden',$documento->fields)) {
+		if(!empty($id_usuario_orden)) $documento->Edit('id_usuario_orden', $id_usuario_orden);
+	} else {
 		$sesion->pdodbh->exec("ALTER TABLE `documento` ADD `id_usuario_orden` INT( 11 ) NULL DEFAULT NULL AFTER `id_usuario_ingresa` ; ALTER TABLE `documento` ADD CONSTRAINT   FOREIGN KEY (`id_usuario_orden`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE;");
 	}
-	if (!empty($id_usuario_ingresa)) $documento->Edit('id_usuario_ingresa', $id_usuario_ingresa);
-	if(!empty($id_usuario_orden)) $documento->Edit('id_usuario_orden', $id_usuario_orden);
+	
+	
  
 }
