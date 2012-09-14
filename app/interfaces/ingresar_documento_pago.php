cab15613b8189c494bb91139f63420a106733017
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-09-14 20:07:45 +0000

    version definitiva plugin adelantos con usuario orden
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@9709 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/interfaces/ingresar_documento_pago.php
+++ app/interfaces/ingresar_documento_pago.php
@@ -85,12 +85,13 @@ if ($id_documento) {
 	if ($id_cobro) {
 		$monto_usado = $documento->MontoUsadoAdelanto($id_cobro);
 	}
-}
-	global $documento, $sesion, $id_usuario_ingresa,$id_usuario_orden;
+	 
 	
 	($Slim=Slim::getInstance('default',true)) ? $Slim->applyHook('hook_guardar_documento_pago') : false; 
+}
+	
 
-	 $documento->Write();
+	 
 
 if (UtilesApp::GetConf($sesion, 'CodigoSecundario') && $codigo_cliente_secundario != '') {
 	$cliente = new Cliente($sesion);
