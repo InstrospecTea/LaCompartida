5a2789d5bc7ef4e76de416cfcada6ec9e4e156db
ffigueroa <ffigueroa@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-09-14 19:41:39 +0000

    version definitiva plugin adelantos con usuario orden
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@9706 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/plugins/adelantos_con_usuario_orden.php
+++ app/plugins/adelantos_con_usuario_orden.php
@@ -9,8 +9,14 @@ $Slim->hook('hook_guardar_documento_pago', 'Guardar_Adelantos_Con_Usuario_Orden'
 function Ingresar_Adelantos_Con_Usuario_Orden() {
 	global $documento, $sesion, $adelanto,$id_usuario_orden, $id_usuario_ingresa;
 
-	if (array_key_exists('id_usuario_orden',$documento->fields) && !empty($id_usuario_orden)) $documento->Edit('id_usuario_orden', $id_usuario_orden);
-	if (array_key_exists('id_usuario_ingresa',$documento->fields) && !empty($id_usuario_ingresa)) $documento->Edit('id_usuario_ingresa', $id_usuario_ingresa);
+		if (!array_key_exists('id_usuario_ingresa',$documento->fields)) {
+		$sesion->pdodbh->exec("ALTER TABLE `documento` ADD  `id_usuario_ingresa` INT( 11 ) NULL DEFAULT NULL AFTER `id_documento`; ALTER TABLE `documento` ADD CONSTRAINT  FOREIGN KEY (`id_usuario_ingresa`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE;");
+	}
+	if (!array_key_exists('id_usuario_orden',$documento->fields)) {
+		$sesion->pdodbh->exec("ALTER TABLE `documento` ADD `id_usuario_orden` INT( 11 ) NULL DEFAULT NULL AFTER `id_usuario_ingresa` ; ALTER TABLE `documento` ADD CONSTRAINT   FOREIGN KEY (`id_usuario_orden`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE;");
+	}
+	if (!empty($id_usuario_ingresa)) $documento->Edit('id_usuario_ingresa', $id_usuario_ingresa);
+	if(!empty($id_usuario_orden)) $documento->Edit('id_usuario_orden', $id_usuario_orden);
 
 		$usuario_defecto = empty($documento->fields['id_usuario']) ? $id_usuario : 'NULL';
 //print_r($documento);
