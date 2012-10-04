4ef3a6eb3d4c2d95807db5f645c01e7906944c4a
esanmartin <esanmartin@0ed447c4-9eac-4a15-b70a-0eefa0b26830>
2012-10-04 16:02:09 +0000

    Correccion de la url en duro de perezdearce
    git-svn-id: svn://svn.lemontech.cl/time_tracking/tags/produccion@9925 0ed447c4-9eac-4a15-b70a-0eefa0b26830

--- app/interfaces/planillas/planilla_deudas.php
+++ app/interfaces/planillas/planilla_deudas.php
@@ -47,7 +47,7 @@ if(in_array($_REQUEST['opcion'], array('buscar', 'xls'))){
 			-SUM(IF(T.dias_atraso_pago BETWEEN 61 AND 90, T.saldo, 0)) AS '61-90',
 			-SUM(IF(T.dias_atraso_pago > 90, T.saldo, 0)) AS '91+',
 			-SUM(T.saldo) AS total,
-			GROUP_CONCAT(CONCAT('<a href=\'http://perezdearce.thetimebilling.com/time_tracking/app/interfaces/cobros6.php?id_cobro=',identificador,'&popup=1&contitulo=true&id_foco=2\' target=\"popup\" onClick=\"window.open(this.href, this.target, \'width=300,height=400\'); return false;\">',identificador,'</a>')) as cobros
+			GROUP_CONCAT(CONCAT('<a href=\'/time_tracking/app/interfaces/cobros6.php?id_cobro=',identificador,'&popup=1&contitulo=true&id_foco=2\' target=\"popup\" onClick=\"window.open(this.href, this.target, \'width=300,height=400\'); return false;\">',identificador,'</a>')) as cobros
 		FROM
 			(SELECT
 				d.fecha,
