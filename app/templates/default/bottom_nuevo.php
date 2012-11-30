5761a326d1f1a30590c58126da87a10906449282
Javier Lopez <ameboide@gmail.com>
2012-11-30 13:42:16 -0300

    usando cookies

--- app/templates/default/bottom_nuevo.php
+++ app/templates/default/bottom_nuevo.php
@@ -1,27 +1,27 @@
 
-        <script type="text/javascript">window.jQuery || document.write('<script src="//static.thetimebilling.com/js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
-        <script type="text/javascript" src="//static.thetimebilling.com/js/plugins.js"></script>
-        <script type="text/javascript" src="//static.thetimebilling.com/js/main.js"></script>
-		<script type="text/javascript" src="//static.thetimebilling.com/js/bottom.js?20121106"></script>
-		
-		
-        
+<script type="text/javascript">window.jQuery || document.write('<script src="//static.thetimebilling.com/js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
+<script type="text/javascript" src="//static.thetimebilling.com/js/plugins.js"></script>
+<script type="text/javascript" src="//static.thetimebilling.com/js/main.js"></script>
+<script type="text/javascript" src="//static.thetimebilling.com/js/bottom.js?20121106"></script>
+
+
+
 <div style="clear:both;display:block;">&nbsp;</div>
 
 
-<?php  
-$Slim=Slim::getInstance('default',true);
-	if($popup==true) {
-		$Slim->applyHook('hook_footer_popup');
-	} else {
-		$Slim->applyHook('hook_footer');
-		echo '<script type="text/javascript" src="//static.thetimebilling.com/js/footer.js"></script>';
-		echo '<div id="ultimocontenedor" style="clear:both;height:70px; width:130px;margin:40px auto 5px ;text-align:center;">
+<?php
+$Slim = Slim::getInstance('default', true);
+if ($popup == true) {
+	$Slim->applyHook('hook_footer_popup');
+} else {
+	$Slim->applyHook('hook_footer');
+	echo '<script type="text/javascript" src="//static.thetimebilling.com/js/footer.js"></script>';
+	echo '<div id="ultimocontenedor" style="clear:both;height:70px; width:130px;margin:40px auto 5px ;text-align:center;">
 				<img src="//static.thetimebilling.com/images/logo_bottom.jpg" width="125" height="37" style="padding:15px 15px 0;float:left;" />&nbsp;
 			<div id="DigiCertClickID_iIR9fwBQ" style="float:right;" >&nbsp;</div>
 			</div>';
-	}
- ?>	
+}
+?>	
 <script type="text/javascript">
 
     jQuery.ajax({async: true,cache:true, type: "GET", url: "//static.thetimebilling.com/js/droplinemenu.js"	,
@@ -52,20 +52,21 @@ if (file_exists($path_aviso)) {
 
 	//solo mostrar aviso a los q tienen algun permiso especificado
 	$mostrar = false;
-	if(isset($aviso['permiso'])){
-		$sesion = new Sesion( array() );
-		foreach($aviso['permiso'] as $permiso){
-			if($sesion->usuario->TienePermiso($permiso)){
+	if (isset($aviso['permiso'])) {
+		$sesion = new Sesion(array());
+		foreach ($aviso['permiso'] as $permiso) {
+			if ($sesion->usuario->TienePermiso($permiso)) {
 				$mostrar = true;
 				break;
 			}
 		}
 	}
 
-	if($mostrar){ ?>
+	if ($mostrar) {
+		?>
 		<span id="mostrar_aviso" style="position:fixed;right:20px;top:20px;color:#fff;cursor:pointer;font-size:larger">[!]</span>
 		<link rel="stylesheet" type="text/css" href="//static.thetimebilling.com/css/jquery.gritter.css" />
-        <script type="text/javascript" src="//static.thetimebilling.com/js/jquery.gritter.min.js"></script>
+		<script type="text/javascript" src="//static.thetimebilling.com/js/jquery.gritter.min.js"></script>
 		<style>
 			.notificacion p, .notificacion a{
 				color: #eee !important;
@@ -93,6 +94,7 @@ if (file_exists($path_aviso)) {
 			if(aviso.link){
 				aviso.mensaje += '<br/><br/><a href="' + aviso.link + '">Ver más información...</a>';
 			}
+
 			function avisar_actualizacion(){
 				jQuery.gritter.add({
 					title: 'Aviso de actualización',
@@ -101,18 +103,18 @@ if (file_exists($path_aviso)) {
 					sticky: true,
 					class_name: 'notificacion',
 					after_close: function(){
-						if(localStorage){
-							localStorage['esconder_notificacion'] = aviso.id;
-						}
+						setCookie('esconder_notificacion', aviso.id);
 					}
 				});
 			}
+
 			jQuery('#mostrar_aviso').click(avisar_actualizacion);
-			if(!localStorage || localStorage['esconder_notificacion'] != aviso.id){
+			if(getCookie('esconder_notificacion') != aviso.id){
 				avisar_actualizacion();
 			}
 		</script>
-	<?php }
+		<?php
+	}
 }
 ?>
 </div>
