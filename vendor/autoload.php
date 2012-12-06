2f6a871e95f9d659ac7b25c5dc016709275bb102
Abraham Barrera <abarrerac@gmail.com>
2012-12-06 10:07:12 -0300

    aplica CodeSniffer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,11 +1,11 @@
 <?php
- 
+
 function autocargattb($class_name) {
-	$class_name=str_replace('_', DIRECTORY_SEPARATOR,$class_name);
+	$class_name = str_replace('_' , DIRECTORY_SEPARATOR, $class_name);
 
 	if (is_readable(APPPATH.'/app/classes/' . $class_name . '.php')) {
 		require_once APPPATH.'/app/classes/'  . $class_name . '.php';
-	} else if (is_readable(APPPATH.'/fw/classes/' . $class_name . '.php')) {
+	} elseif (is_readable(APPPATH.'/fw/classes/' . $class_name . '.php')) {
 		require_once APPPATH.'/fw/classes/' . $class_name . '.php';
 	} else {
 		return false;
@@ -13,5 +13,6 @@ function autocargattb($class_name) {
 }
 spl_autoload_register('autocargattb');
 
-if (!class_exists('Slim') && is_readable(APPPATH.'/fw/classes/Slim/Slim.php')) 		require_once APPPATH.'/fw/classes/Slim/Slim.php';
- 
+if (!class_exists('Slim') && is_readable(APPPATH . '/fw/classes/Slim/Slim.php')) {
+	require_once APPPATH.'/fw/classes/Slim/Slim.php';
+}
