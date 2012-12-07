d1407ff1193d1459b31470ce66c578e4fa04d341
Matias Orellana <morellan@gmail.com>
2012-12-07 19:25:06 -0300

    El mensaje de un nuevo aviso ahora aparece mas notorio, y va cambiando si es que el usuario marcó que desea ocultar la notificación siguiendo las recomendaciones de http://onsaasproducts.wordpress.com/2011/05/14/announcing-new-product-features/
    También el link ahora te manda a una nueva página para no perder lo que
    estabas haciendo

--- app/templates/default/bottom_nuevo.php
+++ app/templates/default/bottom_nuevo.php
@@ -21,7 +21,7 @@ if ($popup == true) {
 			<div id="DigiCertClickID_iIR9fwBQ" style="float:right;" >&nbsp;</div>
 			</div>';
 }
-?>	
+?>
 <script type="text/javascript">
 
 		jQuery.ajax({async: true,cache:true, type: "GET", url: "//static.thetimebilling.com/js/droplinemenu.js"	,
@@ -30,7 +30,7 @@ if ($popup == true) {
 			droplinemenu.buildmenu("droplinetabs1");
 		}
 		});
-	 
+
 	var color=['#B4D3B5','#E7EABB','#FFCAD8','#FFD2C4','#FFE9A6','#A4BE81','#BEFFA8','#A8FFBE','#E0E0E0','#BEF','#DD9','#A4BBFF','#CDA9FE','#FDDC9F','#BABC5C','#CCF','#EAADAA','#AD9F69','#CABCE0','#EB9C63','#45989C','#C0ACD7','#AFCC91','#FFB0B0','#BDAD75','#E8E471','#DC7E7E','#BDD76F','#A0A5D6','#AACCAC','#A4BBFF','#ECD7CA','#D5C9AC','#EDBFA9','#FFEBAE','#8E9C43','#68BB77','#85B188','#DAB6B6','#429D99','#FC9','#E7CEFF','#69C','#FFEBAE','#D2009E','#D9C6FF','#3DAB9A','#AB6573','#C1CDBC','#99CC66','#9999CC','#99CC99','#CCFFCC','#CCFF99','#FFFF66','#F2F587','#55A6AA','#75808A','#C7826D','#C6DEAD','#C6DEDA','#AEAE00','#3F7C7C','#FFCCF2','#92CB45','#8D8E82','#BDEE73','#CCC','#996','#CC6','#699','#F2F2F2','#E9BBBA','#8C4646','#C8EEFD','#C5DEFA','#D1D8D7','#FF99FF','#FC9','#3C7B91','#5EA6BD','#FEEEBC','#CAC793','#FEB1B4','#CC6633','#CCC','#66CC66','#66FF66'];
 	function s2c(s){
 		if(!s) return;
@@ -44,12 +44,12 @@ if ($popup == true) {
 	}
 
 </script>
- 
+
 </div>
 <div id="dialogomodal" style="display:none;text-align:center" > </div>
 <div id="dialog-confirm" style="display:none;" ></div>
 <div id="lttooltip"></div>
-<?php 
+<?php
 	$mostrar_aviso = Aviso::MostrarAviso();
 	if ($mostrar_aviso) {
 		$aviso = Aviso::Obtener();
@@ -82,7 +82,7 @@ if ($popup == true) {
 				}
 			}
 			if(aviso.link) {
-				aviso.mensaje += '<br/><br/><a href="' + aviso.link + '">Ver más información</a>&nbsp;&nbsp;';
+				aviso.mensaje += '<br/><br/><a href="' + aviso.link + '" target="_blank">Ver más información</a>&nbsp;&nbsp;';
 			} else {
 				aviso.mensaje += '<br/>	<br/>&nbsp;&nbsp;';
 			}
@@ -91,9 +91,9 @@ if ($popup == true) {
 			function desactivar_mensaje() { 
 				document.cookie ='esconder_notificacion=' + aviso.id + '; path=/';
 				if (mensaje && mensaje > 0) {
-					jQuery.gritter.remove(mensaje, { 
+					jQuery.gritter.remove(mensaje, {
 						fade: false,
-						speed: 'fast' 
+						speed: 'fast'
 					});
 				}
 			}
@@ -110,7 +110,7 @@ if ($popup == true) {
 			}
 			jQuery('#ocultar_aviso').live('click', desactivar_mensaje);
 			jQuery('#mostrar_aviso').click(avisar_actualizacion);
-			if(getCookie('esconder_notificacion') != aviso.id) {
+			if (getCookie('esconder_notificacion') != aviso.id) {
 				avisar_actualizacion();
 			}
 		</script>
