9de83eadeb5bd773dbad6b8e8c4e22d2fbb33ee7
Matias Orellana <morellan@gmail.com>
2012-12-12 20:46:34 -0300

    - Se integra el desarrollo de integracion_contabilidad4.php desde trunk - Se mueve la Query de consulta de la vista gastos.php a la clase Gasto - Corrección de formato

--- app/classes/Gasto.php
+++ app/classes/Gasto.php
@@ -138,7 +138,7 @@ class Gasto extends Objeto {
 	}
 
 	function Check() {
-		# Los gastos dependiendo de si son generales o no, van a diferentes tablas. 
+		# Los gastos dependiendo de si son generales o no, van a diferentes tablas.
 		# La tabla por defecto es cta_corriente
 		# Además a los gastos asociados a un asunto se les calcula un monto descontado que es con la tasa de cambio del dia en que se anoto. Esto es para que no cambie el monto que se descuenta si es que cambia la tasa.
 		if ($this->changes[general] == 1) {
@@ -177,7 +177,7 @@ class Gasto extends Objeto {
 
 	function Write() {
 		if ($this->Loaded()) {
-			$query = "SELECT fecha, codigo_cliente, codigo_asunto, egreso, ingreso, monto_cobrable, descripcion, id_moneda  
+			$query = "SELECT fecha, codigo_cliente, codigo_asunto, egreso, ingreso, monto_cobrable, descripcion, id_moneda
 					FROM cta_corriente WHERE id_movimiento = " . $this->fields['id_movimiento'];
 			$resp = mysql_query($query, $this->sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $this->sesion->dbh);
 			list($fecha, $codigo_cliente, $codigo_asunto, $egreso, $ingreso, $monto_cobrable, $descripcion, $id_moneda) = mysql_fetch_array($resp);
@@ -190,8 +190,8 @@ class Gasto extends Objeto {
 				$query_valor_ingreso = $ingreso;
 			}
 
-			$query = "INSERT INTO gasto_historial 
-						( id_movimiento, fecha, id_usuario, accion, fecha_movimiento, fecha_movimiento_modificado, codigo_cliente, codigo_cliente_modificado, codigo_asunto, codigo_asunto_modificado, ingreso, ingreso_modificado, monto_cobrable, monto_cobrable_modificado, descripcion, descripcion_modificado, id_moneda, id_moneda_modificado) 
+			$query = "INSERT INTO gasto_historial
+						( id_movimiento, fecha, id_usuario, accion, fecha_movimiento, fecha_movimiento_modificado, codigo_cliente, codigo_cliente_modificado, codigo_asunto, codigo_asunto_modificado, ingreso, ingreso_modificado, monto_cobrable, monto_cobrable_modificado, descripcion, descripcion_modificado, id_moneda, id_moneda_modificado)
 					VALUES( " . $this->fields['id_movimiento'] . ", NOW(), '" . $this->sesion->usuario->fields['id_usuario'] . "', 'MODIFICAR', '" . $fecha . "', '" . $this->fields['fecha'] . "', '" . $codigo_cliente . "', '" . $this->fields['codigo_cliente'] . "', '" . $codigo_asunto . "', '" . $this->fields['codigo_asunto'] . "', '" . $query_valor_ingreso . "', '" . $query_tipo_ingreso . "', '" . $monto_cobrable . "', '" . $this->fields['monto_cobrable'] . "', '" . addslashes($descripcion) . "', '" . addslashes($this->fields['descripcion']) . "', " . $id_moneda . ", " . $this->fields['id_moneda'] . ")";
 		} else {
 			$query = "SELECT MAX(id_movimiento) FROM cta_corriente";
@@ -205,7 +205,7 @@ class Gasto extends Objeto {
 				$query_tipo_ingreso = $this->fields['ingreso'];
 			}
 
-			$query = "INSERT INTO gasto_historial 
+			$query = "INSERT INTO gasto_historial
 						( id_movimiento, fecha, id_usuario, accion, fecha_movimiento_modificado, codigo_cliente_modificado, codigo_asunto_modificado, ingreso_modificado, monto_cobrable_modificado, descripcion_modificado, id_moneda_modificado)
 					VALUES( " . $id_movimiento . ", NOW(), '" . $this->sesion->usuario->fields['id_usuario'] . "', 'CREAR', '" . $this->fields['fecha'] . "', '" . $this->fields['codigo_cliente'] . "', '" . $this->fields['codigo_asunto'] . "','" . $query_tipo_ingreso . "', '" . $this->fields['monto_cobrable'] . "', '" . addslashes($this->fields['descripcion']) . "', " . $this->fields['id_moneda'] . ")";
 		}
@@ -256,7 +256,7 @@ class Gasto extends Objeto {
 	 */
 
 	function EliminaIngreso($id_gasto) {
-		$query = "SELECT COUNT(*) FROM cta_corriente WHERE id_movimiento_pago = '" . $this->fields[id_movimiento] . "' 
+		$query = "SELECT COUNT(*) FROM cta_corriente WHERE id_movimiento_pago = '" . $this->fields[id_movimiento] . "'
 					AND id_movimiento != '$id_gasto'";
 		$resp = mysql_query($query, $this->sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $this->sesion->dbh);
 		list($cont) = mysql_fetch_array($resp);
@@ -274,16 +274,67 @@ class Gasto extends Objeto {
 	 */
 	public function DownloadExcel($search_query) {
 		require_once Conf::ServerDir() . '/classes/Reportes/SimpleReport.php';
-		
+
 		$SimpleReport = new SimpleReport($this->sesion);
 		$SimpleReport->SetRegionalFormat(UtilesApp::ObtenerFormatoIdioma($this->sesion));
 		$SimpleReport->LoadConfiguration('GASTOS');
-		
+
 		$results = $this->sesion->pdodbh->query($search_query)->fetchAll(PDO::FETCH_ASSOC);
 		$SimpleReport->LoadResults($results);
-		
+
 		$writer = SimpleReport_IOFactory::createWriter($SimpleReport, 'Spreadsheet');
 		$writer->save(__('Gastos'));
+	}
+
+	public function SearchQuery($where) {
+		return "SELECT SQL_BIG_RESULT SQL_NO_CACHE
+				cta_corriente.id_movimiento,
+				DATE_FORMAT(cta_corriente.fecha, '%Y-%m-%d') AS fecha,
+				DATE_FORMAT(cta_corriente.fecha_creacion, '%Y-%m-%d') AS fecha_creacion,
+				cta_corriente.codigo_cliente,
+				cliente.glosa_cliente,
+				asunto.codigo_asunto,
+				asunto.glosa_asunto,
+				CONCAT(u_encargado.apellido1, ', ', u_encargado.nombre) AS encargado_comercial,
+				CONCAT(u_ingresa.apellido1, ', ', u_ingresa.nombre) AS usuario_ingresa,
+				CONCAT(u_ordena.apellido1, ', ', u_ordena.nombre) AS usuario_ordena,
+				prm_cta_corriente_tipo.glosa AS tipo,
+				cta_corriente.descripcion,
+				prm_moneda.simbolo,
+				cta_corriente.egreso,
+				cta_corriente.ingreso,
+				IF(
+					monto_cobrable = ingreso,
+					monto_cobrable * (-1),
+					monto_cobrable
+				) AS monto_cobrable,
+				cta_corriente.con_impuesto,
+				cta_corriente.id_cobro,
+				IFNULL(cobro.estado, 'SIN COBRO') AS estado_cobro,
+				IF(cta_corriente.cobrable = 1,'SI', 'NO') AS cobrable,
+				cta_corriente.numero_documento,
+				prm_proveedor.rut AS rut_proveedor,
+				prm_proveedor.glosa AS nombre_proveedor,
+				prm_tipo_documento_asociado.glosa AS tipo_documento_asociado,
+				cta_corriente.fecha_factura AS fecha_documento_asociado,
+				cta_corriente.codigo_factura_gasto AS codigo_documento_asociado,
+				prm_moneda.cifras_decimales,
+				cta_corriente.numero_ot,
+				cta_corriente.id_moneda
+			FROM cta_corriente
+			LEFT JOIN cliente ON cta_corriente.codigo_cliente = cliente.codigo_cliente
+			LEFT JOIN asunto ON asunto.codigo_asunto = cta_corriente.codigo_asunto
+			LEFT JOIN contrato ON asunto.id_contrato = contrato.id_contrato
+			LEFT JOIN usuario AS u_ingresa ON u_ingresa.id_usuario = cta_corriente.id_usuario
+			LEFT JOIN usuario AS u_ordena ON u_ordena.id_usuario = cta_corriente.id_usuario_orden
+			LEFT JOIN usuario AS u_encargado ON u_encargado.id_usuario = contrato.id_usuario_responsable
+			LEFT JOIN prm_cta_corriente_tipo ON (prm_cta_corriente_tipo.id_cta_corriente_tipo = cta_corriente.id_cta_corriente_tipo)
+			LEFT JOIN prm_moneda ON cta_corriente.id_moneda=prm_moneda.id_moneda
+			LEFT JOIN prm_tipo_documento_asociado ON cta_corriente.id_tipo_documento_asociado = prm_tipo_documento_asociado.id_tipo_documento_asociado
+			LEFT JOIN prm_proveedor ON ( cta_corriente.id_proveedor = prm_proveedor.id_proveedor )
+			LEFT JOIN prm_glosa_gasto ON ( cta_corriente.id_glosa_gasto = prm_glosa_gasto.id_glosa_gasto )
+			LEFT JOIN cobro ON cobro.id_cobro=cta_corriente.id_cobro
+			WHERE 1 AND $where";
 	}
 }
 
