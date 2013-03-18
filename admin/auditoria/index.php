1da3d274c94ca310fac4c3c13a673fdac7d27e57
Claudio Peralta <cuayocps@gmail.com>
2013-03-18 11:09:25 -0300

    Corrige ortografía.

--- admin/auditoria/index.php
+++ admin/auditoria/index.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . '/../../app/conf.php';
 $sesion = new Sesion(array('ADM'));
 $pagina = new Pagina($sesion);
-$pagina->titulo = __('Auditorias');
+$pagina->titulo = __('Auditorías');
 $pagina->PrintTop();?>
 
 <a href="correos.php">Correos</a>
