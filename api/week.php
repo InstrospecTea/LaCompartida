1be2f795d71e7d46eb912d0932101106f5b08560
Matias Orellana <morellan@gmail.com>
2013-04-03 18:51:23 -0300

    Primera versión con tooltip

--- api/week.php
+++ api/week.php
@@ -100,6 +100,70 @@ if (!is_object($user_token_data)) {
       #celdastrabajo #celdadia7 .totaldia {
         width: 10%;
       }
+
+      /*
+       *
+       */
+      #tooltip
+    {
+      font-family: Ubuntu, sans-serif;
+      font-size: 0.875em;
+      text-align: center;
+      text-shadow: 0 1px rgba( 0, 0, 0, .5 );
+      line-height: 1.5;
+      color: #fff;
+      background: #333;
+      background: -webkit-gradient( linear, left top, left bottom, from( rgba( 0, 0, 0, .7 ) ), to( rgba( 0, 0, 0, .9 ) ) );
+      background: -webkit-linear-gradient( top, rgba( 0, 0, 0, .7 ), rgba( 0, 0, 0, .9 ) );
+      background: -moz-linear-gradient( top, rgba( 0, 0, 0, .7 ), rgba( 0, 0, 0, .9 ) );
+      background: -ms-radial-gradient( top, rgba( 0, 0, 0, .7 ), rgba( 0, 0, 0, .9 ) );
+      background: -o-linear-gradient( top, rgba( 0, 0, 0, .7 ), rgba( 0, 0, 0, .9 ) );
+      background: linear-gradient( top, rgba( 0, 0, 0, .7 ), rgba( 0, 0, 0, .9 ) );
+      -webkit-border-radius: 5px;
+      -moz-border-radius: 5px;
+      border-radius: 5px;
+      border-top: 1px solid #fff;
+      -webkit-box-shadow: 0 3px 5px rgba( 0, 0, 0, .3 );
+      -moz-box-shadow: 0 3px 5px rgba( 0, 0, 0, .3 );
+      box-shadow: 0 3px 5px rgba( 0, 0, 0, .3 );
+      position: absolute;
+      z-index: 100;
+      padding: 15px;
+      text-align: left;
+    }
+      #tooltip:after
+      {
+            width: 0;
+            height: 0;
+            border-left: 10px solid transparent;
+            border-right: 10px solid transparent;
+        border-top: 10px solid #333;
+            border-top-color: rgba( 0, 0, 0, .7 );
+        content: '';
+        position: absolute;
+        left: 50%;
+        bottom: -10px;
+        margin-left: -10px;
+      }
+        #tooltip.top:after
+        {
+              border-top-color: transparent;
+          border-bottom: 10px solid #333;
+              border-bottom-color: rgba( 0, 0, 0, .6 );
+          top: -20px;
+          bottom: auto;
+        }
+        #tooltip.left:after
+        {
+          left: 10px;
+          margin: 0;
+        }
+        #tooltip.right:after
+        {
+          right: 10px;
+          left: auto;
+          margin: 0;
+        }
     </style>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script src="//static.thetimebilling.com/js/bottom.js"></script>
@@ -113,10 +177,86 @@ $semana = $day;
 include APPPATH . '/app/interfaces/ajax/semana_ajax.php';
 ?>
     <script>
-      jQuery(document).ready(function () {
-        jQuery('.pintame').each(function() {
-          jQuery(this).css('background-color', window.top.s2c(jQuery(this).attr('rel')));
+      $(document).ready(function () {
+        $('.pintame').each(function() {
+          $(this).css('background-color', window.top.s2c($(this).attr('rel')));
         });
+
+        var targets = $( '.cajatrabajo' ),
+        target  = false,
+        tooltip = false,
+        title   = false;
+ 
+    targets.bind( 'mouseenter', function()
+    {
+        target  = $( this );
+        tip     = target.attr( 'onmouseover' );
+        tip = tip.replace("ddrivetip('", "");
+        tip = tip.replace("')", "");
+        tooltip = $( '<div id="tooltip"></div>' );
+ 
+        if( !tip || tip == '' )
+            return false;
+ 
+        target.removeAttr( 'title' );
+        tooltip.css( 'opacity', 0 )
+               .html( tip )
+               .appendTo( 'body' );
+ 
+        var init_tooltip = function()
+        {
+            if( $( window ).width() < tooltip.outerWidth() * 1.5 )
+                tooltip.css( 'max-width', $( window ).width() / 2 );
+            else
+                tooltip.css( 'max-width', 340 );
+ 
+            var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
+                pos_top  = target.offset().top - tooltip.outerHeight() - 20;
+ 
+            if( pos_left < 0 )
+            {
+                pos_left = target.offset().left + target.outerWidth() / 2 - 20;
+                tooltip.addClass( 'left' );
+            }
+            else
+                tooltip.removeClass( 'left' );
+ 
+            if( pos_left + tooltip.outerWidth() > $( window ).width() )
+            {
+                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
+                tooltip.addClass( 'right' );
+            }
+            else
+                tooltip.removeClass( 'right' );
+ 
+            if( pos_top < 0 )
+            {
+                var pos_top  = target.offset().top + target.outerHeight();
+                tooltip.addClass( 'top' );
+            }
+            else
+                tooltip.removeClass( 'top' );
+ 
+            tooltip.css( { left: pos_left, top: pos_top } )
+                   .animate( { top: '+=10', opacity: 1 }, 50 );
+        };
+ 
+        init_tooltip();
+        $( window ).resize( init_tooltip );
+ 
+        var remove_tooltip = function()
+        {
+            tooltip.animate( { top: '-=10', opacity: 0 }, 50, function()
+            {
+                $( this ).remove();
+            });
+ 
+            target.attr( 'title', tip );
+        };
+ 
+        target.bind( 'mouseleave', remove_tooltip );
+        tooltip.bind( 'click', remove_tooltip );
+    });
       });
     </script>
   </body>
