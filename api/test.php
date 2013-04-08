d2c27c86a107ca2c57b581b433356164e155d933
Claudio Briones <brionescl@gmail.com>
2013-04-08 18:10:15 -0300

    glosa del asunto tiene que ser distinto a vacío

--- api/test.php
+++ api/test.php
@@ -1,20 +1,21 @@
 <html>
 	<head>
-		 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	</head>
 	<body>
 		<div id="output"></div>
 		<script type="text/javascript">
 		$(function() {
 			$.ajaxSetup({
-				headers: {'AUTH_TOKEN' : '24739c6d5ac33e4c0bda9c97c1bfe179c1ea8267'}
+				headers: {'AUTH_TOKEN' : 'f0ef5f57052f8836c8815c1ec83e31a52be81571'}
 			});
 
 			$.ajax({
 				//url: 'login',
 				//url: 'clients/002538/matters',
-				//url: 'users/1/works',
-				url: 'users/1/works/618205',
+				url: 'users/1/works',
+				//url: 'users/1/works/618205',
+
 				/*data: {
 					'user' : '99511620',
 					'password' : 'admin.asdwsx',
@@ -30,15 +31,16 @@
 					//'requester' : 'test1 test2 test3',
 					//'activity_code' : 'A2013',
 					//'area_code' : 1,
-					'matter_code' : '0001-03',
+					'matter_code' : '0001-0001',
 					//'task_code' : 1,
-					//'user_id' : 1,
+					'user_id' : 1,
 					'billable' : 1,
 					'visible' : 0
 				},
+
 				//type: 'get'
-				type: 'post'
-				//type: 'put'
+				//type: 'post'
+				type: 'put'
 			}).done(function(data) {
 				//console.log(data);
 				//$('#output').html(data);
