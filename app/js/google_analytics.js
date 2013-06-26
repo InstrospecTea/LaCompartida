c63170347106f736aacef1ba6504dc9bc34ec161
Claudio Briones <brionescl@gmail.com>
2013-06-26 16:58:59 -0400

    agregar google-analytics

--- app/js/google_analytics.js
+++ app/js/google_analytics.js
@@ -1,13 +1,14 @@
-/* Codigo para analizar el trafico via google analytics */
-
+/* Código para analizar el trafico via google analytics */
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-21661196-2']);
-_gaq.push(['_setDomainName', 'none']);
-_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_setDomainName', 'thetimebilling.com']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
 })();
