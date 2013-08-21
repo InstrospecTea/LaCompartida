7503949f40a624ed3d1765a243093c7652e9b5aa
Abraham Barrera <abarrerac@gmail.com>
2013-08-21 18:05:53 -0400

    Reorganización de plugin para que funcione inicializador y ejecución en el mismo archivo

--- app/plugins/facturacion_electronica_mx.php
+++ app/plugins/facturacion_electronica_mx.php
@@ -1,79 +1,87 @@
 <?php
 require_once dirname(__FILE__) . '/../conf.php';
 
-$Slim = Slim::getInstance('default',true);
+$id_factura = $_REQUEST['id_factura_generar'];
 
-$Slim->hook('hook_cobros7_botones_after', 'AgregarBotonFacturaElectronica');
-$Slim->hook('hook_guardar_documento_pago', 'Guardar_Adelantos_Con_Usuario_Orden');
-
-function AgregarBotonFacturaElectronica() {
-	global $id_factura;
-
-	echo '<a href="../plugins/facturacion_electronica_mx.php?id_factura=' . $id_factura . '" target="_factura_electronica">
-			<img src="' . Conf::ImgDir() . '/add_noticia16.gif" border="0" />
-		</a>';
+if (!$id_factura) {
+	#inicialización de SLIM
+	$Slim = Slim::getInstance('default',false);
+	$Slim->hook('hook_cobros7_botones_after', 'AgregarBotonFacturaElectronica');
+} else {
+	GeneraFactura($id_factura);
 }
 
-if (empty($Factura->fields['dte_url_pdf'])) {
-	$client = new SoapClient("https://www.facturemosya.com:443/webservice/sRecibirXML.php?wsdl");
-	$usuario = Conf::GetConf($Sesion, 'FacturacionElectronicaUsuario');
-	$password = Conf::GetConf($Sesion, 'FacturacionElectronicaPassword');
-	
-	if ($err) {
-		$return = 'Constructor error: ' . $err;
+function GeneraFactura($id_factura) {
+
+	$Sesion = new Sesion();
+	$Factura = new Factura($Sesion);
+	$Factura->Load($id_factura);
+
+	if (!$Factura->Loaded()) {
+		die('Error, factura no existe');
 	}
 
-// 	$strdocumento = 'COM|||version|3.2||serie|WS||folio|15||fecha|2013-07-18T10:14:49||formaDePago|PAGO EN UNA SOLA EXHIBICION||TipoCambio|1.000||condicionesDePago|EFECTOS FISCALES AL PAGO||subTotal|425.00||Moneda|MX||total|493.00||tipoDeComprobante|ingreso||metodoDePago|PAGO NO IDENTIFICADO||LugarExpedicion|MEXICO DISTRITO FEDERAL||NumCtaPago|1234||descuento|0.00||motivoDescuento|desc
-// REF|||Regimen|REGIMEN GENERAL DE LEY PERSONAS MORALES
-// REC|||rfc|DNM070221BS4||nombre|DISEÃ‘OS NAOMI MEXICO, S.A. DE C.V.
-// DOR|||calle|JOSE MARIA IZAZAGA # 50 DESP 101 1ER PISO||noExterior|51||colonia|CENTRO||municipio|CUAHUTEMOC||estado|MEXICO, D.F.||pais|MEXICO||codigopostal|06000
-// CON|||cantidad|850||unidad|M||noIdentificacion|6XO959455C-BRU||descripcion|COLA DE RATA X  METRO||valorUnitario|0.50||descuento|0||importe|425.00
-// CUP|||numero|A-1234
-// RET|||impuesto|IVA||importe|0
-// TRA|||impuesto|IVA||tasa|16.0||importe|68.00
-// ADI|||numorden|111111||comentarios|demo comentarios';
-	$strdocumento = FacturaToTXT($Sesion, $Factura);
+	if (empty($Factura->fields['dte_url_pdf'])) {
+		$client = new SoapClient("https://www.facturemosya.com:443/webservice/sRecibirXML.php?wsdl");
+		$usuario = Conf::GetConf($Sesion, 'FacturacionElectronicaUsuario');
+		$password = Conf::GetConf($Sesion, 'FacturacionElectronicaPassword');
 
-	$result = $client->RecibirTXT($usuario, $password, $strdocumento);
+		if ($err) {
+			$return = 'Constructor error: ' . $err;
+		}
 
-	if ($result->codigo == 201) {
-		try {
-			$result_xml = $client->RecibirXML($usuario, $password, $strdocumento);
+		// 	$strdocumento = 'COM|||version|3.2||serie|WS||folio|15||fecha|2013-07-18T10:14:49||formaDePago|PAGO EN UNA SOLA EXHIBICION||TipoCambio|1.000||condicionesDePago|EFECTOS FISCALES AL PAGO||subTotal|425.00||Moneda|MX||total|493.00||tipoDeComprobante|ingreso||metodoDePago|PAGO NO IDENTIFICADO||LugarExpedicion|MEXICO DISTRITO FEDERAL||NumCtaPago|1234||descuento|0.00||motivoDescuento|desc
+		// REF|||Regimen|REGIMEN GENERAL DE LEY PERSONAS MORALES
+		// REC|||rfc|DNM070221BS4||nombre|DISEÃ‘OS NAOMI MEXICO, S.A. DE C.V.
+		// DOR|||calle|JOSE MARIA IZAZAGA # 50 DESP 101 1ER PISO||noExterior|51||colonia|CENTRO||municipio|CUAHUTEMOC||estado|MEXICO, D.F.||pais|MEXICO||codigopostal|06000
+		// CON|||cantidad|850||unidad|M||noIdentificacion|6XO959455C-BRU||descripcion|COLA DE RATA X  METRO||valorUnitario|0.50||descuento|0||importe|425.00
+		// CUP|||numero|A-1234
+		// RET|||impuesto|IVA||importe|0
+		// TRA|||impuesto|IVA||tasa|16.0||importe|68.00
+		// ADI|||numorden|111111||comentarios|demo comentarios';
+		$strdocumento = FacturaToTXT($Sesion, $Factura);
 
-			if ($result_xml->codigo == 201) {
-				$Factura->Edit('dte_xml', $result->documento);
+		$result = $client->RecibirTXT($usuario, $password, $strdocumento);
+
+		if ($result->codigo == 201) {
+			try {
+				$result_xml = $client->RecibirXML($usuario, $password, $strdocumento);
+
+				if ($result_xml->codigo == 201) {
+					$Factura->Edit('dte_xml', $result->documento);
+				}
+
+				$Factura->Edit('dte_fecha_creacion', date('Y-m-d H:i:s'));
+				$Factura->Edit('dte_firma', $result->timbrefiscal);
+
+				$file_name = '/dtes/' . Utiles::sql2date($Factura->fields['fecha'], "%Y%m%d") . "_{$Factura->fields['serie_documento_legal']}-{$Factura->fields['numero']}.pdf";
+				$file_data = base64_decode($result->documentopdf);
+				$file_url = UtilesApp::UploadToS3($file_name, $file_data);
+
+				$Factura->Edit('dte_url_pdf', $file_url);
+
+				if ($Factura->Write()) {
+					header("Location: $file_url");
+				}
+			} catch (Exception $ex) {
+				echo "<pre style='color: red;'>" . print_r($ex, true) . "</pre>";
+				// buu
 			}
+		} else {
+				// $result = $client->__soapCall('RecibirTXT', $datos);
 
-			$Factura->Edit('dte_fecha_creacion', date('Y-m-d H:i:s'));
-			$Factura->Edit('dte_firma', $result->timbrefiscal);
+				header('Content-Type: text/html; charset=utf-8');
 
-			$file_name = '/dtes/' . Utiles::sql2date($Factura->fields['fecha'], "%Y%m%d") . "_{$Factura->fields['serie_documento_legal']}-{$Factura->fields['numero']}.pdf";
-			$file_data = base64_decode($result->documentopdf);
-			$file_url = UtilesApp::UploadToS3($file_name, $file_data);
-
-			$Factura->Edit('dte_url_pdf', $file_url);
-
-			if ($Factura->Write()) {
-				header("Location: $file_url");
-			}
-		} catch (Exception $ex) {
-			echo "<pre style='color: red;'>" . print_r($ex, true) . "</pre>";
-			// buu
+				echo "<pre style='color: green;'>" . utf8_decode(utf8_decode(print_r($result, true))) . "</pre>";
+				echo "<pre style='color: grey;'>" . htmlentities($client->__getLastRequest()) . "</pre>";
+				echo "<pre style='color: blue;'>" . htmlentities($client->__getLastResponse()) . "</pre>";
+				echo "<pre style='color: red;'>" . $return . "</pre>";
+				echo "<pre style='color: yellow;'>" . print_r($client->__getFunctions(), true) . "</pre>";
+				echo "<pre style='color: black;'>" . $strdocumento . "</pre>";
 		}
 	} else {
-			// $result = $client->__soapCall('RecibirTXT', $datos);
-
-			header('Content-Type: text/html; charset=utf-8');
-
-			echo "<pre style='color: green;'>" . utf8_decode(utf8_decode(print_r($result, true))) . "</pre>";
-			echo "<pre style='color: grey;'>" . htmlentities($client->__getLastRequest()) . "</pre>";
-			echo "<pre style='color: blue;'>" . htmlentities($client->__getLastResponse()) . "</pre>";
-			echo "<pre style='color: red;'>" . $return . "</pre>";
-			echo "<pre style='color: yellow;'>" . print_r($client->__getFunctions(), true) . "</pre>";
-			echo "<pre style='color: black;'>" . $strdocumento . "</pre>";
+		header("Location: {$Factura->fields['dte_url_pdf']}");
 	}
-} else {
-	header("Location: {$Factura->fields['dte_url_pdf']}");
 }
 
 function FacturaToTXT(Sesion $Sesion, Factura $Factura) {
@@ -147,7 +155,7 @@ function FacturaToTXT(Sesion $Sesion, Factura $Factura) {
 			'descuento|0.00'
 		);
 	}
-	
+
 	foreach ($r as $identificador => $valores) {
 		if (in_array($identificador, array('CON_honorarios', 'CON_gastos_con_iva', 'CON_gastos_sin_iva'))) {
 			$identificador = 'CON';
@@ -158,4 +166,12 @@ function FacturaToTXT(Sesion $Sesion, Factura $Factura) {
 	}
 
 	return $txt;
+}
+
+function AgregarBotonFacturaElectronica() {
+	global $id_factura;
+
+	echo '<a href="../plugins/facturacion_electronica_mx.php?id_factura_generar=' . $id_factura . '" target="_factura_electronica">
+			<img src="' . Conf::ImgDir() . '/add_noticia16.gif" border="0" />
+		</a>';
 }
