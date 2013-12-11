54a1be7ce91843e5ff17bc5332b710e728166977
Claudio Briones <brionescl@gmail.com>
2013-12-11 12:32:41 -0300

    Corregir correlativo de códigos de actividades

--- app/interfaces/agregar_actividades.php
+++ app/interfaces/agregar_actividades.php
@@ -23,6 +23,10 @@ if ($opcion == 'guardar') {
 } else {
 	if ($id_actividad != '') {
 		$Actividad->Load($id_actividad);
+	} else {
+		if (empty($codigo_actividad)) {
+			$codigo_actividad = $Actividad->AsignarCodigoActividad();
+		}
 	}
 
 	$Actividad->Fill($_REQUEST);
@@ -31,7 +35,7 @@ if ($opcion == 'guardar') {
 $Pagina->titulo = __('Actividad');
 if ($Actividad->Loaded()) {
 	$Pagina->titulo = __('Edición') . ' de ' . $Pagina->titulo . ' N° ' . $Actividad->fields['id_actividad'];
-	
+
 	if (!empty($Actividad->fields['codigo_asunto'])) {
 		$Asunto = new Asunto($Sesion);
 		$Asunto->LoadByCodigo($Actividad->fields['codigo_asunto']);
@@ -51,19 +55,19 @@ $Pagina->PrintTop($popup);
 
 <script type="text/javascript">
 	function Validar(p) {
-		if( document.getElementById('codigo_actividad').value=='' ) {
-			alert( 'Debe ingresar un código.' );
+		if (document.getElementById('codigo_actividad').value == '') {
+			alert('Debe ingresar un código.');
 			document.getElementById('codigo_actividad').focus();
 			return false;
 		}
-		if( document.getElementById('glosa_actividad').value=='' ) {
-			alert( 'Debe ingresar un título.' );
+		if (document.getElementById('glosa_actividad').value == '') {
+			alert('Debe ingresar un título.');
 			document.getElementById('glosa_actividad').focus();
 			return false;
 		}
 
 		document.getElementById('form_actividades').submit();
-		
+
 		return true;
 	}
 </script>
@@ -80,10 +84,9 @@ $Pagina->PrintTop($popup);
 					<?php echo __('Código'); ?>
 				</td>
 				<td align="left">
-					<input id="codigo_actividad" name="codigo_actividad" size="5" maxlength="5" value="<?php echo $Actividad->fields['codigo_actividad']; ?>" />
+					<input id="codigo_actividad" name="codigo_actividad" size="5" maxlength="5" value="<?php echo empty($codigo_actividad) ? $Actividad->fields['codigo_actividad'] : $codigo_actividad; ?>" />
 				</td>
 			</tr>
-			
 			<tr>
 				<td align="right">
 					<?php echo __('Título'); ?>
@@ -92,7 +95,6 @@ $Pagina->PrintTop($popup);
 					<input id='glosa_actividad' name='glosa_actividad' size='35' value="<? echo $Actividad->fields['glosa_actividad']; ?>" />
 				</td>
 			</tr>
-			
 			<tr>
 				<td align="right">
 					<?php echo __('Cliente'); ?>
@@ -101,7 +103,6 @@ $Pagina->PrintTop($popup);
 					<?php UtilesApp::CampoCliente($Sesion, $Actividad->extra_fields['codigo_cliente'], $codigo_cliente_secundario, $Actividad->fields['codigo_asunto'], $codigo_asunto_secundario); ?>
 				</td>
 			</tr>
-
 			<tr>
 				<td align="right">
 					<?php echo __('Asunto')?>
@@ -109,14 +110,14 @@ $Pagina->PrintTop($popup);
 				<td align="left">
 					<?php UtilesApp::CampoAsunto($Sesion, $Actividad->extra_fields['codigo_cliente'], $codigo_cliente_secundario, $Actividad->fields['codigo_asunto'], $codigo_asunto_secundario); ?>
 				</td>
-			</tr>		
+			</tr>
 		</table>
 	</fieldset>
 	<br />
-	<div class="fl">																			
+	<div class="fl">
 		<a class="btn botonizame" href="javascript:void(0);" icon="ui-icon-save" onclick="Validar(jQuery('#form_actividades').get(0))"><?php echo  __('Guardar') ?></a>
 		<a class="btn botonizame" href="javascript:void(0);" icon="ui-icon-exit" onclick="window.close();" ><?php echo  __('Cancelar') ?></a>
 	</div>
 </form>
-<?php 
+<?php
 $Pagina->PrintBottom($popup);
