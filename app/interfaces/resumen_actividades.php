490ccb622faba05fefa5b93d0bf99b93ff1d4c7e
Dominik <dominik@HP-425.(none)>
2013-12-17 12:46:35 -0300

    corrige filtro de area usuario

--- app/interfaces/resumen_actividades.php
+++ app/interfaces/resumen_actividades.php
@@ -119,21 +119,6 @@ if (is_array($clientesF)) {
 }
 $clientes = $clientes_tmp;
 
-
-// AREA areas[]
-
-$areas_tmp = '';
-if (is_array($areasF)) {
-	foreach ($areasF as $key => $value) {
-		if (strlen($areas_tmp) > 0) {
-			$areas_tmp .= ",";
-		}
-		$areas_tmp .= $value;
-	}
-}
-$areas_asunto = $areas_tmp;
-
-
 // AREA TRABAJO  areas_trabajo[]
 
 $areas_trabajo_tmp = '';
@@ -845,15 +830,10 @@ if ($opc == 'print' || $popup) {
 	}
 
 	/* AREAS */
-	$areas = explode(",", $areas_asunto);
-	if (!is_array($areas)) {
-		$areas = array($areas);
+	foreach ($areasF as $valor) {
+		$reporte->addFiltro('usuario', 'id_area_usuario', $valor);
 	}
-	foreach ($areas as $area) {
-		if ($area) {
-			$reporte->addFiltro('asunto', 'id_area_proyecto', $area);
-		}
-	}
+
 	/* TIPOS */
 	$tipos = explode(",", $tipos_asunto);
 	if (!is_array($tipos)) {
@@ -905,9 +885,6 @@ if ($opc == 'print' || $popup) {
 	//genero el formato valor a ser usado en las celdas (
 	$moneda = new Moneda($sesion);
 	$moneda->Load($id_moneda);
-
-
-	//echo ">>> " . $reporte->sQuery(); exit; 
 
 	$reporte->Query();
 	$r = $reporte->toArray();
