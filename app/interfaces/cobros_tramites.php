0f5e81f9ccd6d89e7ec524855093220ededaff34
arielrosver <arielrosver@gmail.com>
2014-01-06 18:48:55 -0300

    orden de codigo

--- app/interfaces/cobros_tramites.php
+++ app/interfaces/cobros_tramites.php
@@ -88,61 +88,61 @@ $pagina->PrintTop($popup);
 		</tr>
 	</table>
 	
-	</form>
+</form>
 		
-	<?php echo  InputId::Javascript($sesion);
-	echo '<script src="guardar_campo_trabajo.js"></script>';
+<?php echo  InputId::Javascript($sesion);
+echo '<script src="guardar_campo_trabajo.js"></script>';
 
-	$pagina->PrintBottom($popup);
+$pagina->PrintBottom($popup);
 
-	function funcionTR(& $tramite)
-	{
-		global $sesion;
-        global $id_cobro;
-		static $i = 0;
+function funcionTR(& $tramite)
+{
+	global $sesion;
+    global $id_cobro;
+	static $i = 0;
 
-		if($i % 2 == 0) {
-			$color = "#dddddd";
-		} else {
-			$color = "#ffffff";
-		}	
+	if($i % 2 == 0) {
+		$color = "#dddddd";
+	} else {
+		$color = "#ffffff";
+	}	
 
-        $img_dir = Conf::ImgDir();
-        $tarifa = Funciones::TramiteTarifa($sesion, $trabajo->fields['id_tramite_tipo'],$trabajo->fields['id_moneda_asunto'],$trabajo->fields['codigo_asunto']);
-		
-		list($h,$m,$s) = split(":",$trabajo->fields['duracion_cobrada']);
-		
-		$duracion = $h + ($m > 0 ? ($m / 60) :'0');
-        $total = round($tarifa, 2);
-        $dur_cob = "$h:$m";
-		
-		list($h,$m,$s) = split(":",$trabajo->fields['duracion']); 
-        
-		$dur = "$h:$m";
-		$formato_fecha = "%d/%m/%y";
-		$fecha = Utiles::sql2fecha($trabajo->fields[fecha],$formato_fecha);
-		
-		$html .= "<tr bgcolor=$color style=\"border-right: 1px solid #409C0B; border-left: 1px solid #409C0B;\">";
-		$html .= "<td colspan=9><strong>".$glosa_tramite."</strong></td></tr>";
-	 	$html .= "<tr bgcolor=$color style=\"border-right: 1px solid #409C0B; border-left: 1px solid #409C0B;\">";
-	 	$html .= "<td>$fecha</td>";
-	 	$html .= "<td>".$trabajo->fields[glosa_cliente]."</td>";
-	 	$html .= "<td nowrap>".  $trabajo->fields['glosa_asunto'] . "</td>";
-	 	$html .= "<td nowrap>". $trabajo->fields['glosa_actividad']."</td>";
-	 	$html .= "<td>". $dur."</td>";
-	 	$html .= "<td>". $dur_cob ."</td>";
-	 	$html .= "<td align=center>";
-		$html .= $trabajo->fields[cobrable] == 1 ? "SI" : "NO" ;
-		$html .= "</td>";
-	 	$html .= "<td>".$trabajo->Estado()."</td>";
-	 	$html .= "<td><a href=editar_trabajo.php?id_cobro=$id_cobro&id_trabajo=".$trabajo->fields[id_trabajo]."><img src=$img_dir/editar_on.gif border=0></td>";
-	 	$html .= "</tr>";
-	 	$html .= "<tr bgcolor=$color style=\"border-right: 1px solid #409C0B; border-left: 1px solid #409C0B; border-bottom: 1px solid #409C0B; \">";
-	 	$html .= "<td><strong>Desc.</strong></td><td colspan=4>".$trabajo->fields['descripcion']."</td>";
-	 	$html .= "<td colspan=2><strong>Profesional.</strong><br>".substr($trabajo->fields[nombre],0,1).". ".$trabajo->fields[apellido1]."</td>";
-	 	$html .= "<td colspan=2><strong>Tarifa</strong><br>".$trabajo->fields[id_moneda]." ".$total."</td>";
-		$html .= "</tr>";
-		$i++;
-		return $html;
-	}
+    $img_dir = Conf::ImgDir();
+    $tarifa = Funciones::TramiteTarifa($sesion, $trabajo->fields['id_tramite_tipo'],$trabajo->fields['id_moneda_asunto'],$trabajo->fields['codigo_asunto']);
+	
+	list($h,$m,$s) = split(":",$trabajo->fields['duracion_cobrada']);
+	
+	$duracion = $h + ($m > 0 ? ($m / 60) :'0');
+    $total = round($tarifa, 2);
+    $dur_cob = "$h:$m";
+	
+	list($h,$m,$s) = split(":",$trabajo->fields['duracion']); 
+    
+	$dur = "$h:$m";
+	$formato_fecha = "%d/%m/%y";
+	$fecha = Utiles::sql2fecha($trabajo->fields[fecha],$formato_fecha);
+	
+	$html .= "<tr bgcolor=$color style=\"border-right: 1px solid #409C0B; border-left: 1px solid #409C0B;\">";
+	$html .= "<td colspan=9><strong>".$glosa_tramite."</strong></td></tr>";
+ 	$html .= "<tr bgcolor=$color style=\"border-right: 1px solid #409C0B; border-left: 1px solid #409C0B;\">";
+ 	$html .= "<td>$fecha</td>";
+ 	$html .= "<td>".$trabajo->fields[glosa_cliente]."</td>";
+ 	$html .= "<td nowrap>".  $trabajo->fields['glosa_asunto'] . "</td>";
+ 	$html .= "<td nowrap>". $trabajo->fields['glosa_actividad']."</td>";
+ 	$html .= "<td>". $dur."</td>";
+ 	$html .= "<td>". $dur_cob ."</td>";
+ 	$html .= "<td align=center>";
+	$html .= $trabajo->fields[cobrable] == 1 ? "SI" : "NO" ;
+	$html .= "</td>";
+ 	$html .= "<td>".$trabajo->Estado()."</td>";
+ 	$html .= "<td><a href=editar_trabajo.php?id_cobro=$id_cobro&id_trabajo=".$trabajo->fields[id_trabajo]."><img src=$img_dir/editar_on.gif border=0></td>";
+ 	$html .= "</tr>";
+ 	$html .= "<tr bgcolor=$color style=\"border-right: 1px solid #409C0B; border-left: 1px solid #409C0B; border-bottom: 1px solid #409C0B; \">";
+ 	$html .= "<td><strong>Desc.</strong></td><td colspan=4>".$trabajo->fields['descripcion']."</td>";
+ 	$html .= "<td colspan=2><strong>Profesional.</strong><br>".substr($trabajo->fields[nombre],0,1).". ".$trabajo->fields[apellido1]."</td>";
+ 	$html .= "<td colspan=2><strong>Tarifa</strong><br>".$trabajo->fields[id_moneda]." ".$total."</td>";
+	$html .= "</tr>";
+	$i++;
+	return $html;
+}
 
