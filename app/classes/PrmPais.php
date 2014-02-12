fa5a5bf77591efd87f7a9a8ac81dfcbab6ade591
claudio Peralta <cperalta@lemontech.cl>
2014-02-12 12:16:24 -0300

    formato código, elimina comentarios.

--- app/classes/PrmPais.php
+++ app/classes/PrmPais.php
@@ -1,17 +1,14 @@
 <?php
-require_once dirname(__FILE__).'/../conf.php';
-require_once Conf::ServerDir().'/../fw/classes/Objeto.php';
-require_once Conf::ServerDir().'/../app/classes/Debug.php';
 
-class PrmPais extends Objeto
-{
+require_once dirname(__FILE__) . '/../conf.php';
+
+class PrmPais extends Objeto {
 
 	public static $llave_carga_masiva = 'nombre';
 
-	function PrmPais($sesion, $fields = "", $params = "")
-	{
-		$this->tabla = "prm_pais";
-		$this->campo_id = "id_pais";
+	function PrmPais($sesion, $fields = '', $params = '') {
+		$this->tabla = 'prm_pais';
+		$this->campo_id = 'id_pais';
 		$this->sesion = $sesion;
 		$this->fields = $fields;
 	}
