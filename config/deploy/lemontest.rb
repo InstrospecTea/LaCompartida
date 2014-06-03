05b3a53aa723936f3c52fabf7f6ce47a5d5da45a
Claudio Peralta <cperalta@deployvm.lemontech.cl>
2014-06-02 20:33:26 -0400

    mejora el manejo de cache-copy, agilizando el deploy de un nuevo branch

--- config/deploy/lemontest.rb
+++ config/deploy/lemontest.rb
@@ -17,8 +17,11 @@ set :deploy_to, "#{base_directory}/#{file_path}"
 
 
 namespace :deploy do
-
-
+ 
+  task:init_cache_dir do
+    run "if [[ ! -d #{deploy_to}/shared ]; then mkdir -p #{deploy_to}/shared; cp -r /var/www/html/deploy/time_tracking/cached-copy/ #{deploy_to}/shared/cached-copy; fi"
+    #run "cp -r /var/www/html/deploy/time_tracking/cached-copy/ #{deploy_to}/shared/cached-copy"
+  end
 
   task :update_symlinks do
     update_symlinks(self)
@@ -37,56 +40,48 @@ namespace :deploy do
   end
 
   task :lemontest_symlink do
-      subdominio = 'lemontest'
-      dominio = 'thetimebilling.com'
-      subdir="#{release_name}"
-      dirname=branch.tr('/','_')
-      absolute_path = base_directory << '/' << file_path << '/current/'
-      symlink_path = '/var/www/virtual/lemontest.thetimebilling.com/htdocs/' << dirname
-      #p absolute_path
-     #p symlink_path
-
+    subdominio = 'lemontest'
+    dominio = 'thetimebilling.com'
+    subdir="#{release_name}"
+    dirname=branch.tr('/','_')
+    absolute_path = base_directory << '/' << file_path << '/current/'
+    symlink_path = '/var/www/virtual/lemontest.thetimebilling.com/htdocs/' << dirname
     run " ln -nsf #{absolute_path} #{symlink_path}"
-       puts "\n\n\n\e[00;32m ====  SUCCESS: \e[0;37m deployed test environment on \e[04;36mhttp://lemontest.thetimebilling.com/#{dirname}\e[00;32m ====\e[0;37m\n\n\n"
-
+    puts "\n\n\n\e[00;32m ====  SUCCESS: \e[0;37m deployed test environment on \e[04;36mhttp://lemontest.thetimebilling.com/#{dirname}\e[00;32m ====\e[0;37m\n\n\n"
   end
 
-task :create_database do
-     dirname=branch.tr('/','_')
-       dbname='lemontest_' << dirname.tr('_','').tr('.','')
+  task :create_database do
+    dirname=branch.tr('/','_')
+    dbname='lemontest_' << dirname.tr('_','').tr('.','')
 
-      existedb = `mysql -h192.168.1.24 -uroot -pasdwsx -e "show databases like '%dbname%'";`
+    existedb = `mysql -h192.168.1.24 -uroot -pasdwsx -e "show databases like '#{dbname}'";`
 
-      if(existedb=='')
-        puts "\n\e[0;31m   ##########################################################################"
-        puts           "   #    No existe la base \e[01;37m #{dbname}\e[0;31m, desea crearla? (y/N) #"
-        puts           "   #########################################################################\e[0m\n"
-        proceed = STDIN.gets[0..0] rescue nil
-        if (proceed == 'y' || proceed == 'Y'  || proceed == 's' || proceed == 'S'  )
-          run "  mysql -h192.168.1.24 -uroot -pasdwsx -e 'CREATE DATABASE IF NOT EXISTS `#{dbname}`' && mysqldump -uroot -pasdwsx  -h192.168.1.24 --opt  lemontest_molde | mysql -uroot -pasdwsx -h192.168.1.24  #{dbname}"
-        end
-
-      else
-          puts "\n\e[0;32m   ############# NO NECESITO CREAR LA BASE \e[01;37m #{dbname}\e[0;32m (PORQUE YA EXISTE) ################\n\n"
-          puts "       __                                  _                 _"
-          puts "      (_ )                              __( )_              ( )     "
-          puts "       | |  ___    ___ ___    ___    ___\\__  _\\ ___    ___  | |__  "
-          puts "       | | / __\\  / _ ` _ \\  / _ \\  / _ \\ | |  / __\\  / ___\\| _  \\ "
-          puts "       | |(  ___|| ( ) ( ) |( (_) )| ( ) || |_(  ___|( (___ | | | | "
-          puts "      (___)\\____)(_) (_) (_) \\___/ (_) (_)\\___)\\____) \\____)(_) (_) \n \n"
-
+    if(existedb=='')
+      puts "\n\e[0;31m   ##########################################################################"
+      puts           "   #    No existe la base \e[01;37m #{dbname}\e[0;31m, desea crearla? (y/N) #"
+      puts           "   #########################################################################\e[0m\n"
+      proceed = STDIN.gets[0..0] rescue nil
+      if (proceed == 'y' || proceed == 'Y'  || proceed == 's' || proceed == 'S'  )
+        run "  mysql -h192.168.1.24 -uroot -pasdwsx -e 'CREATE DATABASE IF NOT EXISTS `#{dbname}`' && mysqldump -uroot -pasdwsx  -h192.168.1.24 --opt  lemontest_molde | mysql -uroot -pasdwsx -h192.168.1.24  #{dbname}"
       end
 
+    else
+      puts "\n\e[0;32m   ############# NO NECESITO CREAR LA BASE \e[01;37m #{dbname}\e[0;32m (PORQUE YA EXISTE) ################\n\n"
+      puts '       __                                  _                 _     '
+      puts '      (_ )                              __( )_              ( )    '
+      puts '       | |  ___    ___ ___    ___    ___\__  _\ ___    ___  | |__  '
+      puts '       | | / __\  / _ ` _ \  / _ \  / _ \ | |  / __\  / ___\| _  \ '
+      puts '       | |(  ___|| ( ) ( ) |( (_) )| ( ) || |_(  ___|( (___ | | | |'
+      puts '      (___)\____)(_) (_) (_) \___/ (_) (_)\___)\____) \____)(_) (_)'
+      puts '' 
+      puts ''
+    end
+  end
 
-end
-    before "deploy:update_code", "deploy:setup"
+  before "deploy:update_code", "deploy:setup"
+  before "deploy:update_code", "deploy:init_cache_dir"
 
   after "deploy:update", "deploy:cleanup"
   after "deploy:cleanup", "deploy:create_database"
-   after "deploy:cleanup", "deploy:lemontest_symlink"
-
-
-
+  after "deploy:cleanup", "deploy:lemontest_symlink"
 end
-
-
