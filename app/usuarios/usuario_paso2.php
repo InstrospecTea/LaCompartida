befda3f60dba024e311ac54596a87352b36434bc
arielrosver <arielrosver@ARV-T430.(none)>
2014-06-25 10:39:22 -0400

    gonzalicosas2

--- app/usuarios/usuario_paso2.php
+++ app/usuarios/usuario_paso2.php
@@ -840,8 +840,8 @@ function CargarPermisos() {
 		$usuario->PermisoALL();
 
 		$error_cupo = "Estimado {$sesion->usuario->fields['nombre']} {$sesion->usuario->fields['apellido1']}, usted ha excedido el cupo de usuarios contratados en el sistema. A continuacion se detalla su cupo actual.<br><br>" .
-			"* Usuarios activos con perfil 'Profesional': {$UsuarioPermiso->cupo_profesionales}<br>".
-			"* Usuarios activos con perfil 'Administrativos': {$UsuarioPermiso->cupo_administrativos}<br><br>" .
+			"* Usuarios activos con perfil <b>Profesional</b>: {$UsuarioPermiso->cupo_profesionales}<br>".
+			"* Usuarios activos con perfil <b>Administrativos</b>': {$UsuarioPermiso->cupo_administrativos}<br><br>" .
 			"Si desea aumentar su cupo debe contactarse con {$mailto} o en su defecto puede desactivar usuarios para habilitar cupos.";
 
 		// lista actual de permisos sin considerar ALL
