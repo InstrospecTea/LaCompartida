05359f5b1ac3c9b3755a947f392c72814f674fc1
Claudio Briones <brionescl@gmail.com>
2014-07-24 19:10:19 -0400

    corregir js y formato

--- app/interfaces/agregar_actividades.php
+++ app/interfaces/agregar_actividades.php
@@ -3,12 +3,9 @@ require_once dirname(__FILE__) . '/../conf.php';
 
 $Sesion = new Sesion(array('DAT'));
 $Pagina = new Pagina($Sesion);
-
 $Actividad = new Actividad($Sesion);
 
-
 if ($opcion == 'guardar') {
-
 	if (Conf::GetConf($Sesion, 'CodigoSecundario')) {
 		$asunto = new Asunto($Sesion);
 		$codigo_asunto = $asunto->CodigoSecundarioACodigo($codigo_asunto_secundario);
@@ -18,9 +15,9 @@ if ($opcion == 'guardar') {
 	$Actividad->Fill($_REQUEST, true);
 
 	if ($_REQUEST['activo'] == 1) {
-		$Actividad->Edit("activo", "1");
+		$Actividad->Edit('activo', '1');
 	} else {
-		$Actividad->Edit("activo", "0");
+		$Actividad->Edit('activo', '0');
 	}
 
 	if (empty($Actividad->fields['codigo_asunto'])){
@@ -31,15 +28,13 @@ if ($opcion == 'guardar') {
 
 	if ($Actividad->Write()) {
 		$Pagina->AddInfo(__('Actividad guardada con éxito'));
-		if($Actividad->fields['codigo_asunto'] == 'NULL'){
+		if ($Actividad->fields['codigo_asunto'] == 'NULL') {
 			$Actividad->Edit('codigo_asunto', null);
 		}
-
 	} else {
 		$Pagina->AddError(__('Por favor corrija lo siguiente: ') . implode(', ', $Actividad->error));
 	}
 } else {
-
 	if ($id_actividad != '') {
 		$Actividad->Load($id_actividad);
 	} else {
@@ -73,14 +68,14 @@ if ($Actividad->Loaded()) {
 $Pagina->PrintTop($popup);
 $Form = new Form;
 ?>
-
 <script type="text/javascript">
-
 	jQuery(document).ready(function() {
+		var glosa_actividad = jQuery('#glosa_actividad').val();
+
 		if (window.opener !== undefined && window.opener.Refrescar) {
 			window.opener.Refrescar();
 		}
-		var glosa_actividad = jQuery('#glosa_actividad').val();
+
 		if (!glosa_actividad){
 			jQuery('#td_check_activo').hide();
 			jQuery('#td_text_activo').hide();
@@ -89,36 +84,37 @@ $Form = new Form;
 			jQuery('#td_text_activo').show();
 		}
 
-		jQuery('#form_actividades').submit(function() {
-			return Validar();
+		jQuery('#form_actividades').submit(function(e) {
+			if (!Validar()) {
+				e.preventDefault();
+			}
 		});
-
 	});
 
 	function Validar() {
 		if (jQuery('#codigo_actividad').val() == '') {
 			alert('Debe ingresar un código.');
-			document.getElementById('codigo_actividad').focus();
+			jQuery('#codigo_actividad').focus();
 			return false;
 		}
+
 		if (jQuery('#glosa_actividad').val() == '') {
 			alert('Debe ingresar un título.');
-			document.getElementById('glosa_actividad').focus();
+			jQuery('#glosa_actividad').focus();
 			return false;
 		}
+
 		if (jQuery('#codigo_cliente').val() && !jQuery('#codigo_asunto').val()) {
 			alert('Si selecciona un cliente debe seleccionar un asunto.');
 			return false;
 		}
+
 		if (jQuery('#codigo_cliente_secundario').size() && jQuery('#codigo_asunto_secundario').size()) {
 			if (jQuery('#codigo_cliente_secundario').val() && !jQuery('#codigo_asunto_secundario').val()) {
 				alert('Si selecciona un cliente debe seleccionar un asunto.');
 				return false;
 			}
 		}
-
-
-		document.getElementById('form_actividades').submit();
 
 		return true;
 	}
@@ -165,11 +161,11 @@ $Form = new Form;
 			</tr>
 			<?php if ($Actividad->Loaded()) { ?>
 				<tr>
-					<td><?php echo __('Activa'); ?></td>
-					<td>
+					<td><label for="activo"><?php echo __('Activa'); ?></label></td>
+					<td style="text-align:left">
 						<?php
 						$activo = ($Actividad->fields['activo'] == 1);
-						echo $Form->checkbox('activo', 1, $activo, array('id' => 'activo', 'onclick' => 'CheckActivo();', 'title' => 'Al inactivar no sera listada en ingreso de horas', 'label' => false));
+						echo $Form->checkbox('activo', 1, $activo, array('id' => 'activo', 'title' => 'Al inactivar no sera listada en ingreso de horas', 'label' => false));
 						?>
 					</td>
 				</tr>
