ef072e80b02ed6c4d8275d388c8bc20833bb08b0
Claudio Peralta <cuayocps@gmail.com>
2014-07-24 12:44:16 -0400

    Formato código.. Elimina querys CREATE TABLE

--- app/ajax.php
+++ app/ajax.php
@@ -1,15 +1,8 @@
 <?php
 
 require_once 'conf.php';
-require_once Conf::ServerDir() . '/../fw/classes/Sesion.php';
-require_once Conf::ServerDir() . '/../fw/classes/Pagina.php';
-require_once Conf::ServerDir() . '/../fw/classes/Utiles.php';
-require_once Conf::ServerDir() . '/../fw/classes/Html.php';
-require_once Conf::ServerDir() . '/../app/classes/UtilesApp.php';
 
 ini_set('display_errors', 'On');
-
-
 
 $sesion = new Sesion('');
 $pagina = new Pagina($sesion);
@@ -24,14 +17,6 @@ switch ($accion) {
 		$maxid = $_POST['cantidad'];
 		$orden = 0;
 		$query = array();
-		$query[] = "CREATE TABLE IF NOT EXISTS `prm_lang` (
-						`id_lang` smallint(3) NOT NULL AUTO_INCREMENT,
-						`archivo_nombre` varchar(100) COLLATE latin1_spanish_ci NOT NULL DEFAULT 'archivo.php' COMMENT 'relativo al path app/lang',
-						`orden` smallint(3) NOT NULL DEFAULT '1',
-						`activo` tinyint(1) NOT NULL,
-						PRIMARY KEY (`id_lang`),
-						UNIQUE KEY `archivo_nombre` (`archivo_nombre`)
-						) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;";
 		$archivos = array();
 		foreach ($_POST as $llave => $datorecibido) {
 			if (strpos($llave, '_php')) {
@@ -71,20 +56,11 @@ switch ($accion) {
 
 		break;
 
-
 	case 'actualiza_plugins':
 
 		$maxid = $_POST['cantidad'];
 		$orden = 0;
 		$query = array();
-		$query[] = "CREATE TABLE IF NOT EXISTS `prm_plugin` (
-						`id_plugin` smallint(3) NOT NULL AUTO_INCREMENT,
-						`archivo_nombre` varchar(100) COLLATE latin1_spanish_ci NOT NULL DEFAULT 'plugin.php' ,
-						`orden` smallint(3) NOT NULL DEFAULT '1',
-						`activo` tinyint(1) NOT NULL,
-						PRIMARY KEY (`id_plugin`),
-						UNIQUE KEY `archivo_nombre` (`archivo_nombre`)
-						) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;";
 		$archivos = array();
 		foreach ($_POST as $llave => $datorecibido):
 			if (strpos($llave, '_php')) {
@@ -94,9 +70,9 @@ switch ($accion) {
 			}
 		endforeach;
 		$query[] = "update prm_plugin set orden=$maxid, activo=0 where archivo_nombre not in ('" . implode("','", $archivos) . "');";
-		foreach ($query as $q)
+		foreach ($query as $q) {
 			mysql_query($q, $sesion->dbh);
-
+		}
 
 		$queryrespuesta = 'select * from prm_plugin order by orden ASC';
 		$resultado = mysql_query($queryrespuesta, $sesion->dbh);
@@ -114,8 +90,9 @@ switch ($accion) {
 		$myDirectory = opendir(Conf::ServerDir() . '/lang/');
 
 		while ($entryName = readdir($myDirectory)) {
-			if (!array_key_exists($entryName, $archivos) && is_file(Conf::ServerDir() . '/plugins/' . $entryName))
+			if (!array_key_exists($entryName, $archivos) && is_file(Conf::ServerDir() . '/plugins/' . $entryName)) {
 				echo '<li > <input type="checkbox" class="checkbox"  id="' . ++$maxid . '_' . $entryName . '" name="' . $entryName . '" value="1" /><label for="' . $maxid . '_' . $entryName . '">' . $entryName . '</label> <span class="ui-icon ui-icon-arrowthick-2-n-s"></span></li>';
+			}
 		}
 
 		closedir($myDirectory);
@@ -123,10 +100,6 @@ switch ($accion) {
 		break;
 
 	case 'actualiza_beacon':
-		$ExisteIndex = mysql_query("SHOW INDEX FROM `configuracion_categoria` WHERE `key_name` = 'glosa_configuracion_categoria'", $sesion->dbh);
-		if (!$ExisteIndex) {
-			mysql_query("ALTER TABLE `configuracion_categoria` ADD UNIQUE (`glosa_configuracion_categoria`);", $sesion->dbh);
-		}
 		mysql_query("INSERT IGNORE INTO `configuracion_categoria` (`glosa_configuracion_categoria`) VALUES ('Plugins - Hooks');", $sesion->dbh);
 		$resp = mysql_query("select id_configuracion_categoria from configuracion_categoria where glosa_configuracion_categoria='Plugins - Hooks'", $sesion->dbh);
 		list($cat_id) = mysql_fetch_row($resp);
@@ -139,7 +112,7 @@ switch ($accion) {
 
 	case 'busca_encargado_por_cliente':
 
-		$campoacomparar = UtilesApp::GetConf($sesion, 'CodigoSecundario') ? 'codigo_cliente_secundario' : 'codigo_cliente';
+		$campoacomparar = Conf::GetConf($sesion, 'CodigoSecundario') ? 'codigo_cliente_secundario' : 'codigo_cliente';
 		$codigobuscado = $_POST['codigobuscado'];
 		$query = "select  contrato.id_usuario_responsable, contrato.id_usuario_secundario, CONCAT_WS(' ',usuario.apellido1,usuario.apellido2,',',usuario.nombre) nombre_usuario_secundario from cliente join contrato using (id_contrato) left join usuario on usuario.id_usuario=contrato.id_usuario_secundario where cliente.$campoacomparar='$codigobuscado'";
 		if ($resp = mysql_query($query, $sesion->dbh)) {
@@ -151,6 +124,7 @@ switch ($accion) {
 		echo $responsable . '|' . $encargadosecundario . '|' . $nombre_usuario_secundario;
 
 		break;
+
 	case "borrar_evento":
 
 		$query = "DELETE FROM events WHERE event_id=" . $id;
@@ -158,6 +132,7 @@ switch ($accion) {
 
 		echo(true);
 		break;
+
 	case "actualizar_evento":
 
 		list($dia_semana, $mes_letra, $dia, $anio, $hora) = explode(' ', $start_date);
@@ -187,6 +162,7 @@ switch ($accion) {
 			case 'Dec': $mes = '12';
 				break;
 		}
+
 		$start_date = $anio . '-' . $mes . '-' . $dia . ' ' . $hora;
 
 		list($dia_semana, $mes_letra, $dia, $anio, $hora) = explode(' ', $end_date);
@@ -216,6 +192,7 @@ switch ($accion) {
 			case 'Dec': $mes = '12';
 				break;
 		}
+
 		$end_date = $anio . '-' . $mes . '-' . $dia . ' ' . $hora;
 
 		$query = "UPDATE events SET event_name='" . $event_text . "', start_date='" . $start_date . "', end_date='" . $end_date . "' WHERE event_id=" . $id;
@@ -223,6 +200,7 @@ switch ($accion) {
 
 		echo(true);
 		break;
+
 	case "agregar_evento":
 
 		list($dia_semana, $mes_letra, $dia, $anio, $hora) = explode(' ', $start_date);
@@ -252,6 +230,7 @@ switch ($accion) {
 			case 'Dec': $mes = '12';
 				break;
 		}
+
 		$start_date = $anio . '-' . $mes . '-' . $dia . ' ' . $hora;
 
 		list($dia_semana, $mes_letra, $dia, $anio, $hora) = explode(' ', $end_date);
@@ -281,6 +260,7 @@ switch ($accion) {
 			case 'Dec': $mes = '12';
 				break;
 		}
+
 		$end_date = $anio . '-' . $mes . '-' . $dia . ' ' . $hora;
 
 		$query = "INSERT INTO events( event_name, start_date, end_date, details ) VALUES('" . $event_text . "','" . $start_date . "','" . $end_date . "', 'Trabajo')";
@@ -288,9 +268,10 @@ switch ($accion) {
 
 		echo(true);
 		break;
+
 	case "actualizacion_usuario":
 
-		if (method_exists('Conf', 'ColumnaNotificacion') || ( method_exists('Conf', 'GetConf') && Conf::GetConf($sesion, 'ColumnaNotificacion') != '' )) {
+		if (Conf::GetConf($sesion, 'ColumnaNotificacion') != '') {
 			if (!$id_not) {
 				$id_not = 0;
 			}
@@ -300,13 +281,14 @@ switch ($accion) {
 			$query = "UPDATE usuario SET id_notificacion_tt=" . $id_not . "+" . $corr . " WHERE id_usuario='$id' AND id_notificacion_tt < " . $id_not . "+" . $corr;
 			$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
 			echo(true);
-		}
-		else
+		} else {
 			echo(false);
+		}
 		break;
+
 	case "cargar_cliente":
 
-		if (( method_exists('Conf', 'CodigoSecundario') && Conf::CodigoSecundario() ) || ( method_exists('Conf', 'GetConf') && Conf::GetConf($sesion, 'CodigoSecundario') )) {
+		if (Conf::GetConf($sesion, 'CodigoSecundario')) {
 			$codigo_asunto = 'asunto.codigo_asunto_secundario';
 			$codigo_cliente = 'cliente.codigo_cliente_secundario';
 			$join = 'LEFT JOIN asunto ON cliente.codigo_cliente=asunto.codigo_cliente';
@@ -319,7 +301,8 @@ switch ($accion) {
 									FROM cliente $join";
 		if ($id != "") {
 			$query .= "WHERE cliente.activo=1 AND $codigo_asunto = '$id' ";   // eleciona solo el cliente correspondente al id del asunto
-		}$query .= "ORDER BY glosa_cliente";
+		}
+		$query .= "ORDER BY glosa_cliente";
 
 		$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
 
@@ -333,6 +316,7 @@ switch ($accion) {
 			echo("VACIO|");
 		}
 		break;
+
 	case "cargar_asuntos":
 	case "cargar_asuntos_desde_campo":
 
@@ -350,12 +334,12 @@ switch ($accion) {
     								WHERE codigo_asunto_secundario = '$id' ";
 			$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
 			list( $id ) = mysql_fetch_array($resp);
-			if (!$id)
+			if (!$id) {
 				$vacio = true;
-			else
+			} else {
 				$vacio = false;
-		}
-		elseif ($accion == "cargar_asuntos_desde_campo") {
+			}
+		} elseif ($accion == "cargar_asuntos_desde_campo") {
 			$query = "SELECT codigo_cliente FROM asunto WHERE codigo_asunto = '$id' ";
 			$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
 			list( $id ) = mysql_fetch_array($resp);
@@ -366,7 +350,7 @@ switch ($accion) {
 			}
 		}
 
-		if (( method_exists('Conf', 'CodigoSecundario') && Conf::CodigoSecundario() ) || ( method_exists('Conf', 'GetConf') && Conf::GetConf($sesion, 'CodigoSecundario') )) {
+		if (Conf::GetConf($sesion, 'CodigoSecundario')) {
 			$codigo_asunto = 'asunto.codigo_asunto_secundario';
 			$codigo_cliente = 'cliente.codigo_cliente_secundario';
 			$join = 'LEFT JOIN cliente ON cliente.codigo_cliente=asunto.codigo_cliente ';
@@ -386,7 +370,6 @@ switch ($accion) {
 		}
 		$query .= " ORDER BY glosa_asunto ";
 
-
 		$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
 
 		for ($i = 0; $fila = mysql_fetch_assoc($resp); $i++) {
@@ -402,9 +385,10 @@ switch ($accion) {
 			echo("VACIO|");
 		}
 		break;
+
 	case 'averiguar_codigo_cliente':
 
-		if (UtilesApp::GetConf($sesion, 'CodigoSecundario')) {
+		if (Conf::GetConf($sesion, 'CodigoSecundario')) {
 			// asumo que recibí un codigo asunto secundario (más vale)
 
 			$cliente = $sesion->pdodbh->query("SELECT cliente.codigo_cliente_secundario  FROM cliente
@@ -429,37 +413,6 @@ switch ($accion) {
 		}
 		break;
 
-	case 'veriguar_codigo_cliente':
-
-		if (UtilesApp::GetConf($sesion, 'CodigoSecundario')) {
-			// asumo que recibí un codigo asunto secundario (más vale)
-			$query = "SELECT codigo_cliente_secundario FROM cliente
-								JOIN asunto ON cliente.codigo_cliente=asunto.codigo_cliente
-								WHERE asunto.codigo_asunto_secundario='$id'";
-			$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
-			list( $codigo_cliente_secundario ) = mysql_fetch_array($resp);
-
-			if ($codigo_cliente_secundario != '') {
-				$query = "SELECT count(*) FROM cliente WHERE codigo_cliente_secundario='$codigo_cliente_secundario'";
-				$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
-				list($cont) = mysql_fetch_array($resp);
-			} else {
-				$cont = 0;
-			}
-
-			if ($cont > 0) {
-				echo $codigo_cliente_secundario;
-			} else {
-				echo false;
-			}
-		} else {
-			$query = "SELECT count(*) FROM cliente WHERE codigo_cliente='$id'";
-			$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
-			list($cont) = mysql_fetch_array($resp);
-
-			echo $cont;
-		}
-		break;
 	case 'cambiar_tarifa_usuario':
 
 		$query2 = "SELECT tarifa,id_tarifa,id_moneda FROM categoria_tarifa
@@ -472,6 +425,7 @@ switch ($accion) {
 		}
 		echo("OK");
 		break;
+
 	case "cargar_actividades":
 		$query = "SELECT codigo_actividad,glosa_actividad FROM actividad WHERE codigo_asunto = '$id' OR codigo_asunto IS NULL ORDER BY glosa_actividad";
 		$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
@@ -506,6 +460,7 @@ switch ($accion) {
 		}
 
 		break;
+
 	case 'cargar_cargos':
 
 		$query_clientes = "SELECT contrato.factura_razon_social, contrato.factura_direccion, contrato.rut FROM cobro
@@ -513,10 +468,8 @@ switch ($accion) {
 												JOIN contrato ON cliente.id_contrato = contrato.id_contrato
 												WHERE cliente.codigo_cliente=$id AND cobro.documento IS NULL LIMIT 1";
 
-
 		$query = "SELECT * FROM cobro WHERE documento IS NULL AND codigo_cliente = '$id'";
 		$resp = mysql_query($query_clientes, $sesion->dbh) or Utiles::errorSQL($query_clientes, __FILE__, __LINE__, $sesion->dbh);
-
 
 		for ($i = 0; $fila = mysql_fetch_assoc($resp); $i++) {
 			if ($i > 0) {
@@ -529,9 +482,10 @@ switch ($accion) {
 		}
 
 		break;
+
 	case "cargar_glosa_cliente":
 
-		if (( method_exists('Conf', 'GetConf') && Conf::GetConf($sesion, 'CodigoSecundario') ) || ( method_exists('Conf', 'CodigoSecundario') && Conf::CodigoSecundario() )) {
+		if (Conf::GetConf($sesion, 'CodigoSecundario')) {
 			if ($id_asunto != '') {
 				$query = "SELECT codigo_cliente_secundario FROM cliente
 									JOIN asunto ON cliente.codigo_cliente=asunto.codigo_cliente
@@ -591,17 +545,20 @@ switch ($accion) {
 			echo("VACIO|");
 		}
 		break;
+
 	case 'cargar_contratos':
 		require_once Conf::ServerDir() . '/classes/Contrato.php';
 		$contrato = new Contrato($sesion);
 		echo $contrato->ListaSelector($codigo_cliente, 'CargarTabla(1);');
 		break;
+
 	case 'evaluacion':
 		// sí existe la ventana del evaluacion después el login
 		$query = "INSERT evaluacion SET id_usuario= '$id', valuacion = '$valuacion',
 						glosa_valuacion = '$glosa_valuacion', fecha_creacion = '" . Utiles::fecha2sql(date()) . "'";
 		$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
 		break;
+
 	default:
 		echo("ERROR");
 };
