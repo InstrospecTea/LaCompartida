a17ae25af595e017ff12bc898e8d10ec950a6a0d
Daniel Ochoa <dochoaj@gmail.com>
2014-07-29 18:43:11 -0400

    agrega condicion fundamental en la query, que seguramente fue la razon de ser de este reporte, que considera solamente a los cobros pendientes que son hitos

--- app/interfaces/planillas/planilla_reporte_hitos.php
+++ app/interfaces/planillas/planilla_reporte_hitos.php
@@ -68,7 +68,8 @@ if ($xls) {
 		->add_left_join_with('prm_moneda AS pmcob', CriteriaRestriction::equals('cob.id_moneda', 'pmcob.id_moneda'))
 		->add_left_join_with('asunto AS asu', CriteriaRestriction::equals('con.id_contrato', 'asu.id_contrato'))
 		->add_left_join_with('cliente AS cli', CriteriaRestriction::equals('con.codigo_cliente', 'cli.codigo_cliente'))
-		->add_grouping('cli.codigo_cliente');
+		->add_grouping('cli.codigo_cliente')
+		->add_restriction(CriteriaRestriction::equals('cp.hito', '1'));
 
 	$resultado = $criteria->run();
 
