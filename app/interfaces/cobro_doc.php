1bfe1ba39619b90747c18a42b9c2fea9900d442e
arielrosver <arielrosver@ARV-T430.(none)>
2014-08-07 17:52:36 -0400

    elimina elementos debug

--- app/interfaces/cobro_doc.php
+++ app/interfaces/cobro_doc.php
@@ -90,7 +90,7 @@ if (empty($orientacion_papel) || !in_array($orientacion_papel, array('PORTRAIT',
 }
 
 $doc = new DocGenerator($html, $cssData, $cobro->fields['opc_papel'], $cobro->fields['opc_ver_numpag'], $orientacion_papel, $docm_top, $docm_right, $docm_bottom, $docm_left, $cobro->fields['estado'], $id_formato, '', $docm_header, $docm_footer, $lang, $Sesion);
-// $valor_unico = substr(time(), -3);
+$valor_unico = substr(time(), -3);
 
 // echo '<style>'.$cssData.'</style>'.$html; exit;
 
