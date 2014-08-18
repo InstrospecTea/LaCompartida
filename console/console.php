b63033b5dc62a49670b7e809e8ce5936d40f201d
Claudio Briones <brionescl@gmail.com>
2014-08-18 15:54:34 -0400

    corregir asignación de url

--- console/console.php
+++ console/console.php
@@ -1,11 +1,16 @@
 <?php
-$args = getopt('', array('script:', 'domain:', 'debug'));
+$args = getopt('', array('script:', 'domain:', 'subdir:', 'debug'));
+
+if (!isset($args['script']) || !isset($args['domain']) || !isset($args['subdir'])) {
+	exit("use: console script_name --domain=dev --subdir=ttb [--debug]\n");
+}
 
 define('__DIR__', dirname(__FILE__));
 define('__BASEDIR__', dirname(__DIR__));
 
 $_SERVER['HTTP_HOST'] = "{$args['domain']}.local";
-$_SERVER['DOCUMENT_ROOT'] = __BASEDIR__;
+$_SERVER['DOCUMENT_ROOT'] = dirname(__BASEDIR__);
+$_SERVER['REQUEST_URI'] = "/{$args['subdir']}/";
 
 require __BASEDIR__ . '/app/conf.php';
 require __DIR__ . '/scripts/AppShell.php';
