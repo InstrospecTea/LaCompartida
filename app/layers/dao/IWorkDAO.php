8bdcbc07429ff5393b1a90bd1910a12200b81997
Claudio Peralta <cuayocps@gmail.com>
2014-08-25 12:47:29 -0400

    Corrige comentario.

--- app/layers/dao/IWorkDAO.php
+++ app/layers/dao/IWorkDAO.php
@@ -2,8 +2,8 @@
 
 
 /**
- * Interface CobroDAO
- * Interfaz que define los servicios que debe prestar la capa DAO para la entidad {@link Charge}.
+ * Interface WorkDAO
+ * Interfaz que define los servicios que debe prestar la capa DAO para la entidad {@link Work}.
  */
 interface IWorkDAO extends BaseDAO{
 
