11035df07ef316001633cf833961b2f95113c197
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2014-09-11 11:18:57 -0300

    Agrega documentación sobre decisión de operación binaria

--- app/classes/ReporteAntiguedadDeudas.php
+++ app/classes/ReporteAntiguedadDeudas.php
@@ -782,6 +782,12 @@ class ReporteAntiguedadDeudas
 
 		if (!empty($this->datos['tipo_liquidacion'])) {
 			$tipo_liquidacion = intval($this->datos['tipo_liquidacion']);
+			# Eficiencias amebísticas:
+			# El uso del operador de comparación bit a bit & no debe ser modificado
+			# Para cada tipo de liquidación 1 (Solo honorarios), 2 (Solo Gastos) y 3 (Solo Mixtas)
+			# se debe cumplir que 1 y 2 son excluyentes; y 3 incluye a 1 y 2
+			# por lo que el operador actuá resolviendo dichas condiciones
+			# Referencia: http://php.net/manual/es/language.operators.bitwise.php			
 			$honorarios = $tipo_liquidacion & 1;
 			$gastos = $tipo_liquidacion & 2 ? 1 : 0;
 			$separar_liquidaciones = ($tipo_liquidacion == '3' ? 0 : 1);
