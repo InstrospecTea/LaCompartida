6fee790a13b61605796076ca7e65d4c720abac21
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2014-09-16 19:33:52 -0300

    Fix problem with foreign key

--- app/layers/service.support/ChargeService.php
+++ app/layers/service.support/ChargeService.php
@@ -11,11 +11,16 @@ class ChargeService extends AbstractService implements IChargeService {
 	public function saveOrUpdate($charge) {
 		$estado = $charge->get('estado');
 		$modalidad_calculo = $charge->get('modalidad_calculo');
+		$id_doc_pago_honorarios = $charge->get('id_doc_pago_honorarios');
 
 		if (empty($estado)) {
 			$charge->set('estado', 'CREADO');
 		}
 		
+		if (empty($id_doc_pago_honorarios)) {
+			$charge->set('id_doc_pago_honorarios', 'NULL');
+		}
+
 		if (empty($modalidad_calculo)) {
 			$charge->set('modalidad_calculo', '0');
 		} 
