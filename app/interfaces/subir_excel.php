9665582fabed14a7a9eedf6f54d7416a21aef292
Claudio Briones <brionescl@brionescl.local>
2014-09-25 17:06:32 -0300

    agregar validación extención

--- app/interfaces/subir_excel.php
+++ app/interfaces/subir_excel.php
@@ -9,15 +9,6 @@ $pagina->titulo = __('Subir excel modificado');
 $pagina->PrintTop($popup);
 ?>
 
-<script type="text/javascript">
-	function confirmar(form) {
-		if (confirm("Esta modificando algunos trabajos con el excel,\n ¿Desea continuar?")) {
-			form.submit();
-			window.opener.Refrescar();
-		}
-	}
-</script>
-
 <table width="480">
 	<tr>
 		<td style="border-bottom: 2px solid #a3d45c;">
@@ -33,16 +24,38 @@ $pagina->PrintTop($popup);
 				<input type="hidden" name="opc" id="opc" value="subir_excel" />
 				Subir excel modificado: <input type="file" name="archivo_data" />
 				<input type="button" value="<?php echo __('Cargar Documento'); ?>" class="btn" onclick="confirmar(this.form);" />
-				<br />
-				<?php
-				if ($opc == 'subir_excel') {
-					// Esta función actualiza los trabajos y entrega feedback del resultado.
-					echo '<br /><span align="center">' . Trabajo::ActualizarConExcel($archivo_data, $sesion) . '</span>';
-				}
-				?>
 			</form>
 		</td>
 	</tr>
+	<tr>
+		<td>&nbsp;</td>
+	</tr>
+	<tr>
+		<td>
+			<span align="center">
+				<?php
+				if ($opc == 'subir_excel') {
+					// Sólo se permiten xls "%\.(xls|xlsx)$%i"
+					if (preg_match('%\.(xls)$%i', $archivo_data['name']) == 1) {
+						// Esta función actualiza los trabajos y entrega feedback del resultado.
+						echo Trabajo::ActualizarConExcel($archivo_data, $sesion);
+					} else {
+						echo '<b>La extenci&oacute;n del excel fue adjuntado es inv&aacute;lido.</b>';
+					}
+				}
+				?>
+			</span>
+		</td>
+	</tr>
 </table>
+
+<script type="text/javascript">
+	function confirmar(form) {
+		if (confirm("Esta modificando algunos trabajos con el excel,\n ¿Desea continuar?")) {
+			form.submit();
+			window.opener.Refrescar();
+		}
+	}
+</script>
 
 <?php $pagina->PrintBottom($popup);
