6e5697817eba311327fdb11d96721db2b86451b0
arielrosver <arielrosver@gmail.com>
2014-10-13 13:03:28 -0300

    interfaces changes

--- app/classes/DocManager.php
+++ app/classes/DocManager.php
@@ -2,10 +2,48 @@
 
 require_once dirname(__FILE__) . '/../conf.php';
 
-class DocManager {
+class DocManager extends Objeto {
 
-    public static function ArraySelector($array, $name, $selected = '') {
-        $select = "<select class='form-control' name='$name' id='$name' $opciones style='width: $width;'>";
+    function __construct($Sesion, $fields = '', $params = '') {
+        $this->tabla = 'carta';
+        $this->campo_id = 'id_carta';
+        $this->campo_glosa = 'descripcion';
+        $this->sesion = $Sesion;
+        $this->fields = $fields;
+    }
+
+    public function GetHtmlHeader() {
+
+        $htmlheader = '';
+        $htmlheader.= '<!DOCTYPE html>';
+        $htmlheader.= '<html lang="en">';
+        $htmlheader.= '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>';
+        $htmlheader.= '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">';
+        $htmlheader.= '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">';
+        $htmlheader.= '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>';
+//        $htmlheader.= '<script type="text/javascript" src="//static.thetimebilling.com/js/ckeditor/ckeditor.js"></script>'; <---- Habilitar CKEDITOR
+        $htmlheader.= '</head>';
+        $htmlheader.= '<body>';
+
+        return $htmlheader;
+    }
+
+    public function GetHtmlFooter() {
+
+        $html_footer = '';
+        $html_footer = '</body>';
+        $html_footer = '</html>';
+
+        return $html_footer;
+    }
+
+    public function GetFormats($sesion, $id_carta) {
+        $formatselector = Html::SelectQuery($sesion, 'SELECT id_carta, descripcion FROM carta', 'id_carta', $id_carta, 'class="form-control"', ' ', '');
+        return $formatselector;
+    }
+
+    public function ImprimirSelector($array, $name, $selected = '') {
+        $select = "<select class='form-control' name='$name' id='$name'>";
         if ($titulo == 'Vacio') {
             $select .= "<option value='-1'>&nbsp;</option>\n";
         } else if ($titulo != '') {
@@ -22,6 +60,20 @@ class DocManager {
 
         $select .= "</select>";
         return $select;
+    }
+
+    public function GetNumOfAsociatedCharges($sesion, $id_carta) {
+        $query = "SELECT count(*) FROM cobro WHERE id_carta = {$id_carta}";
+        $resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $sesion->dbh);
+
+        list($numofasociatedcharges) = mysql_fetch_array($resp);
+
+        return $numofasociatedcharges;
+    }
+
+    public function Deleteformat($session, $id_carta) {
+        $query = "DELETE FROM carta WHERE id_carta = {$id_carta}";
+        $resp = mysql_query($query, $session->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $session->dbh);
     }
 
 }
--- app/mantenedor_cartas/doc_manager.php
+++ app/mantenedor_cartas/doc_manager.php
@@ -4,21 +4,12 @@ require_once dirname(__FILE__) . '/../conf.php';
 $sesion = new Sesion(array('ADM'));
 $CartaCobro = new CartaCobro($sesion);
 $DocManager = new DocManager($sesion);
-$Form = new Form;
 
-echo '<!DOCTYPE html>';
-echo '<html lang="en">';
-echo '<head>';
+// Imprime encabezado html con librerias requeridas.
+echo $DocManager->GetHtmlHeader();
 
-// Librerias utilizadas
-echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>';
-echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">';
-echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">';
-echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>';
-
-echo '<body>';
-
-$id_carta = 1; // Utilizado para previsualizar elementos
+//$opc = 'prev';
+$id_carta = 1;
 
 if ($opc == 'guardar') {
     $id_carta = $CartaCobro->GuardarCarta($_POST['carta']);
@@ -27,133 +18,131 @@ if ($opc == 'guardar') {
     $id_carta = $CartaCobro->PrevisualizarDocumento($carta, $id_cobro);
 } else {
     $carta = $CartaCobro->ObtenerCarta($id_carta);
+    $cobros_asociados = $DocManager->GetNumOfAsociatedCharges($sesion, $id_carta);
 }
 
+// Obtiene Secciones y tags.
 $secciones = UtilesApp::mergeKeyValue($CartaCobro->secciones['CARTA']);
-
-$tags = UtilesApp::mergeKeyValue($CartaCobro->diccionario['DETALLE']);
+$tags = UtilesApp::mergeKeyValue($CartaCobro->diccionario['FECHA']);
 ?>
 
 <script type="text/javascript">
+
     jQuery(document).ready(function ($) {
         $('#tabs').tab();
+
+        // Segmento de codigo solo incremnta el largo de el plugin CKEDITOR
+        // Descomentar y definir alto.
+
+        // CKEDITOR.on('instanceReady', function () {
+        //     var textEditHeight = $(".textPanel").height();
+        //     var ckTopHeight = $("#cke_1_top").height();
+        //     var ckContentsHeight = $("#cke_1_contents").height();
+        //     for (var i = 1; i < 10; i++) {
+        //         $("#cke_" + i + "_contents").height("400px");
+        //     }
+        // });
+
     });
 </script>
 
-<!-- Controles del mantenedor -->
+<!-- Encabezado pagina -->
 
 <div class="container-fluid">
+    <div class="col-sm-4"><h4>Mantenedor de Cartas</h4></div>
+    <div class="col-sm-4"><?php echo Html::SelectQuery($sesion, 'SELECT id_carta, descripcion FROM carta', 'id_carta', $id_carta, 'class="form-control"', ' ', ''); ?></div>
+    <div class="col-sm-4"></div>
+</div>
 
-    <h3>Mantenedor de Cartas</h3>
-
+<div class="col-md-6">
     <div class="panel panel-default">
-        <form>
-            <div class="row">
-                <div class="col-md-6"> 
-                    <div class="row">
-                        <div class="col-md-12">
-                            <strong>Controles del mantenedor:</strong><br>
-                            Formato: <?php echo Html::SelectQuery($sesion, 'SELECT id_carta, descripcion FROM carta', 'id_carta', $id_carta, '', ' '); ?>
-                            <button type="button" class="btn btn-default btn-sm">Editar</button>
-                            <button type="button" class="btn btn-success btn-sm">Guardar</button>
-                            <button type="button" class="btn btn-warning btn-sm">Guardar como nuevo</button>
-                            <button type="button" class="btn btn-danger btn-sm">Eliminar</button>
-                        </div>
-                    </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <strong>Controles del mantenedor:</strong><br>
-                            <div class="col-xs-3">
-                                <input type="text" class="form-control" placeholder="Numero Cobro">
+        <div class="panel-heading">
+            <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
+                <li class="active"><a href="#html_code" data-toggle="tab">(HTML)</a></li>
+                <li><a href="#css_code" data-toggle="tab">(CSS)</a></li>
+            </ul>
+        </div>
+        <div class="panel-body">
+            <div id="content">
+                <div id="my-tab-content" class="tab-content">
+                    <div class="tab-pane active" id="html_code">
+
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-9">
+                                <?php echo $DocManager->ImprimirSelector($secciones, 'secciones'); ?>
                             </div>
-                            <button type="button" class="btn btn-default btn-sm">Descargar Word</button>
+                            <div class="col-md-1">
+                                <button type="button" class="btn btn-primary btn-sm">Insertar</button>
+                            </div>
+
                         </div>
+                        <div class="row" style="margin-bottom: 2%;">
+
+                            <div class="col-md-1"></div>
+                            <div class="col-md-9">
+                                <?php echo $DocManager->ImprimirSelector($tags, 'tags'); ?>
+                            </div>
+                            <div class="col-md-1">
+                                <button type="button" class="btn btn-primary btn-sm">Insertar</button>
+                            </div>
+
+                        </div>
+
+                        <textarea class="ckeditor" id="formato_html" name="formato_html" style="width:100%; height: 525px;"><?php echo $carta['formato']; ?></textarea>
                     </div>
-                </div>
-                <div class="col-md-6"><label>Propiedades del documento:</label>
-                    <div cla class="row">
-                        <div class="col-md-4">
-                            Nombre del formato: <input type="text" class="form-control"  name="carta[descripcion]" value="<?php echo $carta['descripcion']; ?>"/><br/>
-                        </div>
-                        <div class="col-md-3">
-                            Margen Superior: <input type="text" class="form-control"  name="carta[margen_superior]" value="<?php echo $carta['margen_superior']; ?>"/>
-                            Margen Izquierdo: <input type="text" class="form-control"  name="carta[margen_izquierdo]" value="<?php echo $carta['margen_izquierdo']; ?>"/>
-                            Margen Header: <input  type="text" class="form-control"  name="carta[margen_encabezado]" value="<?php echo $carta['margen_encabezado']; ?>"/>
-                        </div>
-                        <div class="col-md-3">
-                            Margen Inferior: <input type="text" class="form-control"  name="carta[margen_inferior]" value="<?php echo $carta['margen_inferior']; ?>"/>
-                            Margen Derecho: <input type="text" class="form-control" name="carta[margen_derecho]" value="<?php echo $carta['margen_derecho']; ?>"/>
-                            Margen Footer: <input type="text" class="form-control" name="carta[margen_pie_de_pagina]" value="<?php echo $carta['margen_pie_de_pagina']; ?>"/><br/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </form>
-    </div>
 
-    <div class="panel panel-default">
-        <div class="row">
-            <div class="col-md-1">
-                <strong>Secciones:</strong>
-            </div>
-            <div class="col-md-9">
-                <?php echo $DocManager->ArraySelector($secciones, 'secciones'); ?>
-            </div>
-            <div class="col-md-1">
-                <button type="button" class="btn btn-default btn-sm">Insertar Seccion</button>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-1">
-                <strong>Tags:</strong>
-            </div>
-            <div class="col-md-9">
-                <?php echo $DocManager->ArraySelector($tags, 'tags'); ?>
-            </div>
-            <div class="col-md-1">
-                <button type="button" class="btn btn-default btn-sm">Insertar Tag</button>
-            </div>
-        </div>
-    </div>
-
-    <!-- Paneles de renderizado -->
-
-    <div class="panel panel-default">
-        <div class="row">
-
-            <div class="col-md-6">
-
-                <div id="content">
-
-                    <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
-                        <li class="active"><a href="#html_code" data-toggle="tab">Código (Template HTML)</a></li>
-                        <li><a href="#css_code" data-toggle="tab">Código (Template CSS)</a></li>
-                    </ul>
-
-                    <div id="my-tab-content" class="tab-content">
-                        <div class="tab-pane active" id="html_code">
-                            <textarea name="nota[formato_html]" style="width:800px; height: 500px;"><?php echo $carta['formato']; ?></textarea>
-                        </div>
-                        <div class="tab-pane" id="css_code">
-                            <textarea name="nota[formato_css]" style="width:800px; height: 500px;"><?php echo $carta['formato_css']; ?></textarea>
-                        </div>
+                    <div class="tab-pane" id="css_code">
+                        <textarea name="formato_css" style="width:100%; height: 605px;"><?php echo $carta['formato_css']; ?></textarea>
                     </div>
                 </div>
 
-            </div> 
-
-            <div class="col-md-6" id="rendered_template">
-                <div class="row" style="text-align: center;">
-                    <h4>Vista Previa</h4>
-                </div>
-                <img src="//placehold.it/800x500" class="img-responsive">
             </div>
-
         </div>
     </div>
 </div>
 
+<div class="col-md-6">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-3">
+                    <h5>Revisando Cobro N°</h5>
+                </div>
+                <div class="col-md-3">
+                    <input type="text" class="form-control"  name="doc_id_cobro" value="">
+                </div>
+                <div class="col-md-4">
+                    <button type="button" class="btn btn-default">Descargar Word</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="panel-body">
+            <iframe id="previsualizacion_html" style="width:100%;height:536px"></iframe>
+        </div>
+
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-2">
+                <button type="button" class="btn btn-default">Margenes</button>
+            </div>
+            <div class="col-md-5">
+                <div class="alert alert-warning alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <strong> Cobros asociados </strong> <?php echo $cobros_asociados ?>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <button type="button" class="btn btn-danger">Eliminar</button>
+                <button type="button" class="btn btn-success">Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 <?php
-echo '</body>';
-echo '</html>';
+echo $DocManager->GetHtmlFooter();
