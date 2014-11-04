9e1dce032d1aa53cbeeaecaef61f4f033938bba3
Claudio Peralta <cuayocps@gmail.com>
2014-11-04 10:52:24 -0300

    Imprime los documentos con `XmlGenerator` que toma los datos desde **Mantención factura PDF**

--- app/interfaces/factura_doc.php
+++ app/interfaces/factura_doc.php
@@ -1,27 +1,19 @@
 <?php
 
 require_once dirname(__FILE__) . '/../conf.php';
-require_once Conf::ServerDir() . '/../fw/classes/Sesion.php';
-require_once Conf::ServerDir() . '/../fw/classes/Pagina.php';
-require_once Conf::ServerDir() . '/../app/classes/UtilesApp.php';
-require_once Conf::ServerDir() . '/../app/classes/DocGenerator.php';
-require_once Conf::ServerDir() . '/../app/classes/TemplateParser.php';
-require_once Conf::ServerDir() . '/../app/classes/Factura.php';
-require_once Conf::ServerDir() . '/../app/classes/Debug.php';
 
 $sesion = new Sesion(array('COB'));
 $pagina = new Pagina($sesion);
 $factura = new Factura($sesion);
 
-if ( !$factura->Load($id_factura_grabada) ) {
+if (!$factura->Load($id_factura_grabada)) {
 	$pagina->FatalError('Factura inválido');
 }
 
-if ( $lang == '' ) {
+if ($lang == '') {
 	$lang = 'es';
 }
-	
-if ( file_exists(Conf::ServerDir() . "/lang/{$lang}_" . Conf::dbUser() . ".php")) {
+if (file_exists(Conf::ServerDir() . "/lang/{$lang}_" . Conf::dbUser() . ".php")) {
 	$lang_archivo = $lang . '_' . Conf::dbUser() . '.php';
 } else {
 	$lang_archivo = $lang . '.php';
@@ -39,23 +31,31 @@ $cssData = $html_css['css'];
 $xml = $html_css['xml'];
 $xmlbit = $html_css['xmlbit'];
 
-list($docm_top, $docm_right, $docm_bottom, $docm_left, $docm_header, $docm_footer,$papel) = UtilesApp::ObtenerMargenesFactura($sesion, $factura->fields['id_documento_legal']);
+$nombre_documento = "doc_tributario_{$id_factura_grabada}_{$valor_unico}";
 
-if ( !$papel || $papel=='' ) {
-	$papel='LETTER';
+/*
+ * Si el xml tiene el anchor %xml_body% se asume que es un template para XmlGenerator
+ */
+if ($xmlbit == 1 && strpos($xml, '%xml_body%') !== false) {
+	$xml = new XmlGenerator($sesion);
+	$filename = "$nombre_documento.xml";
+	$xml->outputXml($id_factura_grabada, $html_css['xml'], $filename);
+	exit;
+}
+
+list($docm_top, $docm_right, $docm_bottom, $docm_left, $docm_header, $docm_footer, $papel) = UtilesApp::ObtenerMargenesFactura($sesion, $factura->fields['id_documento_legal']);
+
+if (empty($papel)) {
+	$papel = 'LETTER';
 }
 
 $doc = new DocGenerator($html, $cssData, $papel, false, 'PORTRAIT', $docm_top, $docm_right, $docm_bottom, $docm_left, 'EMITIDO', '', $configuracion);
-
-// echo '<style>'.$cssData.'</style>'.$html; exit;
-
 $valor_unico = substr(time(), -3);
 
-if ( $xmlbit == 1) {
-	$doc->outputxml($xml, 'doc_tributario_' . $id_factura_grabada . '_' . $valor_unico . '.xml');
+if ($xmlbit == 1) {
+	$doc->outputxml($xml, "$nombre_documento.xml");
 } else {
-	$doc->output('doc_tributario_' . $id_factura_grabada . '_' . $valor_unico . '.doc', '', 'factura');
+	$doc->output("$nombre_documento.doc", '', 'factura');
 }
 
 exit;
-?>
