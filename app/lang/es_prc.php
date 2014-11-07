6a06b8824e4bdeae13d82435660dec3288e8def6
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2014-11-07 11:13:04 -0300

    Agrega autocompletador en Giro del cliente

--- app/lang/es_prc.php
+++ app/lang/es_prc.php
@@ -71,5 +71,6 @@ $_LANG['Para agregar un nuevo usuario ingresa su RUT aquí.'] = 'Para agregar un 
 $_LANG['Honorarios Legales'] = 'Asesoría Legal';
 $_LANG['Pago de Documentos tributarios'] = 'Reporte de Recaudación';
 
+$_LANG['Giro'] = 'Sector económico';
 
 ?>
