2cfe83ddb215575db8c6a11e10102abb346560d8
brionescl <brionescl@gmail.com>
2014-12-04 17:26:13 -0300

    setlocale

--- app/layers/utilities/twig/DateTwigExtension.php
+++ app/layers/utilities/twig/DateTwigExtension.php
@@ -6,9 +6,9 @@ class DateTwigExtension extends AbstractTwigExtension {
 		return 'TtbDateExtension';
 	}
 
-	public function extStrfTime($d, $format = 'Y/m/d', $locale = 'es_CL') {
+	public function extStrfTime($d, $format = 'Y-m-d', $locale = 'es_CL') {
 		if ($d instanceof \DateTime) {
-    	$d = $d->format('Y/m/d');
+    	$d = $d->format('Y-m-d H:i:s');
     }
     setlocale(LC_ALL, $locale);
     return utf8_encode(strftime($format, strtotime($d)));
