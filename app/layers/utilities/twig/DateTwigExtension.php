3dd3407dd0814b758b6368f54a2cf612f959c9e2
brionescl <brionescl@gmail.com>
2014-12-05 10:49:29 -0300

    tabs

--- app/layers/utilities/twig/DateTwigExtension.php
+++ app/layers/utilities/twig/DateTwigExtension.php
@@ -8,9 +8,9 @@ class DateTwigExtension extends AbstractTwigExtension {
 
 	public function extStrfTime($d, $format = 'Y-m-d', $locale = 'es_ES') {
 		if ($d instanceof \DateTime) {
-    	$d = $d->format('Y-m-d H:i:s');
-    }
-    setlocale(LC_ALL, $locale);
-    return utf8_encode(strftime($format, strtotime($d)));
+			$d = $d->format('Y-m-d H:i:s');
+		}
+		setlocale(LC_ALL, $locale);
+		return utf8_encode(strftime($format, strtotime($d)));
 	}
 }
