b556e1e951d4d675fe1ab38b67e3754bbd90b94b
brionescl <brionescl@gmail.com>
2014-12-31 09:57:32 -0300

    En método Conf::Server se pregunta si está definido SERVER_URL para retornar la URL completa del subdominio y dominio (https)

--- app/conf.php
+++ app/conf.php
@@ -22,82 +22,91 @@ if (!class_exists('Conf')) {
 		public static function dbName() { return DBNAME; }
 		public static function dbUser() { return DBUSER; }
 		public static function dbPass() { return DBPASS; }
-		public static function Server() { return "https://" . SUBDOMAIN . ".thetimebilling.com"; }
+		public static function Server() { return defined('SERVER_URL') ? SERVER_URL : 'https://' . SUBDOMAIN . '.thetimebilling.com'; }
 		public static function RootDir() { return '/' . ROOTDIR; }
 		public static function ServerIP() { return SUBDOMAIN . '.thetimebilling.com'; }
 		public static function logoutRedirect() { return Conf::RootDir() . '/index.php?logout'; }
 		public static function Hash() { return 'c85ef9997e6a30032a765a20ee69630b'; }
 		public static function Logo($fullPath = false) { return ($fullPath ? Conf::Server() : '') . Conf::ImgDir() . '/logo_tt.jpg'; }
 		public static function LogoDoc($fullPath = false) { return ($fullPath ? Conf::Server() : '') . Conf::ImgDir() . '/logo_tt.jpg'; }
-		public static function FicheroLogoDoc() { return "logo_lemon.png"; }
-		public static function EsAmbientePrueba() { if(defined('BACKUP')&& (BACKUP == 2 || BACKUP == '2')) return true; }
-		public static function RutaGraficos() { return "/usr/lib64/php/modules/ChartDirector/lib/phpchartdir.php"; }
-		public static function RutaPdf() { return "/usr/share/php/fpdf/fpdf.php"; }
+		public static function FicheroLogoDoc() { return 'logo_lemon.png'; }
+		public static function EsAmbientePrueba() { if (defined('BACKUP') && (BACKUP == 2 || BACKUP == '2')) return true; }
+		public static function RutaGraficos() { return '/usr/lib64/php/modules/ChartDirector/lib/phpchartdir.php'; }
+		public static function RutaPdf() { return '/usr/share/php/fpdf/fpdf.php'; }
 		public static function DireccionPdf() { return "Estudio Lemontech\nTorremolinos 70, Oficina 4\nCP 7550159 Las Condes\nSantiago, Chile\nTf. +56-2 2299243\ninfo@lemontech.cl";}
 		public static function LogoPdf($fullPath = true) { return ($fullPath ? Conf::Server() : '') . Conf::ImgDir() . '/logo_pdf.jpg'; }
-		public static function Templates() { return "default"; }
-		public static function Host() { return Conf::Server() . "/" . ROOTDIR . "/"; }
-		public static function MantencionTablas() { return array('grupo_cliente','prm_comuna','prm_area_proyecto','prm_area_usuario','prm_tipo_proyecto','prm_moneda','j_prm_materia','j_prm_estado_causa','prm_categoria_usuario'); }
+		public static function Templates() { return 'default'; }
+		public static function Host() { return Conf::Server() . '/' . ROOTDIR . '/'; }
+
+		public static function MantencionTablas() {
+			return array(
+				'grupo_cliente',
+				'prm_comuna',
+				'prm_area_proyecto',
+				'prm_area_usuario',
+				'prm_tipo_proyecto',
+				'prm_moneda',
+				'j_prm_materia',
+				'j_prm_estado_causa',
+				'prm_categoria_usuario'
+			);
+		}
+
 		public static function GlosaTablas() {
-			$glosa['prm_comuna'] = "glosa_comuna";
-			$glosa['j_prm_materia'] = "glosa_materia";
-			$glosa['cliente'] = "glosa_cliente";
-			$glosa['j_prm_estado_causa'] = "glosa_estado_causa";
-			$glosa['prm_categoria_usuario'] = "CategorÃƒÂ­de Usuario";
-			$glosa['prm_area_proyecto'] = "prm_area_proyecto";
-			$glosa['prm_tipo_proyecto'] = "prm_tipo_proyecto";
-			$glosa['prm_area_usuario'] = "prm_area_usuario";
-
-			$glosa['asunto'] = "glosa_asunto";
-			$glosa['prm_banco']="Bancos";
-			$glosa['cuenta_banco']="Cuentas Bancarias";
-
-			return $glosa;
+			return array(
+				'prm_comuna' => 'glosa_comuna',
+				'j_prm_materia' => 'glosa_materia',
+				'cliente' => 'glosa_cliente',
+				'j_prm_estado_causa' => 'glosa_estado_causa',
+				'prm_categoria_usuario' => 'Categoría ­de Usuario',
+				'prm_area_proyecto' => 'prm_area_proyecto',
+				'prm_tipo_proyecto' => 'prm_tipo_proyecto',
+				'prm_area_usuario' => 'prm_area_usuario',
+				'asunto' => 'glosa_asunto',
+				'prm_banco' => 'Bancos',
+				'cuenta_banco' => 'Cuentas Bancarias'
+			);
 		}
 
 		public static function Locale() { return array('es_CL','es_ES'); }
 		public static function BorrarDatosAdministracion() { return false; }
-
 		public static function TimestampDeployJS(){ return '1234370043'; }
 		public static function TimestampDeployCSS(){ return '1226330411'; }
 		public static function TieneTablaVisitante() { return true; }
 
 		/**
+		 * Obtener configuraciones desde la base de datos
 		 *
-		 * @param object $sesion
+		 * @param object $Sesion
 		 * @param string $conf
 		 * @return string
-		 *  Ahora comprueba si existe el array $sesion->arrayconf para llenarlo una sola vez y consultar de él de ahí en adelante.
+		 *
+		 * Ahora comprueba si existe el array $Sesion->arrayconf para llenarlo una sola vez y consultar de él de ahí en adelante.
 		 * Si no, intenta usar memcache
-		 * Tiene fallback al código antiguo por si
 		 */
 		public static function GetConf(Sesion $Sesion, $conf) {
 			global $memcache;
-			$existememcache = isset($memcache) && is_object($memcache); // nunca se sabe si correrán este código en una máquina sin MC. Primero se comprueba con isset para evitar un warning de undefined variable.
 
-			// Prioridad sobre los conf?
-			//1) Primera Prioridad: Siempre es más barato leer un método static de la clase conf que obtenerlo de memcache o de la base de datos.
+			// nunca se sabe si correrán este código en una máquina sin MC. Primero se comprueba con isset para evitar un warning de undefined variable.
+			$existememcache = isset($memcache) && is_object($memcache);
+
+			// Prioridad sobre los conf
 			if (method_exists(__CLASS__, $conf)) {
+				// 1) Primera Prioridad: Siempre es más barato leer un método static de la clase conf que obtenerlo de memcache o de la base de datos.
 				$config = self::$conf();
-				//error_log("CONF ESTATICA $conf = $config");
 				return $config;
-
-			 // 2) Segunda prioridad: leer de la memoria. Existe variable caching?
 			} else if (count($Sesion->arrayconf ) > 0) {
+			 	// 2) Segunda prioridad: leer de la memoria. Existe variable caching?
 				// 2.1) Usar variable desde caching
 				$arrayconf = $Sesion->arrayconf;
-				//error_log("CACHE HIT! $conf = {$arrayconf[$conf]}");
 				return $arrayconf[$conf];
-
-			// 3) Tercera prioridad: existe memcache y la llave de configuración está vigente.
 			} else if ($existememcache && $arrayconf = UtilesApp::utf8izar(json_decode($memcache->get(DBNAME . '_config'), true), false)) {
+				// 3) Tercera prioridad: existe memcache y la llave de configuración está vigente.
 				$Sesion->arrayconf = $arrayconf;
-				//error_log("CACHE HIT FROM MEMCACHED! $conf ={$arrayconf[$conf]}");
 				return $arrayconf[$conf];
-
-			// 4) Cuarta prioridad: tengo que obtener el dato de la BD, aprovecho de llenar el dato en memoria y en memcache.
 			} else {
-				//4.1 compruebo conexión a la BBDD para consultar array de configuraciones
+				// 4) Cuarta prioridad: tengo que obtener el dato de la BD, aprovecho de llenar el dato en memoria y en memcache.
+				// 4.1) compruebo conexión a la BD para consultar array de configuraciones
 				if(isset($Sesion->pdodbh)) {
 					$query = "SELECT glosa_opcion, valor_opcion FROM configuracion";
 					$bd_configs = $Sesion->pdodbh->query($query)->fetchAll(PDO::FETCH_NUM | PDO::FETCH_GROUP);
@@ -108,20 +117,17 @@ if (!class_exists('Conf')) {
 					// 4.2) Si existe memcache, fijo la llave usando lo obtenido en 4.1
 					if ($existememcache) {
 						$memcache->set(DBNAME . '_config', json_encode(UtilesApp::utf8izar($Sesion->arrayconf)), false, 120);
-						//error_log("MEMCACHE CACHE SET $conf = {$Sesion->arrayconf[$conf]} (" . count($Sesion->arrayconf) . " registros)");
 					}
 
 					return $Sesion->arrayconf[$conf];
-
 				} else {
-					//error_log('No hay conexion a BD');
 					return false;
 				}
 			}
 		}
 
-		public static function PasswordWS() { return defined('PASSWS') ? PASSWS : base64_encode(rand(10000,90000)); }
-		public static function UsuarioWS() { return defined('USERWS') ? USERWS : base64_encode(rand(10000,90000)); }
+		public static function PasswordWS() { return defined('PASSWS') ? PASSWS : base64_encode(rand(10000, 90000)); }
+		public static function UsuarioWS() { return defined('USERWS') ? USERWS : base64_encode(rand(10000, 90000)); }
 
 		public static function AmazonKey() {
 			return array(
@@ -146,5 +152,5 @@ defined('LOGDIR') || define('LOGDIR', '/tmp/logs/');
 defined('S3_UPLOAD_BUCKET') || define('S3_UPLOAD_BUCKET', 'timebilling-uploads');
 
 require_once APPPATH . '/fw/funciones/funciones.php';
-require_once APPPATH . '/app/lang/es.php';		//Para que cargue el idioma por defecto
-require_once APPPATH . '/app/lang/abogado.php';	//Por si hay palabras especificas relacionadas con el rubro
+require_once APPPATH . '/app/lang/es.php'; // Para que cargue el idioma por defecto
+require_once APPPATH . '/app/lang/abogado.php';	// Por si hay palabras especificas relacionadas con el rubro
