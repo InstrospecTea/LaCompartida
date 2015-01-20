8821220cd269e2ddb69df74a029e2168fa2166af
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-01-20 18:14:41 -0300

    Mostrar descuentos en facturas que se estan creando

--- app/layers/controller/InvoiceController.php
+++ app/layers/controller/InvoiceController.php
@@ -12,13 +12,21 @@ class InvoiceController extends AbstractController {
 
 		$invoiceId = $this->data['invoice'] ? $this->data['invoice'] : $this->params['invoice'];
 		$chargeId = $this->data['charge'] ? $this->data['charge'] : $this->params['charge'];
+		$amount  = $this->data['amount'] > 0 ? $this->data['amount'] : $this->params['amount'];
 
-		$invoice = $this->BillingBusiness->getInvoice($invoiceId);
 		$charge = $this->ChargingBusiness->getCharge($chargeId);
 		$currency = $this->CoiningBusiness->getCurrency($charge->get('opc_moneda_total'));
 		$language = $this->TranslatingBusiness->getLanguageByCode('es');
 
-		$detail = $this->BillingBusiness->getFeesDataOfInvoiceByCharge($invoice, $charge, $currency);
+		if (is_null($amount))  {
+			$invoice = $this->BillingBusiness->getInvoice($invoiceId);
+			$detail = $this->BillingBusiness->getFeesDataOfInvoiceByCharge($invoice, $charge, $currency);
+		} else {
+			$chargeDetail = $this->ChargingBusiness->getAmountDetailOfFees($charge, $currency);
+			$chargeFees = $chargeDetail->get('saldo_honorarios');
+			$chargeDiscount = $chargeDetail->get('descuento_honorarios');
+			$detail = $this->BillingBusiness->getFeesDataOfInvoiceByAmounts($amount, $chargeFees, $chargeDiscount, $currency);
+		}
 		
 		$this->set('feeDetiail', $detail);
 		$this->set('currency', $currency);
