818d446e340cca0b8ed418caa391f283bd8ca73b
Matias Orellana <morellan@gmail.com>
2015-02-02 17:08:20 -0300

    Nueva columna en SimpleReport de Facturas y Correcciones varias de PHP [Fixes #87152264]

--- app/classes/Factura.php
+++ app/classes/Factura.php
@@ -196,7 +196,7 @@ class Factura extends Objeto {
 				'symbol' => 'simbolo',
 				'subtotal' => 'simbolo'
 			),
-		),		
+		),
 		array(
 			'field' => 'honorarios',
 			'format' => 'number',
@@ -315,6 +315,11 @@ class Factura extends Objeto {
 			'title' => 'id_factura',
 			'visible' => false,
 		),
+		array(
+			'field' => 'glosa_estudio',
+			'title' => 'Companía',
+			'visible' => false,
+		)
 	);
 
 	// Twig, the flexible, fast, and secure template language for PHP
@@ -2477,7 +2482,8 @@ class Factura extends Objeto {
                 , if(factura.RUT_cliente != contrato.rut,factura.cliente,'no' ) as mostrar_diferencia_razon_social
                 , GROUP_CONCAT(asunto.codigo_asunto SEPARATOR ';') AS codigos_asunto
                 , GROUP_CONCAT(asunto.glosa_asunto SEPARATOR ';') AS glosas_asunto
-                , factura.RUT_cliente";
+                , factura.RUT_cliente
+                , prm_estudio.glosa_estudio";
 
 		if ($opciones['mostrar_pagos']) {
 			$query .= ", (
@@ -2516,6 +2522,7 @@ class Factura extends Objeto {
 		   LEFT JOIN usuario ON usuario.id_usuario=contrato.id_usuario_responsable
 		   LEFT JOIN cobro_asunto ON cobro_asunto.id_cobro = factura.id_cobro
 		   LEFT JOIN asunto ON asunto.codigo_asunto = cobro_asunto.codigo_asunto
+		   LEFT JOIN prm_estudio ON prm_estudio.id_estudio = factura.id_estudio
 		   WHERE ";
 
 		$resultingquery = $query . " \n " . $where . " \n " . $groupby . "\n" . $orderby;
@@ -2582,16 +2589,16 @@ class Factura extends Objeto {
 	, $descripcion_factura, $serie, $desde_asiento_contable, $opciones) {
 
 		$query = $this->QueryReporte($orden, $where, $numero, $fecha1, $fecha2
-			, $tipo_documento_legal_buscado
-			, $codigo_cliente, $codigo_cliente_secundario
-			, $codigo_asunto, $codigo_asunto_secundario
-			, $id_contrato, $id_estudio, $id_cobro, $id_estado, $id_moneda, $grupo_ventas, $razon_social, $descripcion_factura, $serie, $desde_asiento_contable);
-		
+			, $tipo_documento_legal_buscado, $codigo_cliente, $codigo_cliente_secundario
+			, $codigo_asunto, $codigo_asunto_secundario, $id_contrato, $id_estudio
+			, $id_cobro, $id_estado, $id_moneda, $grupo_ventas, $razon_social
+			, $descripcion_factura, $serie, $desde_asiento_contable, $opciones);
+
 		//agregar al reporte de factura las columnas, monto real - observaciones - Saldo - fecha último pago
 		$statement = $this->sesion->pdodbh->prepare($query);
 		$statement->execute();
 		$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-			
+
 
 		$billingBusiness = new BillingBusiness($this->sesion);
 		$charginBusiness = new ChargingBusiness($this->sesion);
@@ -2604,7 +2611,7 @@ class Factura extends Objeto {
 			//monto_real
 			$monto_real = $this->ObtenerValorReal($fila['id_factura']);
 			$results[$key]['monto_real'] = strtoupper($fila['codigo_estado']) == 'A' ? '0' : $monto_real;
-			
+
 			//Obtener Descuento y Bruto
 			$id_cobro = $results[$key]['id_cobro'];
 			$id_factura = $results[$key]['id_factura'];
@@ -2624,7 +2631,7 @@ class Factura extends Objeto {
 			$chargeFees = $charginData[$id_cobro]->get('saldo_honorarios');
 			$chargeDiscount = $charginData[$id_cobro]->get('descuento_honorarios');
 			$billingData = $billingBusiness->getFeesDataOfInvoiceByAmounts($invoiceFees, $chargeFees, $chargeDiscount, $currency);
-			
+
 			$results[$key]['bruto_honorarios'] = $billingData->get('subtotal_honorarios');
 			$results[$key]['descuento_honorarios'] = $billingData->get('descuento_honorarios');
 
@@ -2660,7 +2667,7 @@ class Factura extends Objeto {
 			$results[$key]['pagos'] = $pago['saldo_pagos'] > 0 ? $pago['saldo_pagos'] : '0';
 			$results[$key]['fecha_ultimo_pago'] = $pago['fecha_ultimo_pago'];
 		}
-		return $results;	
+		return $results;
 	}
 
 	public function PreCrearDato($data) {
