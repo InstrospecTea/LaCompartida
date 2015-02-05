caf2f43fa73f8da870065ae4cda1c889bdf730bc
Claudio Peralta <cuayocps@gmail.com>
2015-02-05 11:03:55 -0300

    Sobre escribe ProcessLockDAO::Write() para evitar la escritura de logs en db.

--- app/layers/dao.framework/ProcessLockDAO.php
+++ app/layers/dao.framework/ProcessLockDAO.php
@@ -7,4 +7,43 @@ class ProcessLockDAO extends AbstractDAO implements IProcessLockDAO{
 		return 'ProcessLock';
 	}
 
+	/**
+	 * Sobre escribe el método original para evitar la escritura de logs en db.
+	 * @return boolean
+	 */
+	public function Write() {
+		$id = $this->fields[$this->campo_id];
+		$valores = array();
+		if (empty($id)) {
+			$query = "INSERT INTO {$this->tabla} SET ";
+			$valores[] = "fecha_creacion = NOW()";
+		} else {
+			$query = "UPDATE {$this->tabla} SET ";
+			$valores[] = "fecha_modificacion = NOW()";
+		}
+		foreach ($this->fields as $key => $value) {
+			if ($this->campo_id != $key && $this->changes[$key]) {
+				$value = mysql_real_escape_string($value);
+				if ($value == 'NULL') {
+					$valores[] = "$key = NULL";
+				} else {
+					$valores[] = "$key = '$value'";
+				}
+			}
+		}
+
+		$query .= implode(', ', $valores);
+
+		if (!empty($id)) {
+			$query .= " WHERE {$this->campo_id} = '{$id}'";
+		}
+
+		try {
+			return $this->sesion->pdodbh->query($query);
+		} catch (Exception $e) {
+			Log::write('ProcessLockDAO: ' . $e->getMessaje(), Cobro::PROCESS_NAME);
+		}
+		return false;
+	}
+
 }
