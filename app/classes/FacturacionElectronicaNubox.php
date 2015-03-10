97faa13c3ef8da54bae87c9ccdddbd8bbd1d3d3a
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-03-10 17:13:52 -0300

    Nubox fixes to prevent Layer 8

--- app/classes/FacturacionElectronicaNubox.php
+++ app/classes/FacturacionElectronicaNubox.php
@@ -90,15 +90,13 @@ class FacturacionElectronicaNubox extends FacturacionElectronica {
 				}
 				var self = jQuery(this);
 				var id_factura = self.data("factura");
+				var id_cobro = self.data("cobro");
 				var loading = jQuery("<span/>", {class: "loadingbar", style: "float:left;position:absolute;width:95px;height:20px;margin-left:-90px;"});
 				self.parent().append(loading);
 				jQuery.ajax({url: root_dir + "/api/index.php/invoices/" + id_factura +  "/build",
 					type: "POST"
 				}).success(function(data) {
-					loading.remove();
-					buttons = jQuery('{$BotonDescargarHTML}');
-					buttons.each(function(i, e) { jQuery(e).attr("data-factura", id_factura)});
-					self.replaceWith(buttons);
+					jQuery('#cajafacturas').load('ajax/cobros7.php', {id_cobro: id_cobro, opc: 'cajafacturas'});
 				}).error(function(error_data){
 					loading.remove();
 					response = JSON.parse(error_data.responseText);
