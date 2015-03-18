4caaac1536391bcba2752e789dfbee5d30d96050
Claudio Peralta <cuayocps@gmail.com>
2015-03-18 11:35:22 -0300

    Agrega clase ControllerLoader. Agrega mensajes de error cuando controller, method o view no existen o 404 si error reporting no tiene E_ERROR.

--- app/layers/controller/AbstractController.php
+++ app/layers/controller/AbstractController.php
@@ -33,7 +33,12 @@ abstract class AbstractController {
 			$this->Session->drop('post_data');
 		}
 
-		$reflector = new ReflectionMethod($this, $method);
+		try {
+			$reflector = new ReflectionMethod($this, $method);
+		} catch (ReflectionException $re) {
+			$get = array('controller' => $this->request['controller'], 'method' => $method);
+			new ControllerLoader('ErrorPage', 'error_method', array(), $this->request['isAjax'], $get);
+		}
 		$reflector->invokeArgs($this, $args);
 
 		if ($this->autoRender !== false) {
@@ -123,8 +128,6 @@ abstract class AbstractController {
 		if (in_array($classname, $this->loadedClass)) {
 			return;
 		}
-		$filename = LAYER_PATH . "/utilities/{$classname}.php";
-		require $filename;
 		$this->{$classname} = new $classname($this->Session);
 		$this->loadedClass[] = $classname;
 	}
@@ -156,12 +159,12 @@ abstract class AbstractController {
 	 */
 	private function loadRequestParameters($method) {
 		$reflector = new ReflectionClass($this);
-		$this->name = preg_replace('/Controller$/', '', $reflector->name);
+		$this->name = $reflector->name;
 		$this->params = $_GET;
 		$this->data = $_POST;
 
 		$this->request = array(
-			'controller' => $this->name,
+			'controller' => preg_replace('/Controller$/', '', $reflector->name),
 			'action' => $method,
 			'method' => strtolower($_SERVER['REQUEST_METHOD']),
 			'isAjax' => !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'
@@ -178,6 +181,7 @@ abstract class AbstractController {
 		$this->ViewRenderer->request = $this->request;
 		$this->ViewRenderer->set($this->vars);
 		echo $this->ViewRenderer->render($this->layout, $_element);
+		$this->afterRender();
 	}
 
 
