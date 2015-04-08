136e6ea1f06c09c257f0bb4f4bf04872b346d2f1
Claudio Peralta <cuayocps@gmail.com>
2015-04-08 18:57:50 -0300

    modifica nombre de funcion

--- app/classes/Tarifa.php
+++ app/classes/Tarifa.php
@@ -107,7 +107,7 @@ class Tarifa extends Objeto {
 		if (!empty($id_tarifa)) {
 			$this->Load($id_tarifa);
 			if ($this->Loaded() && $this->fields['tarifa_flat'] == $valor && $this->fields['glosa_tarifa'] == $glosa) {
-			$this->verificaTerifaFlatUsuarios($id_tarifa, $id_moneda, $valor);
+			$this->verificaTarifaFlatUsuarios($id_tarifa, $id_moneda, $valor);
 				return $id_tarifa;
 			}
 		}
@@ -116,7 +116,7 @@ class Tarifa extends Objeto {
 		$resp = mysql_query($query, $this->sesion->dbh);
 		$tarifa = mysql_fetch_assoc($resp);
 		if ($tarifa !== false) {
-			$this->verificaTerifaFlatUsuarios($tarifa[$this->campo_id], $id_moneda, $valor);
+			$this->verificaTarifaFlatUsuarios($tarifa[$this->campo_id], $id_moneda, $valor);
 			return $tarifa[$this->campo_id];
 		}
 
@@ -130,7 +130,7 @@ class Tarifa extends Objeto {
 
 		$id_tarifa = $this->fields[$this->campo_id];
 
-		$this->verificaTerifaFlatUsuarios($id_tarifa, $id_moneda, $valor);
+		$this->verificaTarifaFlatUsuarios($id_tarifa, $id_moneda, $valor);
 
 		//eliminar tarifas flat huachas (se crea 1 por contrato flat, y quedan flotando al cambiarse a variable)
 		$query = "SELECT t.id_tarifa FROM tarifa t LEFT JOIN contrato c ON t.id_tarifa = c.id_tarifa WHERE c.id_contrato IS NULL AND t.tarifa_flat IS NOT NULL";
@@ -153,7 +153,7 @@ class Tarifa extends Objeto {
 	 * @param type $id_moneda
 	 * @param type $valor
 	 */
-	function verificaTerifaFlatUsuarios($id_tarifa, $id_moneda, $valor) {
+	public function verificaTarifaFlatUsuarios($id_tarifa, $id_moneda, $valor) {
 		$uCrireria = new Criteria($this->sesion);
 		$uCrireria
 				->add_select('usuario.id_usuario')
