2522e0d92e35372b81d5c0392f81dfef72ae627e
Nicolás Torres <ntorres@lemontech.cl>
2015-07-29 11:56:55 -0300

    update a la BD para setear estado visible = 0 a usuario Lemontech

--- app/update.php
+++ app/update.php
@@ -10840,6 +10840,23 @@ QUERY;
 				ADD `enviar_liquidacion_al_generar` int(11) NULL DEFAULT 0 COMMENT 'Liquidaciones Programadas: Enviar la liquidación generada al cliente'";
 
 			break;
+
+		case 8.07:
+			$queries = array("UPDATE usuario SET visible = 0 WHERE rut = '99511620'");
+			break;
+	}
+
+	if (!empty($queries)) {
+		ejecutar($queries, $dbh);
+	}
+}
+
+/* PASO 2: Agregar el numero de version al arreglo VERSIONES.
+	(No olvidar agregar la notificacion de los cambios) */
+
+$num = 0;
+$min_update = 2; //FFF: del 2 hacia atrás no tienen soporte
+$max_update = 8.07;
 	}
 
 	if (!empty($queries)) {
