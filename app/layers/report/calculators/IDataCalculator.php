b6ef2ff061c64499ceb279268d968b69da81c408
brionescl <brionescl@gmail.com>
2015-08-25 16:12:58 -0300

    comentarios

--- app/layers/report/calculators/IDataCalculator.php
+++ app/layers/report/calculators/IDataCalculator.php
@@ -3,19 +3,39 @@
 interface IDataCalculator() {
 
 	/**
-	* Exporta los datos según la instancia de {@link ReporteEngine}
-	* @return mixed
-	*/
+	 * Obtiene los agrupadores por los cuales se podrán filtrar y posteriormente agrupar
+	 * @return array
+	 */
 	function getAllowedGroupers();
 
+	/**
+	 * Obtiene los filtros por los cuales se podrá filtra
+	 * @return array
+	 */
 	function getAllowedFilters();
 
+	/**
+	 * Construye la query de trabajos
+	 * @return boolean
+	 */
 	function buildWorkQuery();
 
+	/**
+	 * Construye la query de tramites
+	 * @return boolean
+	 */
 	function buildErrandQuery();
 
+	/**
+	 * Construye la query de cobros
+	 * @return boolean
+	 */
 	function buildChargeQuery();
 
+	/**
+	 * Ejecuta las querys construidas
+	 * @return array
+	 */
 	function calculate();
 
 }
