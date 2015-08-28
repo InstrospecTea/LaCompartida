a6cc7414e94ee61ed68924ce017c0773f68ab037
brionescl <brionescl@gmail.com>
2015-08-28 18:02:09 -0300

    agregar agrupador para tramite y cobro

--- app/layers/report.support/groupers/CodigoClienteGrouper.php
+++ app/layers/report.support/groupers/CodigoClienteGrouper.php
@@ -15,10 +15,23 @@ class CodigoClienteGrouper extends AbstractGrouperTranslator {
 	}
 
 	function translateForCharges(Criteria $Criteria) {
+		$Criteria
+			->add_select($this->getSelectField(), 'codigo_cliente')
+			->add_left_join_with('cliente', 'asunto.codigo_cliente = cliente.codigo_cliente')
+			->add_grouping($this->getSelectField())
+			->add_ordering($this->getOrderField());
+
 		return $Criteria;
 	}
 
 	function translateForErrands(Criteria $Criteria) {
+		$Criteria
+			->add_select($this->getSelectField(), 'codigo_cliente')
+			->add_left_join_with('asunto', 'asunto.codigo_asunto = trabajo.codigo_asunto')
+			->add_left_join_with('cliente', 'asunto.codigo_cliente = cliente.codigo_cliente')
+			->add_grouping($this->getSelectField())
+			->add_ordering($this->getOrderField());
+
 		return $Criteria;
 	}
 
