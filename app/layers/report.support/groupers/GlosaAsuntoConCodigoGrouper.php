151384838d9882e565af0d140a332df06cfddbf0
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-08-28 21:10:06 -0300

    Agrupador glosa_asunto y glosa_asunto_con_codigo

---
+++ app/layers/report.support/groupers/GlosaAsuntoConCodigoGrouper.php
@@ -0,0 +1,53 @@
+<?php
+
+class GlosaAsuntoConCodigoGrouper extends AbstractGrouperTranslator {
+
+	function getGroupField() {
+		return 'asunto.codigo_asunto';
+	}
+
+	function getSelectField() {
+		return "CONCAT(asunto.codigo_asunto, ': ', asunto.glosa_asunto)";
+	}
+
+	function getOrderField() {
+		return "CONCAT(asunto.codigo_asunto, ': ', asunto.glosa_asunto)";
+	}
+
+	function translateForCharges(Criteria $Criteria) {
+		$Criteria
+			->add_select($this->getSelectField(), 'glosa_asunto_con_codigo')
+			->add_grouping($this->getGroupField())
+			->add_ordering($this->getOrderField())
+			->add_left_join_with('cobro_asunto',
+				CriteriaRestriction::equals('cobro_asunto.id_cobro', 'cobro.id_cobro'))
+			->add_left_join_with('asunto',
+				CriteriaRestriction::equals('asunto.codigo_asunto', 'cobro_asunto.codigo_asunto'));
+
+		return $Criteria;
+	}
+
+	function translateForErrands(Criteria $Criteria) {
+		$Criteria
+			->add_select($this->getSelectField(), 'glosa_asunto_con_codigo')
+			->add_grouping($this->getGroupField())
+			->add_ordering($this->getOrderField())
+			->add_left_join_with('asunto',
+				CriteriaRestriction::equals('asunto.codigo_asunto', 'tramite.codigo_asunto')
+			);
+
+		return $Criteria;
+	}
+
+	function translateForWorks(Criteria $Criteria) {
+		$Criteria
+			->add_select($this->getSelectField(), 'glosa_asunto_con_codigo')
+			->add_grouping($this->getGroupField())
+			->add_ordering($this->getOrderField())
+			->add_left_join_with('asunto',
+				CriteriaRestriction::equals('asunto.codigo_asunto', 'trabajo.codigo_asunto')
+			);
+
+		return $Criteria;
+	}
+}
