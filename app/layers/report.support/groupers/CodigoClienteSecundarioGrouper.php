650f8d98d442ebf5efe347c01cd20e583a6e3538
Daniel Ochoa John <dochoaj@gmail.com>
2015-08-31 16:28:16 -0300

    Add codigo cliente secundario grouper

---
+++ app/layers/report.support/groupers/CodigoClienteSecundarioGrouper.php
@@ -0,0 +1,81 @@
+<?php
+
+class CodigoClienteSecundarioGrouper extends AbstractGrouperTranslator {
+
+	function getGroupField() {
+		return 'codigo_cliente_secundario';
+	}
+
+	function getSelectField() {
+		return 'cliente.codigo_cliente_secundario';
+	}
+
+	function getOrderField() {
+		return 'codigo_cliente_secundario';
+	}
+
+	function translateForCharges(Criteria $Criteria) {
+		$Criteria->add_select(
+			$this->getSelectField(), 'codigo_cliente'
+		)->add_left_join_with(
+			'cliente',
+			'cliente.codigo_cliente = cobro.codigo_cliente'
+		)->add_grouping(
+			$this->getGroupField()
+		)->add_ordering(
+			$this->getOrderField()
+		);
+
+		return $Criteria;
+	}
+
+	function translateForErrands(Criteria $Criteria) {
+		$Criteria->add_select(
+			$this->getSelectField(),
+			'codigo_cliente'
+		)->add_left_join_with(
+			'asunto', 
+			CriteriaRestriction::equals(
+				'asunto.codigo_asunto',
+				'tramite.codigo_asunto'
+			)
+		)->add_left_join_with(
+			'cliente',
+			CriteriaRestriction::equals(
+				'cliente.codigo_cliente',
+				'asunto.codigo_cliente'
+			)
+		)->add_grouping(
+			$this->getGroupField()
+		)->add_ordering(
+			$this->getOrderField()
+		);
+
+		return $Criteria;
+	}
+
+	function translateForWorks(Criteria $Criteria) {
+		$Criteria->add_select(
+			$this->getSelectField(),
+			'codigo_cliente_secundario'
+		)->add_left_join_with(
+			'asunto', 
+			CriteriaRestriction::equals(
+				'asunto.codigo_asunto',
+				'trabajo.codigo_asunto'
+			)
+		)->add_left_join_with(
+			'cliente',
+			CriteriaRestriction::equals(
+				'cliente.codigo_cliente',
+				'asunto.codigo_cliente'
+			)
+		)->add_grouping(
+			$this->getGroupField()
+		)->add_ordering(
+			$this->getOrderField()
+		);
+
+		return $Criteria;
+	}
+}
