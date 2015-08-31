9e0a5e2c89af53ee2c8cc17b5c567a4bf7ce84c5
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-08-31 11:58:47 -0300

    Documentacion de calculadores

--- app/layers/report.support/calculators/BilledErrandsAmountDataCalculator.php
+++ app/layers/report.support/calculators/BilledErrandsAmountDataCalculator.php
@@ -1,11 +1,33 @@
 <?php
 
- class BilledErrandsAmountDataCalculator extends AbstractProportionalDataCalculator {
+/**
+ * El valor cobrado corresponde al monto subtotal (descontado) de trámites del cobro.
+ * Esta información se obtiene desde Trámites
+ *
+ * Condiciones para obtener un valor cobrado:
+ * 	* Que exista un cobro en estado: EMITIDO, FACTURADO, ENVIADO AL CLIENTE,
+ * 		PAGO PARCIAL o PAGADO
+ *	* Que el trámite sea cobrable
+ *
+ * Más info en https://github.com/LemontechSA/ttb/wiki/Reporte-Calculador:-Valor-Cobrado-de-Tramites
+ */
+class BilledErrandsAmountDataCalculator extends AbstractProportionalDataCalculator {
 
+	/**
+	 * Obtiene la query de trabajos correspondiente al valor cobrado
+	 * @param  Criteria $Criteria Query a la que se agregará el cálculo
+	 * @return void
+	 */
 	function getReportWorkQuery(Criteria &$Criteria) {
 		$Criteria = null;
 	}
 
+
+	/**
+	 * Obtiene la query de trámites correspondiente al valor cobrado
+	 * @param  Criteria $Criteria Query a la que se agregará el cálculo
+	 * @return void
+	 */
 	function getReportErrandQuery($Criteria) {
 		$rate = $this->getErrandsFeeField();
 		$amount = $this->getErrandsProportionalityAmountField();
@@ -29,6 +51,11 @@
 			->add_restriction(CriteriaRestriction::in('cobro.estado', array('EMITIDO', 'FACTURADO', 'ENVIADO AL CLIENTE', 'PAGO PARCIAL', 'PAGADO')));
 	}
 
+	/**
+	 * Obtiene la query de cobros sin trabajos ni trámites
+	 * @param  Criteria $Criteria Query a la que se agregará el cálculo
+	 * @return void
+	 */
 	function getReportChargeQuery(&$Criteria) {
 		$Criteria = null;
 	}
--- app/layers/report.support/calculators/StandardBilledAmountDataCalculator.php
+++ app/layers/report.support/calculators/StandardBilledAmountDataCalculator.php
@@ -1,7 +1,29 @@
 <?php
 
- class StandardBilledAmountDataCalculator extends AbstractCurrencyDataCalculator {
+/**
+ * El valor cobrado estándar (o valor_estandar) corresponde
+ * al monto subtotal (descontado) que se hubiese cobrado si la liquidación
+ * se tarificara en tarifa estándar. Independiente del tipo de liquidación.
+ *
+ * Esta información se obtiene de: Trabajos y Trámites, ya que para cobros
+ * sin trabajos ni trámites no es posible establecer una relación estándar.
+ *
+ * Condiciones para obtener un valor cobrado:
+ * 	* Que exista un cobro en estado: EMITIDO, FACTURADO, ENVIADO AL CLIENTE,
+ * 		PAGO PARCIAL o PAGADO
+ *	* Que lo que se esté cobrando sea Cobrable
+ *
+ * Más info:
+ * https://github.com/LemontechSA/ttb/wiki/Reporte-Calculador:-Valor-Cobrado-Estandar
+ *
+ */
+class StandardBilledAmountDataCalculator extends AbstractCurrencyDataCalculator {
 
+	/**
+	 * Obtiene la query de trabajos correspondiente al valor cobrado estándar
+	 * @param  Criteria $Criteria Query a la que se agregará el cálculo
+	 * @return void
+	 */
 	function getReportWorkQuery(Criteria $Criteria) {
 		$standard_amount = "
 			SUM(tarifa_hh_estandar * TIME_TO_SEC(trabajo.duracion_cobrada) / 3600)
@@ -26,6 +48,11 @@
 		);
 	}
 
+	/**
+	 * Obtiene la query de trámites correspondiente al valor cobrado estándar
+	 * @param  Criteria $Criteria Query a la que se agregará el cálculo
+	 * @return void
+	 */
 	function getReportErrandQuery($Criteria) {
 		$standard_amount = "
 			SUM(tarifa_tramite_estandar)
@@ -50,6 +77,11 @@
 		);
 	}
 
+	/**
+	 * Obtiene la query de cobros sin trabajos ni trámites
+	 * @param  Criteria $Criteria Query a la que se agregará el cálculo
+	 * @return void
+	 */
 	function getReportChargeQuery(&$Criteria) {
 		$Criteria = null;
 	}
