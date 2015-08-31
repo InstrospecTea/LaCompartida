5d46b0dc1b839356f19c78be8b14456a338b559b
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-08-31 13:16:06 -0300

    Documentación agrupadoro area asunto

--- app/layers/report.support/groupers/AbstractGrouperTranslator.php
+++ app/layers/report.support/groupers/AbstractGrouperTranslator.php
@@ -1,21 +1,41 @@
 <?php
 
+/**
+ * Abstracto para traducir keys de grupos en campos de tablas
+ */
 abstract class AbstractGrouperTranslator  implements IGrouperTranslator {
 
 	private $Session;
 
+	/**
+	 * Constructor
+	 * @param Sesion $Session La sessión para acceso a datos y configuración
+	 */
 	public function __construct(Sesion $Session) {
 		$this->Session = $Session;
 	}
 
+	/**
+	 * Obtiene el campo de la tabla de usuarios correspondiente para Usuario
+	 * @return String
+	 */
 	function getUserField() {
 		return $this->getUserFieldBy('usuario');
 	}
 
+	/**
+	 * Obtiene el campo de la tabla de usuarios correspondiente para Usuario Encargado
+	 * @return String
+	 */
 	function getUserAcountManagerField() {
 		return $this->getUserFieldBy('usuario_responsable');
 	}
 
+	/**
+	 * Dependiendo de la configuración UsaUsernameEnTodoElSistema devuelve
+	 * username o una concatenación del nombre y apellidos para agrupar
+	 * @return String
+	 */
 	function getUserFieldBy($table) {
 		if (Conf::GetConf($this->Session, 'UsaUsernameEnTodoElSistema')) {
 			return "{$table}.username";
@@ -23,8 +43,11 @@ abstract class AbstractGrouperTranslator  implements IGrouperTranslator {
 		return "CONCAT_WS(' ', {$table}.nombre, {$table}.apellido1, LEFT({$table}.apellido2, 1))";
 	}
 
+	/**
+	 * Devuelve el campo "Indefinido" para grupos.
+	 * @return String
+	 */
 	function getUndefinedField() {
 		return "'Indefinido'";
 	}
-
 }
--- app/layers/report.support/groupers/AreaAsuntoGrouper.php
+++ app/layers/report.support/groupers/AreaAsuntoGrouper.php
@@ -1,19 +1,44 @@
 <?php
-
+/**
+ * Agrupador por área de Asunto:
+ *
+ * * Agrupa por:  prm_area_proyecto.glosa
+ * * Muestra: prm_area_proyecto.glosa
+ * * Ordena por: prm_area_proyecto.glosa
+ *
+ * Más info en: https://github.com/LemontechSA/ttb/wiki/Reporte-Agrupador:-Area-Asunto
+ *
+ */
 class AreaAsuntoGrouper extends AbstractGrouperTranslator {
 
+	/**
+	 * Obtiene el campo por el cual se agrupará la query
+	 * @return String Campo por el que se agrupa en par tabla.campo o alias
+	 */
 	function getGroupField() {
 		return 'prm_area_proyecto.glosa';
 	}
 
+	/**
+	 * Obtiene el campo de grupo que se devolverá en el SELECT de la query
+	 * @return String par tabla.campo o alias de función
+	 */
 	function getSelectField() {
 		return 'prm_area_proyecto.glosa';
 	}
 
+	/**
+	 * Obtiene el campo de grupo por el cual se ordenará la query
+	 * @return String par tabla.campo o alias de función
+	 */
 	function getOrderField() {
 		return 'prm_area_proyecto.glosa';
 	}
 
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Cobros
+	 * @return void
+	 */
 	function translateForCharges(Criteria $criteria) {
 		return $criteria->add_select(
 			$this->getSelectField()
@@ -42,6 +67,10 @@ class AreaAsuntoGrouper extends AbstractGrouperTranslator {
 		);
 	}
 
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Trámites
+	 * @return void
+	 */
 	function translateForErrands(Criteria $criteria) {
 		return $criteria->add_select(
 			$this->getSelectField()
@@ -64,6 +93,10 @@ class AreaAsuntoGrouper extends AbstractGrouperTranslator {
 		);
 	}
 
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Trabajos
+	 * @return void
+	 */
 	function translateForWorks(Criteria $criteria) {
 		return $criteria->add_select(
 			$this->getSelectField()
