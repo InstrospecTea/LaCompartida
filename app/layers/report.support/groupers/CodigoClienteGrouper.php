45ca3319f871564bbd5a91f0bd5899d6a6a677d6
Daniel Ochoa John <dochoaj@gmail.com>
2015-08-31 16:28:03 -0300

    Codigo cliente grouper fix

--- app/layers/report.support/groupers/CodigoClienteGrouper.php
+++ app/layers/report.support/groupers/CodigoClienteGrouper.php
@@ -3,45 +3,78 @@
 class CodigoClienteGrouper extends AbstractGrouperTranslator {
 
 	function getGroupField() {
-		return 'cliente.codigo_cliente';
+		return 'codigo_cliente';
 	}
 
 	function getSelectField() {
-		return 'cliente.codigo_cliente';
+		return 'cliente.codigo_cliente'
 	}
 
 	function getOrderField() {
-		return 'cliente.codigo_cliente';
+		return 'codigo_cliente';
 	}
 
 	function translateForCharges(Criteria $Criteria) {
-		$Criteria
-			->add_select($this->getSelectField(), 'codigo_cliente')
-			->add_left_join_with('cliente', 'asunto.codigo_cliente = cliente.codigo_cliente')
-			->add_grouping($this->getGroupField())
-			->add_ordering($this->getOrderField());
+		$Criteria->add_select(
+			$this->getSelectField(), 'codigo_cliente'
+		)->add_left_join_with(
+			'cliente',
+			'cliente.codigo_cliente = cobro.codigo_cliente'
+		)->add_grouping(
+			$this->getGroupField()
+		)->add_ordering(
+			$this->getOrderField()
+		);
 
 		return $Criteria;
 	}
 
 	function translateForErrands(Criteria $Criteria) {
-		$Criteria
-			->add_select($this->getSelectField(), 'codigo_cliente')
-			->add_left_join_with('asunto', 'asunto.codigo_asunto = trabajo.codigo_asunto')
-			->add_left_join_with('cliente', 'asunto.codigo_cliente = cliente.codigo_cliente')
-			->add_grouping($this->getGroupField())
-			->add_ordering($this->getOrderField());
+		$Criteria->add_select(
+			$this->getSelectField(),
+			'codigo_cliente'
+		)->add_left_join_with(
+			'asunto', 
+			CriteriaRestriction::equals(
+				'asunto.codigo_asunto',
+				'tramite.codigo_asunto'
+			)
+		)->add_left_join_with(
+			'cliente',
+			CriteriaRestriction::equals(
+				'cliente.codigo_cliente',
+				'asunto.codigo_cliente'
+			)
+		)->add_grouping(
+			$this->getGroupField()
+		)->add_ordering(
+			$this->getOrderField()
+		);
 
 		return $Criteria;
 	}
 
 	function translateForWorks(Criteria $Criteria) {
-		$Criteria
-			->add_select($this->getSelectField(), 'codigo_cliente')
-			->add_left_join_with('asunto', 'asunto.codigo_asunto = trabajo.codigo_asunto')
-			->add_left_join_with('cliente', 'asunto.codigo_cliente = cliente.codigo_cliente')
-			->add_grouping($this->getGroupField())
-			->add_ordering($this->getOrderField());
+		$Criteria->add_select(
+			$this->getSelectField(),
+			'codigo_cliente'
+		)->add_left_join_with(
+			'asunto', 
+			CriteriaRestriction::equals(
+				'asunto.codigo_asunto',
+				'trabajo.codigo_asunto'
+			)
+		)->add_left_join_with(
+			'cliente',
+			CriteriaRestriction::equals(
+				'cliente.codigo_cliente',
+				'asunto.codigo_cliente'
+			)
+		)->add_grouping(
+			$this->getGroupField()
+		)->add_ordering(
+			$this->getOrderField()
+		);
 
 		return $Criteria;
 	}
