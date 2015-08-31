acb679b5d534e9bfc640ef3ce0b02d0d770e7ada
Daniel Ochoa John <dochoaj@gmail.com>
2015-08-31 15:31:21 -0300

    Clientes filter

--- app/layers/report.support/filters/ClientesFilter.php
+++ app/layers/report.support/filters/ClientesFilter.php
@@ -3,7 +3,11 @@
 class ClientesFilter extends AbstractUndependantFilterTranslator {
 
 	function getFieldName() {
-		return 'cliente.codigo_cliente';
+		if (Conf::GetConf($this->Session, 'CodigoSecundario')) {
+			return 'cliente.codigo_cliente_secundario';
+		} else {
+			return 'cliente.codigo_cliente'
+		}
 	}
 
 	function translateForCharges(Criteria $criteria) {
@@ -13,21 +17,57 @@ class ClientesFilter extends AbstractUndependantFilterTranslator {
 		)->add_select(
 			$this->getFieldName()
 		)->add_left_join_with(
-			'contrato', CriteriaRestriction::equals(
+			'contrato',
+			CriteriaRestriction::equals(
 				'contrato.id_contrato', 'cobro.id_contrato'
 			)
 		)->add_left_join_with(
-			'cliente', CriteriaRestriction::equals(
+			'cliente',
+			CriteriaRestriction::equals(
 				'cliente.codigo_cliente', 'contrato.codigo_cliente'
 			)
 		);
 	}
 
 	function translateForErrands(Criteria $criteria) {
-
+		return $this->addData(
+			$this->getFilterData(), 
+			$criteria
+		)->add_select(
+			$this->getFieldName()
+		)->add_left_join_with(
+			'asunto',
+			CriteriaRestriction::equals(
+				'asunto.codigo_asunto', 
+				'tramite.codigo_asunto'
+			)
+		)->add_left_join_with(
+			'cliente',
+			CriteriaRestriction::equals(
+				'cliente.codigo_cliente',
+				'asunto.codigo_cliente'
+			)
+		)
 	}
 
 	function translateForWorks(Criteria $criteria) {
-
+		return $this->addData(
+			$this->getFilterData(), 
+			$criteria
+		)->add_select(
+			$this->getFieldName()
+		)->add_left_join_with(
+			'asunto',
+			CriteriaRestriction::equals(
+				'asunto.codigo_asunto', 
+				'trabajo.codigo_asunto'
+			)
+		)->add_left_join_with(
+			'cliente',
+			CriteriaRestriction::equals(
+				'cliente.codigo_cliente',
+				'asunto.codigo_cliente'
+			)
+		)
 	}
 }
