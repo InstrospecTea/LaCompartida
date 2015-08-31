880696ec50e3decd922c5bb9461e49a186ba9ef2
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-08-31 17:36:12 -0300

    remove pr

--- app/layers/report.support/calculators/AbstractDataCalculator.php
+++ app/layers/report.support/calculators/AbstractDataCalculator.php
@@ -133,17 +133,17 @@ abstract class AbstractDataCalculator implements IDataCalculator {
 		$results = array();
 
 		if (!empty($this->WorksCriteria)) {
-			pr($this->WorksCriteria->get_plain_query());
+			// pr($this->WorksCriteria->get_plain_query());
 			$results = array_merge($results, $this->WorksCriteria->run());
 		}
 
 		if (!empty($this->ErrandsCriteria)) {
-			pr($this->ErrandsCriteria->get_plain_query());
+			// pr($this->ErrandsCriteria->get_plain_query());
 			$results = array_merge($results, $this->ErrandsCriteria->run());
 		}
 
 		if (!empty($this->ChargesCriteria)) {
-			pr($this->ChargesCriteria->get_plain_query());
+			// pr($this->ChargesCriteria->get_plain_query());
 			$results = array_merge($results, $this->ChargesCriteria->run());
 		}
 
