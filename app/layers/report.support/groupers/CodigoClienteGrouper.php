c96defc7b5ffd305c1f42cef2c543cb57fd80025
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-08-31 16:22:36 -0300

    Documentacion codigo cliente grouper

--- app/layers/report.support/groupers/CodigoClienteGrouper.php
+++ app/layers/report.support/groupers/CodigoClienteGrouper.php
@@ -1,19 +1,44 @@
 <?php
-
+/**
+ * Agrupador por Código de cliente:
+ *
+ * * Agrupa por: cliente.codigo_cliente
+ * * Muestra: cliente.codigo_cliente'
+ * * Ordena por: cliente.codigo_cliente
+ *
+ * Más info en: https://github.com/LemontechSA/ttb/wiki/Reporte-Agrupador:-Codigo-Cliente
+ */
 class CodigoClienteGrouper extends AbstractGrouperTranslator {
 
+	/**
+	 * Obtiene el campo por el cual se agrupará la query
+	 * @return String Campo por el que se agrupa en par tabla.campo o alias
+	 */
 	function getGroupField() {
 		return 'cliente.codigo_cliente';
 	}
 
+	/**
+	 * Obtiene el campo de grupo que se devolverá en el SELECT de la query
+	 * @return String par tabla.campo o alias de función
+	 */
 	function getSelectField() {
 		return 'cliente.codigo_cliente';
 	}
 
+	/**
+	 * Obtiene el campo de grupo por el cual se ordenará la query
+	 * @return String par tabla.campo o alias de función
+	 */
 	function getOrderField() {
 		return 'cliente.codigo_cliente';
 	}
 
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Cobros
+	 * Código del cliente de cada asunto incluido en la liquidación
+	 * @return void
+	 */
 	function translateForCharges(Criteria $Criteria) {
 		$Criteria
 			->add_select($this->getSelectField(), 'codigo_cliente')
@@ -24,6 +49,11 @@ class CodigoClienteGrouper extends AbstractGrouperTranslator {
 		return $Criteria;
 	}
 
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Trámites
+	 * Código del cliente del asunto del trámite
+	 * @return void
+	 */
 	function translateForErrands(Criteria $Criteria) {
 		$Criteria
 			->add_select($this->getSelectField(), 'codigo_cliente')
@@ -35,6 +65,11 @@ class CodigoClienteGrouper extends AbstractGrouperTranslator {
 		return $Criteria;
 	}
 
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Trabajos
+	 * código del cliente del asunto del trabajo
+	 * @return void
+	 */
 	function translateForWorks(Criteria $Criteria) {
 		$Criteria
 			->add_select($this->getSelectField(), 'codigo_cliente')
--- app/layers/report.support/groupers/GlosaAsuntoConCodigoGrouper.php
+++ app/layers/report.support/groupers/GlosaAsuntoConCodigoGrouper.php
@@ -1,19 +1,44 @@
 <?php
-
+/**
+ * Agrupador por Glosa asunto con Código:
+ *
+ * * Agrupa por: asunto.codigo_asunto
+ * * Muestra: asunto.codigo_asunto concatenado con asunto.glosa_asunto
+ * * Ordena por: asunto.codigo_asunto concatenado con asunto.glosa_asunto
+ *
+ * Más info en: https://github.com/LemontechSA/ttb/wiki/Reporte-Agrupador:-Glosa-Asunto-Codigo
+ */
 class GlosaAsuntoConCodigoGrouper extends AbstractGrouperTranslator {
 
+	/**
+	 * Obtiene el campo por el cual se agrupará la query
+	 * @return String Campo por el que se agrupa en par tabla.campo o alias
+	 */
 	function getGroupField() {
 		return 'asunto.codigo_asunto';
 	}
 
+	/**
+	 * Obtiene el campo de grupo que se devolverá en el SELECT de la query
+	 * @return String par tabla.campo o alias de función
+	 */
 	function getSelectField() {
 		return "CONCAT(asunto.codigo_asunto, ': ', asunto.glosa_asunto)";
 	}
 
+	/**
+	 * Obtiene el campo de grupo por el cual se ordenará la query
+	 * @return String par tabla.campo o alias de función
+	 */
 	function getOrderField() {
 		return "CONCAT(asunto.codigo_asunto, ': ', asunto.glosa_asunto)";
 	}
 
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Cobros
+	 * Glosa Código de cada asunto incluido en la liquidación
+	 * @return void
+	 */
 	function translateForCharges(Criteria $Criteria) {
 		$Criteria
 			->add_select($this->getSelectField(), 'glosa_asunto_con_codigo')
@@ -27,6 +52,11 @@ class GlosaAsuntoConCodigoGrouper extends AbstractGrouperTranslator {
 		return $Criteria;
 	}
 
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Trámites
+	 * Glosa y Código del asunto del trámite
+	 * @return void
+	 */
 	function translateForErrands(Criteria $Criteria) {
 		$Criteria
 			->add_select($this->getSelectField(), 'glosa_asunto_con_codigo')
@@ -39,6 +69,11 @@ class GlosaAsuntoConCodigoGrouper extends AbstractGrouperTranslator {
 		return $Criteria;
 	}
 
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Trabajos
+	 * Glosa y código del asunto del trabajo
+	 * @return void
+	 */
 	function translateForWorks(Criteria $Criteria) {
 		$Criteria
 			->add_select($this->getSelectField(), 'glosa_asunto_con_codigo')
