d1dfc86c4dd28438f5bdc4a245bed78d42de034a
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-09-01 11:06:52 -0300

    Forma de cobro y algunas mejoras

--- app/layers/report.support/groupers/CodigoAsuntoGrouper.php
+++ app/layers/report.support/groupers/CodigoAsuntoGrouper.php
@@ -10,79 +10,79 @@
  */
 class CodigoAsuntoGrouper extends AbstractGrouperTranslator {
 
-  /**
-   * Obtiene el campo por el cual se agrupará la query
-   * @return String Campo por el que se agrupa en par tabla.campo o alias
-   */
-  function getGroupField() {
-    return $this->getProjectCodeField();
-  }
+	/**
+	 * Obtiene el campo por el cual se agrupará la query
+	 * @return String Campo por el que se agrupa en par tabla.campo o alias
+	 */
+	function getGroupField() {
+		return $this->getProjectCodeField();
+	}
 
-  /**
-   * Obtiene el campo de grupo que se devolverá en el SELECT de la query
-   * @return String par tabla.campo o alias de función
-   */
-  function getSelectField() {
-    return $this->getProjectCodeField();
-  }
+	/**
+	 * Obtiene el campo de grupo que se devolverá en el SELECT de la query
+	 * @return String par tabla.campo o alias de función
+	 */
+	function getSelectField() {
+		return $this->getProjectCodeField();
+	}
 
-  /**
-   * Obtiene el campo de grupo por el cual se ordenará la query
-   * @return String par tabla.campo o alias de función
-   */
-  function getOrderField() {
-    return $this->getProjectCodeField();
-  }
+	/**
+	 * Obtiene el campo de grupo por el cual se ordenará la query
+	 * @return String par tabla.campo o alias de función
+	 */
+	function getOrderField() {
+		return $this->getProjectCodeField();
+	}
 
-  /**
-   * Traduce los keys de agrupadores a campos para la query de Cobros
-   * Código de cada asunto incluido en la liquidación
-   * @return void
-   */
-  function translateForCharges(Criteria $Criteria) {
-    $Criteria
-      ->add_select($this->getSelectField(), 'codigo_asunto')
-      ->add_grouping($this->getGroupField())
-      ->add_ordering($this->getOrderField())
-      ->add_left_join_with('cobro_asunto',
-        CriteriaRestriction::equals('cobro_asunto.id_cobro', 'cobro.id_cobro'))
-      ->add_left_join_with('asunto',
-        CriteriaRestriction::equals('asunto.codigo_asunto', 'cobro_asunto.codigo_asunto'));
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Cobros
+	 * Código de cada asunto incluido en la liquidación
+	 * @return void
+	 */
+	function translateForCharges(Criteria $Criteria) {
+		$Criteria
+			->add_select($this->getSelectField(), 'codigo_asunto')
+			->add_grouping($this->getGroupField())
+			->add_ordering($this->getOrderField())
+			->add_left_join_with('cobro_asunto',
+				CriteriaRestriction::equals('cobro_asunto.id_cobro', 'cobro.id_cobro'))
+			->add_left_join_with('asunto',
+				CriteriaRestriction::equals('asunto.codigo_asunto', 'cobro_asunto.codigo_asunto'));
 
-    return $Criteria;
-  }
+		return $Criteria;
+	}
 
-  /**
-   * Traduce los keys de agrupadores a campos para la query de Trámites
-   * Código del asunto del trámite
-   * @return void
-   */
-  function translateForErrands(Criteria $Criteria) {
-    $Criteria
-      ->add_select($this->getSelectField(), 'codigo_asunto')
-      ->add_grouping($this->getGroupField())
-      ->add_ordering($this->getOrderField())
-      ->add_left_join_with('asunto',
-        CriteriaRestriction::equals('asunto.codigo_asunto', 'tramite.codigo_asunto')
-      );
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Trámites
+	 * Código del asunto del trámite
+	 * @return void
+	 */
+	function translateForErrands(Criteria $Criteria) {
+		$Criteria
+			->add_select($this->getSelectField(), 'codigo_asunto')
+			->add_grouping($this->getGroupField())
+			->add_ordering($this->getOrderField())
+			->add_left_join_with('asunto',
+				CriteriaRestriction::equals('asunto.codigo_asunto', 'tramite.codigo_asunto')
+			);
 
-    return $Criteria;
-  }
+		return $Criteria;
+	}
 
-  /**
-   * Traduce los keys de agrupadores a campos para la query de Trabajos
-   * Código del asunto del trabajo
-   * @return void
-   */
-  function translateForWorks(Criteria $Criteria) {
-    $Criteria
-      ->add_select($this->getSelectField(), 'codigo_asunto')
-      ->add_grouping($this->getGroupField())
-      ->add_ordering($this->getOrderField())
-      ->add_left_join_with('asunto',
-        CriteriaRestriction::equals('asunto.codigo_asunto', 'trabajo.codigo_asunto')
-      );
+	/**
+	 * Traduce los keys de agrupadores a campos para la query de Trabajos
+	 * Código del asunto del trabajo
+	 * @return void
+	 */
+	function translateForWorks(Criteria $Criteria) {
+		$Criteria
+			->add_select($this->getSelectField(), 'codigo_asunto')
+			->add_grouping($this->getGroupField())
+			->add_ordering($this->getOrderField())
+			->add_left_join_with('asunto',
+				CriteriaRestriction::equals('asunto.codigo_asunto', 'trabajo.codigo_asunto')
+			);
 
-    return $Criteria;
-  }
+		return $Criteria;
+	}
 }
