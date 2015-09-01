02200113dbeab1a1b2311a06cf7b39cf46a860df
Daniel Ochoa <dochoaj@gmail.com>
2015-09-01 11:56:52 -0300

    Agrega documentacion para tipo asunto

--- app/layers/report.support/filters/TipoAsuntoFilter.php
+++ app/layers/report.support/filters/TipoAsuntoFilter.php
@@ -1,76 +1,81 @@
 <?php
-
+/**
+ * Filtro por tipo asunto:
+ *
+ * * Filtra por:
+ * * Cobros: asunto.id_tipo_asunto
+ * * Trámites: asunto.id_tipo_asunto
+ * * Trabajos: asunto.id_tipo_asunto
+ *
+ * Más info en: https://github.com/LemontechSA/ttb/wiki/Reporte-Filtro:-Tipo-Asunto
+ *
+ */
 class TipoAsuntoFilter extends AbstractUndependantFilterTranslator {
 
+	/**
+	 * Obtiene el nombre del campo que se filtrará
+	 * @return String
+	 */
 	function getFieldName() {
-		return 'prm_tipo_proyecto.glosa_tipo_proyecto';
+		return 'asunto.id_tipo_asunto';
 	}
 
+	/**
+	 * Traduce el filtro para el caso de los cobros
+	 * @param  Criteria $criteria Query builder asociado a los cobros
+	 * @return Criteria Query builder con las restricciones del filtro ya aplicadas.
+	 */
 	function translateForCharges(Criteria $criteria) {
 		return $this->addData(
 			$this->getFilterData(),
 			$criteria
-		)->add_select(
-			$this->getFieldName()
 		)->add_left_join_with(
 			'cobro_asunto',
 			CriteriaRestriction::equals(
-				'cobro_asunto.id_cobro', 
+				'cobro_asunto.id_cobro',
 				'cobro.id_cobro'
 			)
 		)->add_left_join_with(
 			'asunto',
 			CriteriaRestriction::equals(
-				'asunto.codigo_asunto', 
+				'asunto.codigo_asunto',
 				'cobro_asunto.codigo_asunto'
-			)
-		)->add_left_join_with(
-			'prm_tipo_proyecto',
-			CriteriaRestriction::equals(
-				'prm_tipo_proyecto.id_tipo_proyecto',
-				'asunto.id_tipo_asunto'
 			)
 		);
 	}
 
+	/**
+	 * Traduce el filtro para el caso de los trámites
+	 * @param  Criteria $criteria Query builder asociado a los trámites
+	 * @return Criteria Query builder con las restricciones del filtro ya aplicadas.
+	 */
 	function translateForErrands(Criteria $criteria) {
 		return $this->addData(
 			$this->getFilterData(),
 			$criteria
-		)->add_select(
-			$this->getFieldName()
 		)->add_left_join_with(
 			'asunto',
 			CriteriaRestriction::equals(
-				'asunto.codigo_asunto', 
+				'asunto.codigo_asunto',
 				'tramite.codigo_asunto'
-			)
-		)->add_left_join_with(
-			'prm_tipo_proyecto',
-			CriteriaRestriction::equals(
-				'prm_tipo_proyecto.id_tipo_proyecto',
-				'asunto.id_tipo_asunto'
 			)
 		);
 	}
 
+	/**
+	 * Traduce el filtro para el caso de los trabajos
+	 * @param  Criteria $criteria Query builder asociado a los trabajos
+	 * @return Criteria Query builder con las restricciones del filtro ya aplicadas.
+	 */
 	function translateForWorks(Criteria $criteria) {
 		return $this->addData(
 			$this->getFilterData(),
 			$criteria
-		)->add_select(
-			$this->getFieldName()
 		)->add_left_join_with(
 			'asunto',
 			CriteriaRestriction::equals(
-				'asunto.codigo_asunto', 
+				'asunto.codigo_asunto',
 				'trabajo.codigo_asunto'
-			)
-		)->add_left_join_with(
-			'prm_tipo_proyecto',
-			CriteriaRestriction::equals(
-				'prm_tipo_proyecto.id_tipo_proyecto',
-				'asunto.id_tipo_asunto'
 			)
 		);
 	}
