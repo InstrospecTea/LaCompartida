6a36a79290f71b60c03bad97b5d51519b1e51dbd
Daniel Ochoa <dochoaj@gmail.com>
2015-09-01 12:06:21 -0300

    Filtro para usuarios documentado

--- app/layers/report.support/filters/UsuariosFilter.php
+++ app/layers/report.support/filters/UsuariosFilter.php
@@ -1,15 +1,39 @@
 <?php
-
+/**
+ * Filtro por usuarios:
+ *
+ * * Filtra por:
+ * * Cobros: no aplica
+ * * Trámites: usuario.id_usuario
+ * * Trabajos: usuario.id_usuario
+ *
+ * Más info en: https://github.com/LemontechSA/ttb/wiki/Reporte-Filtro:-Usuario
+ *
+ */
 class UsuariosFilter extends AbstractUndependantFilterTranslator {
 
+	/**
+	 * Obtiene el nombre del campo que se filtrará
+	 * @return String
+	 */
 	function getFieldName() {
 		return 'usuario.id_usuario';
 	}
 
+	/**
+	 * Traduce el filtro para el caso de los cobros
+	 * @param  Criteria $criteria Query builder asociado a los cobros
+	 * @return Criteria Query builder con las restricciones del filtro ya aplicadas.
+	 */
 	function translateForCharges(Criteria $criteria) {
 		return $criteria;
 	}
 
+	/**
+	 * Traduce el filtro para el caso de los trámites
+	 * @param  Criteria $criteria Query builder asociado a los trámites
+	 * @return Criteria Query builder con las restricciones del filtro ya aplicadas.
+	 */
 	function translateForErrands(Criteria $criteria) {
 		return $this->addData(
 			$this->getFilterData(),
@@ -23,6 +47,11 @@ class UsuariosFilter extends AbstractUndependantFilterTranslator {
 		);
 	}
 
+	/**
+	 * Traduce el filtro para el caso de los trabajos
+	 * @param  Criteria $criteria Query builder asociado a los trabajos
+	 * @return Criteria Query builder con las restricciones del filtro ya aplicadas.
+	 */
 	function translateForWorks(Criteria $criteria) {
 		return $this->addData(
 			$this->getFilterData(),
