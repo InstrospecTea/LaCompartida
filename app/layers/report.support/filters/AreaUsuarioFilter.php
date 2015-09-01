558c26190f025005e38dd7c35f61c1661ec84791
Daniel Ochoa <dochoaj@gmail.com>
2015-09-01 11:57:44 -0300

    Documentación para area usuario filter

--- app/layers/report.support/filters/AreaUsuarioFilter.php
+++ app/layers/report.support/filters/AreaUsuarioFilter.php
@@ -1,58 +1,66 @@
 <?php
-
+/**
+ * Filtro por area usuario:
+ *
+ * * Filtra por:
+ * * Cobros: no aplica
+ * * Trámites: usuario.id_area_usuario
+ * * Trabajos: usuario.id_area_usuario
+ *
+ * Más info en: https://github.com/LemontechSA/ttb/wiki/Reporte-Filtro:-Area-Usuario
+ *
+ */
 class AreaUsuarioFilter extends AbstractUndependantFilterTranslator {
 
-	function getSelect() {
-		return "IFNULL(prm_area_usuario.glosa, '-')";
-	}
-
+	/**
+	 * Obtiene el nombre del campo que se filtrarÃ¡
+	 * @return String
+	 */
 	function getFieldName() {
-		return 'prm_area_usuario.glosa';
+		return 'usuario.id_area_usuario';
 	}
 
+	/**
+	 * Traduce el filtro para el caso de los cobros
+	 * @param  Criteria $criteria Query builder asociado a los cobros
+	 * @return Criteria Query builder con las restricciones del filtro ya aplicadas.
+	 */
 	function translateForCharges(Criteria $criteria) {
 		return $criteria;
 	}
 
+	/**
+	 * Traduce el filtro para el caso de los trÃ¡mites
+	 * @param  Criteria $criteria Query builder asociado a los trámites
+	 * @return Criteria Query builder con las restricciones del filtro ya aplicadas.
+	 */
 	function translateForErrands(Criteria $criteria) {
 		return $this->addData(
 			$this->getFilterData(),
 			$criteria
-		)->add_select(
-			$this->getSelect(),
-			"'prm_area_usuario.glosa'"
 		)->add_left_join_with(
 			'usuario',
 			CriteriaRestriction::equals(
-				'usuario.id_usuario', 
+				'usuario.id_usuario',
 				'tramite.id_usuario'
-			)
-		)->add_left_join_with(
-			'prm_area_usuario',
-			CriteriaRestriction::equals(
-				'prm_area_usuario.id',
-				'usuario.id_area_usuario'
 			)
 		);
 	}
 
+	/**
+	 * Traduce el filtro para el caso de los trabajos
+	 * @param  Criteria $criteria Query builder asociado a los trabajos
+	 * @return Criteria Query builder con las restricciones del filtro ya aplicadas.
+	 */
 	function translateForWorks(Criteria $criteria) {
 		return $this->addData(
 			$this->getFilterData(),
 			$criteria
-		)->add_select(
-			$this->getSelect()
 		)->add_left_join_with(
 			'usuario',
 			CriteriaRestriction::equals(
-				'usuario.id_usuario', 
+				'usuario.id_usuario',
 				'trabajo.id_usuario'
-			)
-		)->add_left_join_with(
-			'prm_area_usuario',
-			CriteriaRestriction::equals(
-				'prm_area_usuario.id',
-				'usuario.id_area_usuario'
 			)
 		);
 	}
