b7962cf0c37e444c7bd3abf8232edf69d15b2674
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-09-24 18:11:55 -0300

    Recover cap servers and rubocop

--- config/cap_servers_production.rb
+++ config/cap_servers_production.rb
@@ -1,17 +1,16 @@
-require 'aws-sdk'
+# require 'aws-sdk'
 
 ec2 = AWS::EC2.new(
-  :access_key_id     => "AKIAJSLZNMWQ3H3BN3WA",
-  :secret_access_key => "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"
+  access_key_id: 'AKIAJSLZNMWQ3H3BN3WA',
+  secret_access_key: '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'
 )
 
-instances = ec2.instances
-    .tagged('environment')
-    .tagged_values('production')
-    .tagged('app')
-    .tagged_values('ttb-c')
-    .each { |instance|
-        puts "#{instance.dns_name} #{instance.tags.username}"
-        roles = instance.tags.roles.split(',').map &:to_sym
-        # server instance.dns_name, *roles, {:user => instance.tags.username, :port => 22}
-    }
+ec2.instances
+  .tagged('environment')
+  .tagged_values('production')
+  .tagged('app')
+  .tagged_values('ttb-c')
+  .each do |instance|
+    roles = instance.tags.roles.split(',').map(&:to_sym)
+    server instance.dns_name, *roles, user: instance.tags.username, port: 22
+  end
