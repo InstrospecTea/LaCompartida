89fdbdd6d8d81788c0f7e297548d280d560c7579
Nicolás Torres <ntorresj@gmail.com>
2015-11-09 13:31:58 -0300

    remove id

--- app/interfaces/genera_cobros.php
+++ app/interfaces/genera_cobros.php
@@ -1288,7 +1288,7 @@ function funcionTR(& $contrato) {
 	$html .= "</b></td>";
 
 	$lista_asuntos = $contrato->MattersByContract($contrato->fields['id_contrato']);
-	$html .= "<td style='font-size:10px' align='left' id='tip_{$i}' valing='top'><b>{$lista_asuntos}</b></td>";
+	$html .= "<td style='font-size:10px' align='left' valing='top'><b>{$lista_asuntos}</b></td>";
 	$html .= "<td style='font-size:10px' align=center valing=top><b>" . $fecha_ultimo_cobro . "</b></td>";
 
 	if ($contrato->fields['forma_cobro'] == 'RETAINER' || $contrato->fields['forma_cobro'] == 'PROPORCIONAL') {
