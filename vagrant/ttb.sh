70bd45f3a6a5d817b17281f9dae56bc9f07d31ff
Nicolás Torres <ntorresj@gmail.com>
2015-11-25 10:47:34 -0300

    remove register_globals

--- vagrant/ttb.sh
+++ vagrant/ttb.sh
@@ -36,7 +36,6 @@ sed -i "s/html_errors = Off/html_errors = On/" /etc/php5/apache2/php.ini
 sed -i "s/short_open_tag = Off/short_open_tag = On/g" /etc/php5/apache2/php.ini
 sed -i 's/;default_charset = "iso-8859-1"/default_charset = "iso-8859-1"/g' /etc/php5/apache2/php.ini
 sed -i "s/error_reporting = E_ALL & ~E_DEPRECATED/error_reporting = E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR/g" /etc/php5/apache2/php.ini
-sed -i "s/register_globals = Off/register_globals = On/g" /etc/php5/apache2/php.ini
 
 # Configuracion local de la conexion a la base de datos
 if [ ! -f /vagrant/ttb/app/miconf.php ]; then
