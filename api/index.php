b25654692cc08e4c84ab3ecca5141e42b4da3738
Matias Orellana <morellan@gmail.com>
2015-11-25 10:07:18 -0300

    API OPTIONS

--- api/index.php
+++ api/index.php
@@ -1,6 +1,22 @@
 <?php
 require_once dirname(__FILE__) . '/../app/conf.php';
 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+	header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+	header('Access-Control-Allow-Credentials: true');
+	// header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+		header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+	}
+
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+	}
+}
+
 $Slim = new Slim();
 $Session = new Sesion();
 
@@ -9,6 +25,10 @@ define(MAX_TIMESTAMP, 4182191999);
 
 // header('Access-Control-Allow-Origin: *');
 // header('Access-Control-Allow-Methods: GET, POST, PUT');
+
+$Slim->map(':x+', function($x) {
+	http_response_code(200);
+})->via('OPTIONS');
 
 $Slim->post('/login', function () use ($Session, $Slim) {
 	$UserToken = new UserToken($Session);
