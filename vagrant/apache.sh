69786fcbfe7631e04ad6aa43d44bc2fc50ec5b54
Nicolás Torres <ntorresj@gmail.com>
2015-12-02 15:40:49 -0300

    corrección a2enmod rewrite

--- vagrant/apache.sh
+++ vagrant/apache.sh
@@ -5,7 +5,6 @@ apt-get -y install apache2 apache2-utils
 
 if [ ! -L /var/www/vagrant ]; then
     sed -i "s/\/var\/www\/html/\/var\/www\/vagrant/" /etc/apache2/sites-available/000-default.conf
-    sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/sites-available/000-default.conf
     ln -s /vagrant /var/www/vagrant
 fi
 
--- vagrant/ttb.sh
+++ vagrant/ttb.sh
@@ -28,6 +28,7 @@ mysql -u root -p$MYSQL_ROOT_PASS timetracking < /vagrant/ttb/vagrant/database.ex
 # TTB Clasic tiene su codigo con encodign ISO-8859-1
 echo "AddDefaultCharset ISO-8859-1" >> /etc/apache2/conf-available/charset.conf
 echo "AddCharset ISO-8859-1 .iso8859-1 .latin1" >> /etc/apache2/conf-available/charset.conf
+sed -i '166 s/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf
 
 # PHP
 
@@ -37,6 +38,7 @@ sed -i "s/short_open_tag = Off/short_open_tag = On/g" /etc/php5/apache2/php.ini
 sed -i 's/default_charset = "UTF-8"/default_charset = "iso-8859-1"/g' /etc/php5/apache2/php.ini
 sed -i "s/error_reporting = E_ALL & ~E_DEPRECATED/error_reporting = E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR/g" /etc/php5/apache2/php.ini
 sed -i "s/max_input_vars = 1000/max_input_vars = 100/g" /etc/php5/apache2/php.ini
+service apache2 restart
 
 # Configuracion local de la conexion a la base de datos
 if [ ! -f /vagrant/ttb/app/miconf.php ]; then
@@ -56,4 +58,3 @@ curl -I "http://localhost/ttb/app/update.php?hash=c85ef9997e6a30032a765a20ee6963
 apt-get update
 apt-get install wkhtmltopdf -y
 ln -s /usr/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
-
