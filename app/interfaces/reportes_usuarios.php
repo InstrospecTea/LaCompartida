b272bea2cb125768c1192d5c73f87898a250094b
Sebastián González <brutalchrist@gmail.com>
2015-12-04 11:50:26 -0300

    Pantalla Reporte Gráfico Usuarios 📊

--- app/interfaces/reportes_usuarios.php
+++ app/interfaces/reportes_usuarios.php
@@ -5,84 +5,104 @@
 	$pagina = new Pagina($sesion);
 	$Form = new Form($sesion);
 
-    if($id_usuario == "")
-        $id_usuario = $sesion->usuario->fields['id_usuario'];
+  if($id_usuario == "")
+		$id_usuario = $sesion->usuario->fields['id_usuario'];
+
+	if (!$fecha1) {
+		$fecha1 = date("d-m-Y", strtotime("- 1 month"));
+	}
+	if (!$fecha2) {
+		$fecha2 = date("d-m-Y");
+	}
 
 	$pagina->titulo = __('Reporte Gráfico Usuarios');
 	$pagina->PrintTop();
 ?>
 
-<form method=post action="<?= $_SERVER[PHP_SELF] ?>">
-<input type=hidden name=opcion value="desplegar" />
+<script src="../../public/vendors/Chart.js/Chart.js" ></script>
 
+<form method="post" action="<?php echo $_SERVER[PHP_SELF]; ?>">
 <table class="border_plomo tb_base">
 	<tr>
-		<td align=right>
-			<?=__('Fecha desde')?>
+		<td align="right">
+			<?php echo __('Fecha desde');?>
 		</td>
-		<td align=left>
-			<?= Html::PrintCalendar("fecha1", "$fecha1"); ?>
-		</td>
-	</tr>
-	<tr>
-		<td align=right>
-			<?=__('Fecha hasta')?>
-		</td>
-		<td align=left>
-			<?= Html::PrintCalendar("fecha2", "$fecha2"); ?>
+		<td align="left">
+			<input class="fechadiff" type="text" name="fecha1" value="<?php echo $fecha1 ?>" id="fecha1" size="11" maxlength="10" />
 		</td>
 	</tr>
 	<tr>
-		<td align=right>
-			<?=__('Usuario')?>
+		<td align="right">
+			<?php echo __('Fecha hasta'); ?>
 		</td>
-		<td align=left><!-- Nuevo Select -->
+		<td align="left">
+			<input class="fechadiff" type="text" name="fecha2" value="<?php echo $fecha2 ?>" id="fecha2" size="11" maxlength="10" />
+		</td>
+	</tr>
+	<tr>
+		<td align="right">
+			<?php echo __('Usuario');?>
+		</td>
+		<td align="left"><!-- Nuevo Select -->
 			<?php echo $Form->select('id_usuario', $sesion->usuario->ListarActivos('', 'PRO'), $id_usuario, array('empty' => FALSE, 'style' => 'width: 200px')); ?>
 		</td>
 	</tr>
 	<tr>
-		<td align=right>
-			<?=__('Tipo de reporte')?>
+		<td align="right">
+			<?php echo __('Tipo de reporte'); ?>
 		</td>
-		<td align=left>
-			<select name="tipo_reporte">
-				<option <?= $tipo_reporte == "proyectos_trabajados" ? "selected" : "" ?> value="proyectos_trabajados"><?=__('Asuntos trabajados')?></option>
+		<td align="left">
+			<select id="tipo_reporte" name="tipo_reporte">
+				<option <?php echo $tipo_reporte == "proyectos_trabajados" ? "selected" : ""; ?> value="proyectos_trabajados"><?php echo __('Asuntos trabajados'); ?></option>
 			</select>
 		</td>
 	</tr>
 	<tr>
-		<td colspan=4 align=right>
-			<input type=submit class=btn value="<?=__('Generar reporte')?>"/>
+		<td colspan="4" align="right">
+			<input type="button" class="btn" id="genera_reporte" value="<?php echo __('Generar reporte'); ?>">
 		</td>
 	</tr>
-
 </table>
-
 </form>
 
-<?
-	if($opcion == "desplegar")
-	{
-?>
-		<br />
-		<img src=graficos/grafico_<?=$tipo_reporte?>.php?id_usuario=<?=$id_usuario?>&fecha1=<?=$fecha1?>&fecha2=<?=$fecha2?> alt='' />
-<?
-	}
+<canvas id="grafico_usuarios" width="600" height="400"></canvas>
 
-
-?>
 <script type="text/javascript">
 <!-- //
-function setDateDefecto()
-{
-    hoy = new Date();//tiene hora actual
-    hoy.setHours(0,0,0,0);
-    ninety_days = new Date();
-    ninety_days.setDate(hoy.getDate()-30);
+jQuery(function() {
+	var graficoBarraUsuarios;
 
-    if(fecha1_Object.picked.date.getTime() == hoy.getTime())
-        fecha1_Object.setValor(ninety_days);
-}
+	jQuery("#genera_reporte").on("click", function() {
+		var id_usuario = jQuery("#id_usuario").val();
+		var fecha1 = jQuery("#fecha1").val();
+		var fecha2 = jQuery("#fecha2").val();
+
+		jQuery.ajax({
+			url: 'graficos/grafico_' + jQuery("#tipo_reporte").val() + '.php',
+			data: {
+				'id_usuario': id_usuario,
+				'fecha1': fecha1,
+				'fecha2': fecha2
+			},
+			dataType: 'json',
+			type: 'POST',
+			success: function(respuesta) {
+				var canvas = jQuery("#grafico_usuarios")[0];
+				var context = canvas.getContext('2d');
+
+				if (graficoBarraUsuarios) {
+					graficoBarraUsuarios.destroy();
+				}
+
+				graficoBarraUsuarios = new Chart(context).Bar(respuesta);
+			},
+			error: function(e) {
+				console.log(e);
+			}
+		});
+	});
+});
+
 function Habilitar(form)
 {
 	if(form.tipo_reporte.selectedIndex > 0)
@@ -90,7 +110,6 @@ function Habilitar(form)
 	else
 		form.codigo_asunto.disabled = false;
 }
-setDateDefecto();
 // ->
 </script>
 
