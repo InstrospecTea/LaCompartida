2f027f55adc7e4c224d19e8ef06fc76aff429df8
Sebastián González <brutalchrist@gmail.com>
2015-12-04 09:58:16 -0300

    Pantalla Reportes Gráfico asuntos (me duele un poco el script src)

--- app/interfaces/reportes_asuntos.php
+++ app/interfaces/reportes_asuntos.php
@@ -19,126 +19,122 @@
 		$fecha_ini = "01-".$fecha_mes."-".$fecha_anio;
 	}
 
-
 ?>
+<script src="../../public/vendors/Chart.js/Chart.js" ></script>
 
 <form method='post' name='formulario'>
-<input type=hidden name=opcion value="desplegar">
-
 <table class="border_plomo tb_base">
 	<tr>
-		<td align=right>
-			<?=__('Fecha desde')?>
+		<td align="right">
+			<?php echo __('Fecha desde');?>
 		</td>
-		<td align=left>
-			<input type="text" name="fecha_ini" value="<?=$fecha_ini ? $fecha_ini : date("d-m-Y",strtotime("$hoy - 1 month")) ?>" id="fecha_ini" size="11" maxlength="10" />
-			<img src="<?=Conf::ImgDir()?>/calendar.gif" id="img_fecha_ini" style="cursor:pointer" />
-		</td>
-	</tr>
-	<tr>
-		<td align=right>
-			<?=__('Fecha hasta')?>
-		</td>
-		<td align=left>
-			<input type="text" name="fecha_fin" value="<?=$fecha_fin ? $fecha_fin : date("d-m-Y",strtotime("$hoy")) ?>" id="fecha_fin" size="11" maxlength="10" />
-			<img src="<?=Conf::ImgDir()?>/calendar.gif" id="img_fecha_fin" style="cursor:pointer" />
+		<td align="left">
+			<input type="text" name="fecha_ini" value="<?php echo $fecha_ini ? $fecha_ini : date("d-m-Y",strtotime("$hoy - 1 month")); ?>" id="fecha_ini" size="11" maxlength="10" />
+			<img src="<?php echo Conf::ImgDir(); ?>/calendar.gif" id="img_fecha_ini" style="cursor:pointer" />
 		</td>
 	</tr>
 	<tr>
-		<td align=right>
-			<?=__('Clientes')?>
+		<td align="right">
+			<?php echo __('Fecha hasta'); ?>
 		</td>
-		<td align=left>
-	  		<?=Html::SelectQuery($sesion,"SELECT codigo_cliente, glosa_cliente AS nombre FROM cliente WHERE 1 ORDER BY nombre ASC", "clientes[]",$clientes,"class=\"selectMultiple\" multiple size=6 ","","230"); ?>
+		<td align="left">
+			<input type="text" name="fecha_fin" value="<?php echo $fecha_fin ? $fecha_fin : date("d-m-Y",strtotime("$hoy")); ?>" id="fecha_fin" size="11" maxlength="10" />
+			<img src="<?php echo Conf::ImgDir(); ?>/calendar.gif" id="img_fecha_fin" style="cursor:pointer" />
 		</td>
 	</tr>
 	<tr>
-		<td align=right>
-			<?=__('Profesionales')?>
+		<td align="right">
+			<?php echo __('Clientes'); ?>
 		</td>
-		<td align=left>
+		<td align="left">
+	  		<?php echo Html::SelectQuery($sesion,"SELECT codigo_cliente, glosa_cliente AS nombre FROM cliente WHERE 1 ORDER BY nombre ASC", "clientes",$clientes,"class=\"selectMultiple\" multiple size=6 ","","230"); ?>
+		</td>
+	</tr>
+	<tr>
+		<td align="right">
+			<?php echo __('Profesionales'); ?>
+		</td>
+		<td align="left">
 			<!-- Nuevo Select -->
-            <?php echo $Form->select('usuarios[]', $sesion->usuario->ListarActivos('', 'PRO'), $usuarios, array('empty' => FALSE, 'style' => 'width: 230px', 'class' => 'selectMultiple', 'multiple' => 'multiple', 'size' => '6')); ?>
+      <?php echo $Form->select('usuarios', $sesion->usuario->ListarActivos('', 'PRO'), $usuarios, array('empty' => FALSE, 'style' => 'width: 230px', 'class' => 'selectMultiple', 'multiple' => 'multiple', 'size' => '6')); ?>
 		</td>
 	</tr>
 	<tr>
-		<td align=right>
-			<?=__('Solo activos')?>
+		<td align="right">
+			<?php echo __('Solo activos');?>
 		</td>
 		<td>
 			<? if( $solo_activos ) $chk = "checked='checked'"; ?>
-			<input type="checkbox" name="solo_activos" id="solo_activos" value=1 <?=$chk ?> />
+			<input type="checkbox" name="solo_activos" id="solo_activos" value="1" <?php echo $chk; ?> />
 		</td>
 	</tr>
 	<tr>
-		<td align=right>
-			<?=__('Tipo de reporte')?>
+		<td align="right">
+			<?php echo __('Tipo de reporte');?>
 		</td>
 		<td align=left>
-			<select name="tipo_reporte">
-				<option <?= $tipo_reporte == "hh_por_empleado" ? "selected" : "" ?> value="hh_por_empleado"><?=__('Horas trabajadas por empleado')?></option>
-				<option <?= $tipo_reporte == "hh_por_asunto" ? "selected" : "" ?> value="hh_por_asunto"><?=__('Horas trabajadas por asunto')?></option>
-				<option <?= $tipo_reporte == "hh_por_cliente" ? "selected" : "" ?> value="hh_por_cliente"><?=__('Horas trabajadas por cliente')?></option>
+			<select id="tipo_reporte" name="tipo_reporte">
+				<option <?php $tipo_reporte == "hh_por_empleado" ? "selected" : "" ?> value="hh_por_empleado"><?php echo __('Horas trabajadas por empleado'); ?></option>
+				<option <?php $tipo_reporte == "hh_por_asunto" ? "selected" : "" ?> value="hh_por_asunto"><?php echo __('Horas trabajadas por asunto'); ?></option>
+				<option <?php $tipo_reporte == "hh_por_cliente" ? "selected" : "" ?> value="hh_por_cliente"><?php echo __('Horas trabajadas por cliente'); ?></option>
 			</select>
 		</td>
 	</tr>
 	<tr>
-		<td colspan=3 align=right>
-			<input type=submit class=btn value="<?=__('Generar reporte')?>" onclick=" return Verificar(this.form,'reporte');">
+		<td colspan="3" align="right">
+			<input type="button" class="btn" id="genera_reporte" value="<?php echo __('Generar reporte'); ?>">
 		</td>
-		<td align=right>
-			<input type=submit class=btn value="<?=__('Planilla')?>" onclick=" return Verificar(this.form,'planilla');">
+		<td align="right">
+			<input type="submit" class="btn" value="<?php echo __('Planilla'); ?>" onclick="return Planilla(this.form);">
 		</td>
 	</tr>
-
 </table>
-
 </form>
-<script>
-</script>
-<?
-	if($opcion == "desplegar")
-	{
-		$url_clientes = "&clientes=";
-		if(is_array($clientes))
-			$url_clientes .= implode(',',$clientes);
-		else if($clientes)
-			$url_clientes .= $clientes;
-		else
-			$url_clientes = '';
 
-		$url_usuarios = "&usuarios=";
-		if(is_array($usuarios))
-			$url_usuarios .= implode(',',$usuarios);
-		else if($usuarios)
-			$url_usuarios .= $usuarios;
-		else
-			$url_usuarios = '';
-
-		$url_activos = "&solo_activos=".$solo_activos;
-?>
-		<br />
-		<img src="graficos/grafico_<?=$tipo_reporte?>.php?popup=1<?=$url_clientes?><?=$url_activos?><?=$url_usuarios?>&fecha_ini=<?=Utiles::fecha2sql($fecha_ini)?>&fecha_fin=<?=Utiles::fecha2sql($fecha_fin)?>" alt='' />
-
-		<!--
-		<?
-		echo "graficos/grafico_".$tipo_reporte.".php?clientes=".implode(',',$clientes)."&usuarios=".implode(',',$usuarios)."&fecha_ini=".Utiles::fecha2sql($fecha_ini)."&fecha_fin=".Utiles::fecha2sql($fecha_fin); ?>
-		-->
-
-<?
-	}
-?>
-
+<canvas id="grafico_hito" width="600" height="400"></canvas>
 
 <script type="text/javascript">
 <!-- //
+jQuery(function() {
+	var graficoBarraHito;
 
-function Verificar(form,opc)
-{
-	if(opc == 'planilla')
-		form.action = "planillas/planilla_horas_general.php";
-	else
-		form.action = "reportes_asuntos.php";
+	jQuery("#genera_reporte").on("click", function() {
+		var usuarios = jQuery("#usuarios").val();
+		var clientes = jQuery("#clientes").val();
+		var solo_activos = jQuery("#solo_activos:checked").val();
+		var fecha_ini = jQuery("#fecha_ini").val();
+		var fecha_fin = jQuery("#fecha_fin").val();
+
+		jQuery.ajax({
+			url: 'graficos/grafico_' + jQuery("#tipo_reporte").val() + '.php',
+			data: {
+				'usuarios': usuarios,
+				'clientes': clientes,
+				'solo_activos': solo_activos,
+				'fecha_ini': fecha_ini,
+				'fecha_fin': fecha_fin
+			},
+			dataType: 'json',
+			type: 'POST',
+			success: function(respuesta) {
+				var canvas = jQuery("#grafico_hito")[0];
+				var context = canvas.getContext('2d');
+
+				if (graficoBarraHito) {
+					graficoBarraHito.destroy();
+				}
+
+				graficoBarraHito = new Chart(context).Bar(respuesta);
+			},
+			error: function(e) {
+				console.log(e);
+			}
+		});
+	});
+});
+
+function Planilla(form) {
+	form.action = "planillas/planilla_horas_general.php";
 	form.submit();
 }
 Calendar.setup(
@@ -156,7 +152,6 @@ Calendar.setup(
 	}
 );
 
-// ->
 </script>
 <?
 	$pagina->PrintBottom();
