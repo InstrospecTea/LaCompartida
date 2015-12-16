e4b1bfc364cce26e5f1e72e7db0b9ee6cce954d4
Sebastián González <brutalchrist@gmail.com>
2015-12-16 18:40:29 -0300

    Se agrega el uso de grunt para generar el vendor.js

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+	var vendors = [
+		'./bower_components/Chart.js/Chart.js'
+	];
+
+	grunt.file.defaultEncoding = 'iso-8859-1';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				compress: {
+					global_defs: {
+						'debug': false
+					},
+					dead_code: true
+				}
+			},
+			dist: {
+				files: {
+					'./public/js/vendors.js': vendors
+				}
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default task(s).
+	grunt.registerTask('default', ['uglify:dist']);
+	grunt.registerTask('build', ['uglify:dist']);
+	grunt.registerTask('build-all', ['uglify'])
+};
