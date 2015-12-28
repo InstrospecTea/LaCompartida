219652f47c94c6b7677a812d25b3d75cb6a7b241
brionescl <brionescl@gmail.com>
2015-12-28 18:56:27 -0300

    corregir order by en agrupador

--- app/layers/report.support/groupers/IdCobroGrouper.php
+++ app/layers/report.support/groupers/IdCobroGrouper.php
@@ -4,15 +4,15 @@
  *
  * * Agrupa por: cobro.id_cobro o Indefinido
  * * Muestra: cobro.id_cobro o Indefinido
- * * Ordena por: cobro.id_cobro o Indefinido
+ * * Ordena por: cobro.id_cobro
  *
- * M√°s info en: https://github.com/LemontechSA/ttb/wiki/Reporte-Agrupador:-Id-Cobro
+ * M·s info en: https://github.com/LemontechSA/ttb/wiki/Reporte-Agrupador:-Id-Cobro
  */
 class IdCobroGrouper extends AbstractGrouperTranslator {
 
 	/**
-	 * Obtiene el campo por el cual se agrupar√° la query
-	 * @return String Campo por el que se agrupa en par tabla.campo o alias
+	 * Obtiene el campo por el cual se agrupar· la query
+	 * @return String Campo por el que se agrupa en par tabl a.campo o alias
 	 */
 	function getGroupField() {
 		$undefined = $this->getUndefinedField();
@@ -20,19 +20,19 @@ class IdCobroGrouper extends AbstractGrouperTranslator {
 	}
 
 	/**
-	 * Obtiene el campo de grupo que se devolver√° en el SELECT de la query
-	 * @return String par tabla.campo o alias de funci√≥n
+	 * Obtiene el campo de grupo que se devolver· en el SELECT de la query
+	 * @return String par tabla.campo o alias de funciÛn
 	 */
 	function getSelectField() {
 		return $this->getGroupField();
 	}
 
 	/**
-	 * Obtiene el campo de grupo por el cual se ordenar√° la query
-	 * @return String par tabla.campo o alias de funci√≥n
+	 * Obtiene el campo de grupo por el cual se ordenar· la query
+	 * @return String par tabla.campo o alias de funciÛn
 	 */
 	function getOrderField() {
-		return $this->getGroupField();
+		return 'cobro.id_cobro';
 	}
 
 	/**
@@ -50,8 +50,8 @@ class IdCobroGrouper extends AbstractGrouperTranslator {
 	}
 
 	/**
-	 * Traduce los keys de agrupadores a campos para la query de Tr√°mites
-	 * id cobro del tr√°mite
+	 * Traduce los keys de agrupadores a campos para la query de Tr·mites
+	 * id cobro del tr·mite
 	 * @return void
 	 */
 	function translateForErrands(Criteria $Criteria) {
