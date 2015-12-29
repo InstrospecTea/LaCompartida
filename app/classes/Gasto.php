9bc9439a4afba9a3244c922130c804e1e3b714e9
Nicolás Torres <ntorresj@gmail.com>
2015-12-29 12:45:20 -0300

    corrección subir excel

--- app/classes/Gasto.php
+++ app/classes/Gasto.php
@@ -606,11 +606,3 @@ class Gasto extends Objeto {
 		}
 	}
 }
-
-if (!class_exists('ListaGastos')) {
-	class ListaGastos extends Lista {
-		function ListaGastos($sesion, $params, $query) {
-			$this->Lista($sesion, 'Gasto', $params, $query);
-		}
-	}
-}
