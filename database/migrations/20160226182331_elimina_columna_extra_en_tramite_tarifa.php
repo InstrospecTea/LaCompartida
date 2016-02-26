e2a9ea75a516e6962549bc1345f5889e412eefe3
Nicolás Torres <ntorresj@gmail.com>
2016-02-26 15:53:46 -0300

    remove unnecessary column on tramite_tarifa

---
+++ database/migrations/20160226182331_elimina_columna_extra_en_tramite_tarifa.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database;
+
+class EliminaColumnaExtraEnTramiteTarifa extends \Database\Migration implements \Database\ITemplateMigration {
+
+	/**
+   * Run the migrations.
+   * @return void
+   */
+	function up() {
+		$this->addQueryUp('ALTER TABLE `tramite_tarifa` DROP COLUMN `guardado`');
+	}
+
+	/**
+   * Reverse the migrations.
+   * @return void
+   */
+	function down() {
+		$this->addQueryDown("ALTER TABLE `tramite_tarifa` ADD `guardado` INT(1)  NOT NULL  DEFAULT '0'  COMMENT 'cuando se guarda el tarifa se pone 1, si no guarda el tarifa que ya esta creado se borra cuando salgas de la pantalla'  AFTER `tarifa_defecto`;");
+	}
+}
