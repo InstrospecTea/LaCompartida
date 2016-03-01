80ba748bd4023ec91a1e59977234115b830a617f
vladzur <vladzur@gmail.com>
2016-03-01 15:04:25 -0300

    Revertir cambio en menu

--- app/interfaces/reportes_especificos.php
+++ app/interfaces/reportes_especificos.php
@@ -95,7 +95,7 @@ if (Conf::GetConf($sesion,'UsaDisenoNuevo')) {
 													<li><a href='<?php echo Conf::RootDir() ?>/app/Report/areaCharge' style="color:#000;text-decoration: none;"><?php echo __('Cobros por Area') ?></a></li>
 													<li><a href='<?php echo Conf::RootDir() ?>/app/interfaces/reporte_liq_no_facturadas.php' style="color:#000;text-decoration: none;"><?php echo __(__('Cobros') . ' por facturar'); ?></a></li>
 													<li><a href='<?php echo Conf::RootDir() ?>/app/interfaces/planillas/planilla_resumen_cobranza.php' style="color:#000;text-decoration: none;"><?php echo __('Resumen de Liquidaciones') ?></a></li>
-													<li><a href='<?php echo Conf::RootDir() ?>/app/Report/clientOldDebtAccountingConcepts' style="color:#000;text-decoration: none;"><?php echo __('Reporte Morosidad') ?></a></li>
+													<li><a href='<?php echo Conf::RootDir() ?>/app/interfaces/planillas/planilla_morosidad.php' style="color:#000;text-decoration: none;"><?php echo __('Reporte Morosidad') ?></a></li>
 													<li><a href='<?php echo Conf::RootDir() ?>/app/interfaces/tipo_cambio_historico.php' style="color:#000;text-decoration: none;"><?php echo __('Histórico de Tipo de Cambio') ?></a></li>
 													<li><a href='<?php echo Conf::RootDir() ?>/app/interfaces/planillas/planilla_ciclo_facturacion.php' style="color:#000;text-decoration: none;"><?php echo __('Ciclo de Facturación') ?></a></li>
 												</ul>
