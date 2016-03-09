bf710f4dcbf47bdd62733bd6743d798df4818714
Nicolás Torres <ntorresj@gmail.com>
2016-03-08 22:41:35 -0300

    Nuevas funciones

--- app/classes/InsertCriteria.php
+++ app/classes/InsertCriteria.php
@@ -17,11 +17,14 @@ class InsertCriteria
 	private $insert = 'INSERT INTO ';
 	private $table_name = '';
 	private $select_criteria = null;
+	private $insert_criteria = array();
+	private $pivotes_temp = array();
 
 	/*
 		CRITERIA SCOPE ENVELOPERS.
 	 */
 	private $insert_value_clause = array();
+	private $insert_id = null;
 
 	/**
 	 * Constructor de la clase.
@@ -39,8 +42,21 @@ class InsertCriteria
 		if ($this->sesion == null) {
 			throw new Exception('Criteria dice: No hay una sesión definida para Criteria, no es posible ejecutar.');
 		}
-		$statement = $this->sesion->pdodbh->prepare($this->get_plain_query());
-		$statement->execute();
+
+		$response = new stdClass();
+
+		try {
+			$statement = $this->sesion->pdodbh->prepare($this->get_plain_query());
+			$statement->execute();
+
+			$response->success = true;
+			$this->set_last_insert_id($this->sesion->pdodbh->lastInsertId());
+		} catch (Exception $e) {
+			$response->success = false;
+			$response->message = $e->getMessage();
+		}
+
+		return $response;
 	}
 
 	public function add_pivot_with_value($column_key, $value, $default = false) {
@@ -84,22 +100,64 @@ class InsertCriteria
 	private function get_pivotes_fragment() {
 		$pivotes = array_keys($this->insert_value_clause);
 
-		return '('.implode(',',$pivotes).')';
+		if (empty($pivotes)) {
+			$pivotes = $this->pivotes_temp;
+		}
+
+		return '(' . implode(', ', $pivotes) . ')';
 
 	}
 
 	private function get_values_fragment() {
 		$pivotes = array_values($this->insert_value_clause);
 
-		return '('.implode(',',$pivotes).')';
+		return '(' . implode(', ', $pivotes) . ')';
+	}
+
+	private function get_inserts_fragment() {
+		if (!empty($this->insert_criteria)) {
+			return implode(', ', $this->insert_criteria);
+		} else {
+			return $this->get_values_fragment();
+		}
 	}
 
 	public function get_plain_query() {
-		return $this->insert . $this->table_name . ' ' . $this->get_pivotes_fragment() . ' VALUES ' . $this->get_values_fragment();
+		return $this->insert . $this->table_name . ' ' . $this->get_pivotes_fragment() . ' VALUES ' . $this->get_inserts_fragment();
 
 	}
 
 	public function get_plain_query_from_criteria(){
 		throw new Exception("No implementado aún.");
+	}
+
+  /**
+   * Add a new row for sql insert statemen
+   * e.g. INSERT INTO table (key1, key2, ..., keyN)
+   * 			VALUES (value1, value2, ..., valueN), (value1, value2, ..., valueN), ..., (value1, value2, ..., valueN);
+   */
+	public function add_insert() {
+		$insert = $this->get_values_fragment();
+		if (!empty($insert)) {
+			$this->insert_criteria[] = $insert;
+			$this->pivotes_temp = array_keys($this->insert_value_clause);
+			$this->insert_value_clause = array();
+		}
+	}
+
+	/**
+	 * Set $insert_id value
+	 * @param int
+	 */
+	public function set_last_insert_id($insert_id) {
+		$this->insert_id = $insert_id;
+	}
+
+	/**
+	 * Get $insert_id value
+	 * @return int
+	 */
+	public function get_last_insert_id() {
+		return $this->insert_id;
 	}
 }
