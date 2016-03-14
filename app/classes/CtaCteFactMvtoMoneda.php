7b4d55b1f79d3baa2a275794f01b3dca306ddaa2
Nicolás Torres <ntorresj@gmail.com>
2016-03-14 14:52:07 -0300

    se quitan comentarios y se cambia query a criteria

--- app/classes/CtaCteFactMvtoMoneda.php
+++ app/classes/CtaCteFactMvtoMoneda.php
@@ -12,29 +12,31 @@ class CtaCteFactMvtoMoneda extends Objeto
 	}
 
 	function LoadByFactura($id_factura) {
-		$query = "SELECT id_cta_cte_mvto FROM cta_cte_fact_mvto WHERE id_factura_pago = '$id_factura'";
-		$resp = mysql_query($query, $this->sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__,$this->sesion->dbh);
-		list($id) = mysql_fetch_array($resp);
+		$Criteria = new Criteria($this->sesion);
+		$id_cta_cte_mvto = $Criteria->add_select('id_cta_cte_mvto', 'id_cta_cte_mvto')
+																	->add_from('cta_cte_fact_mvto')
+																	->add_restriction(CriteriaRestriction::equals('id_factura_pago', $id_factura))
+																	->run();
 
-		if($id)
-			return $this->Load($id);
+		if($id_cta_cte_mvto[0]['id_cta_cte_mvto']) {
+			return $this->Load($id_cta_cte_mvto[0]['id_cta_cte_mvto']);
+		}
+
 		return false;
 	}
 
 	function ActualizarMvtoMoneda($tipo_cambio = array()) {
 		$query = "DELETE FROM cta_cte_fact_mvto_moneda WHERE id_cta_cte_fact_mvto = {$this->fields['id_cta_cte_fact_mvto']}";
-		// echo $query . '<br>';
-		$resp = mysql_query($query,$this->sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__, $this->sesion->dbh);
+		$resp = mysql_query($query, $this->sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $this->sesion->dbh);
 
 		if (empty($tipo_cambio)) {
 			$query = "INSERT INTO cta_cte_fact_mvto_moneda (id_cta_cte_mvto, id_moneda, tipo_cambio)
 									SELECT {$this->fields['id_cta_cte_fact_mvto']}, id_moneda, tipo_cambio FROM prm_moneda WHERE 1";
-			$resp = mysql_query($query,$this->sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__, $this->sesion->dbh);
+			$resp = mysql_query($query, $this->sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $this->sesion->dbh);
 		} else {
 			foreach($tipo_cambio as $id_moneda => $tc) {
 				$query = "INSERT INTO cta_cte_fact_mvto_moneda (id_cta_cte_fact_mvto, id_moneda, tipo_cambio) VALUES ({$this->fields['id_cta_cte_fact_mvto']}, {$id_moneda}, {$tc});";
-			$resp = mysql_query($query,$this->sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__, $this->sesion->dbh);
-		// echo $query . '<br>';
+			$resp = mysql_query($query, $this->sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $this->sesion->dbh);
 			}
 		}
 	}
