8eea3eb1b02a30dc11b6d1d6e3aa52c85ab5d2f2
Mario Lavandero <mlavandero@lemontech.com>
2016-03-21 17:29:08 -0300

    Add Mixpanel Agregar Cliente Event when creating clients

--- app/interfaces/agregar_cliente.php
+++ app/interfaces/agregar_cliente.php
@@ -263,6 +263,11 @@ if ($opcion == "guardar") {
 					$Pagina->AddInfo(__('Cliente') . ' ' . __('Guardado con exito') . '<br>' . __('Contrato guardado con éxito'));
 					//To S3
 					$archivo->LoadById($contrato->fields['id_contrato']);
+					if($desde_agrega_cliente == 1) {
+						$mp = Mixpanel::getInstance("35700d667425ad9858d92ff694febf63");
+						$mp->identify($RUT);
+						$mp->track("Agregar Cliente");
+					}
 				} else {
 					$Pagina->AddError($contrato->error);
 				}
