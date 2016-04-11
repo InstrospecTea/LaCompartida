86e1f50abd5d0e1332a87c90e4997c7acbfffd13
brionescl <brionescl@gmail.com>
2016-04-11 18:57:03 -0300

    archivos base

--- app/classes/WsFacturacionSatcom.php
+++ app/classes/WsFacturacionSatcom.php
@@ -4,4 +4,14 @@ require_once(dirname(__FILE__) . '/../conf.php');
 ini_set('soap.wsdl_cache_enabled', 0);
 
 class WsFacturacionSatcom extends WsFacturacion {
+	protected $url = 'http://190.108.68.38:9005/SatcomWS.asmx';
+
+	public function __construct() {
+		$this->Client = new SoapClient($this->url, array('trace' => 1));
+	}
+
+	public function emitirFactura($dataFactura) {
+		$x = $this->Client->recibeInfo();
+		var_dump($x); exit;
+	}
 }
---
+++ app/plugins/facturacion_electronica_satcom.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Ofrece exportar las facturas al WS de Facturación electrónica Satcom (http://satcomec.com/)
+ *
+ * @package The Time Billing
+ * @subpackage Plugins
+ */
+
+require_once dirname(__FILE__) . '/../conf.php';
+
+$Slim = Slim::getInstance('default', true);
+
+$clase = 'FacturacionElectronicaSatcom';
+
+$Slim->hook('hook_factura_javascript_after', array($clase, 'InsertaJSFacturaElectronica'));
+$Slim->hook('hook_factura_metodo_pago', array($clase, 'InsertaMetodoPago'));
+$Slim->hook('hook_factura_dte_estado', array($clase, 'InsertaEstadoDTE'));
+$Slim->hook('hook_validar_factura', array($clase, 'ValidarFactura'));
+$Slim->hook('hook_cobro6_javascript_after', array($clase, 'InsertaJSFacturaElectronica'));
+$Slim->hook('hook_descargar_pdf_factura_electronica', array($clase, 'DescargarPdf'));
+
+$Slim->hook('hook_cobros7_botones_after', function($hookArg) {
+  return FacturacionElectronicaSatcom::AgregarBotonFacturaElectronica($hookArg);
+});
+
+$Slim->hook('hook_genera_factura_electronica', function($hookArg) {
+  return FacturacionElectronicaSatcom::GeneraFacturaElectronica($hookArg);
+});
+
+$Slim->hook('hook_anula_factura_electronica', function($hookArg) {
+  return FacturacionElectronicaSatcom::AnulaFacturaElectronica($hookArg);
+});
