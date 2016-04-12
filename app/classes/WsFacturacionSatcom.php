86b5b4d33a99851029da54e7c82baabfe255db6c
brionescl <brionescl@gmail.com>
2016-04-12 17:53:47 -0300

    conectar con el servicio

--- app/classes/WsFacturacionSatcom.php
+++ app/classes/WsFacturacionSatcom.php
@@ -4,14 +4,48 @@ require_once(dirname(__FILE__) . '/../conf.php');
 ini_set('soap.wsdl_cache_enabled', 0);
 
 class WsFacturacionSatcom extends WsFacturacion {
-	protected $url = 'http://190.108.68.38:9005/SatcomWS.asmx';
+	protected $url = 'http://190.108.68.38:9010/Bridge/WcfBridge.svc';
 
 	public function __construct() {
-		$this->Client = new SoapClient($this->url, array('trace' => 1));
+		$this->Client = new SoapClient(
+			"{$this->url}?WSDL",
+			array(
+				"trace" => TRUE,
+				"exception" => 0,
+				"soap_version" => SOAP_1_2
+			)
+		);
 	}
 
 	public function emitirFactura($dataFactura) {
-		$x = $this->Client->recibeInfo();
-		var_dump($x); exit;
+		$xml = '<![CDATA[<?xml version="1.0" encoding="utf-8"?><Requerimiento></Requerimiento>]]>';
+		$actionHeader = array();
+
+		try {
+			$actionHeader[] = new SoapHeader(
+				'http://www.w3.org/2005/08/addressing',
+				'Action',
+				'http://tempuri.org/IBridge/ProcesarComprobante'
+			);
+
+			$actionHeader[] = new SoapHeader(
+				'http://www.w3.org/2005/08/addressing',
+				'To',
+				$this->url
+			);
+
+			$this->Client->__setSoapHeaders($actionHeader);
+
+			$ProcesarComprobante  = new stdClass();
+			$ProcesarComprobante->strRequerimiento = $xml;
+			$ProcesarComprobante->Comprimido = false;
+
+			$response = $this->Client->ProcesarComprobante($ProcesarComprobante);
+		} catch(SoapFault $fault) {
+			echo $fault;
+			echo $this->Client->__getLastRequest();
+		}
+
+		var_dump($response); exit;
 	}
 }
