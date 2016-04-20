e35da86b10075d500fe291372a318810924b40ec
brionescl <brionescl@gmail.com>
2016-04-20 12:52:24 -0300

    generar factura con satcom

--- app/classes/FacturacionElectronicaSatcom.php
+++ app/classes/FacturacionElectronicaSatcom.php
@@ -29,24 +29,6 @@ class FacturacionElectronicaSatcom extends FacturacionElectronica {
 		}
 	}
 
-	public static function BotonDescargarHTMLCedible($id_factura) {
-		$img_dir = Conf::ImgDir();
-		$Html = self::getHtml();
-		$img_pdf_copia = $Html->img("{$img_dir}/pdf.gif", array('border' => 0, 'style' => 'opacity:0.5;'));
-		if (!empty($id_factura) && $id_factura != 0) {
-			$sesion = new Sesion();
-			$factura = new Factura($sesion);
-			$factura->Load($id_factura);
-			$tipo_doc = $factura->CodigoTipoDocumentoLegal();
-			if (!empty($tipo_doc) && $tipo_doc != 'NC' && $tipo_doc != 'ND') {
-				$output = $Html->tag('a', $img_pdf_copia, array('title' => 'Descargar copia cedible', 'class' => 'factura-documento factura-cedible', 'data-factura' => $id_factura, 'data-original' => 0, 'href' => '#'));
-			}
-		} else {
-			$output = $Html->tag('a', $img_pdf_copia, array('title' => 'Descargar copia cedible', 'class' => 'factura-documento factura-cedible', 'data-factura' => $id_factura, 'data-original' => 0, 'href' => '#'));
-		}
-		return $output;
-	}
-
 	public static function BotonDescargarHTML($id_factura) {
 		$img_dir = Conf::ImgDir();
 		$Html = self::getHtml();
@@ -59,7 +41,7 @@ class FacturacionElectronicaSatcom extends FacturacionElectronica {
 		$Factura = $hookArg['Factura'];
 		$id_factura = $Factura->fields['id_factura'];
 		if ($Factura->FacturaElectronicaCreada()) {
-			$hookArg['content'] = self::BotonDescargarHTML($id_factura) . self::BotonDescargarHTMLCedible($id_factura);
+			$hookArg['content'] = self::BotonDescargarHTML($id_factura);
 		} elseif (!$Factura->Anulada()) {
 			$hookArg['content'] = self::BotonGenerarHTML($id_factura);
 		}
@@ -88,7 +70,6 @@ class FacturacionElectronicaSatcom extends FacturacionElectronica {
 
 	public static function InsertaJSFacturaElectronica() {
 		$BotonDescargarHTML = self::BotonDescargarHTML('0');
-		$BotonDescargarCedible = self::BotonDescargarHTMLCedible('0');
 		echo <<<EOF
 			jQuery(document).on("click", ".factura-electronica", function() {
 				if (!confirm("¿Confirma la generación del documento electrónico?")) {
@@ -103,11 +84,7 @@ class FacturacionElectronicaSatcom extends FacturacionElectronica {
 					type: "POST"
 				}).success(function(data) {
 					loading.remove();
-					if (codigo_tipo_doc == 'NC' || codigo_tipo_doc == 'ND') {
-						buttons = jQuery('{$BotonDescargarHTML}');
-					} else {
-						buttons = jQuery('{$BotonDescargarHTML}{$BotonDescargarCedible}');
-					}
+					buttons = jQuery('{$BotonDescargarHTML}');
 					buttons.each(function(i, e) {
 						jQuery(e).attr("data-factura", id_factura);
 					});
@@ -164,7 +141,7 @@ EOF;
 			$WsFacturacionCl = new WsFacturacionCl;
 			$WsFacturacionCl->setLogin($rut, $usuario, $password);
 			if ($WsFacturacionCl->hasError()) {
-				$hookArg['Error'] = self::ParseError($WsFacturacionCl, $WsFacturacionCl->getErrorCode());
+				$hookArg['Error'] = self::parseError($WsFacturacionCl, $WsFacturacionCl->getErrorCode());
 			} else {
 				$arrayDocumento = self::FacturaToArray($Sesion, $Factura ,$Estudio);
 				try {
@@ -172,13 +149,14 @@ EOF;
 					if (!$WsFacturacionCl->hasError()) {
 						$hookArg['InvoiceURL'] = $result;
 					} else {
-						$hookArg['Error'] = self::ParseError($WsFacturacionCl, 'BuildingInvoiceError');
+						$hookArg['Error'] = self::parseError($WsFacturacionCl, 'BuildingInvoiceError');
 					}
 				} catch  (Exception $ex) {
-					$hookArg['Error'] = self::ParseError($ex, 'BuildingInvoiceError');
+					$hookArg['Error'] = self::parseError($ex, 'BuildingInvoiceError');
 				}
 			}
 		}
+
 		if (!is_null($hookArg['Error'])) {
 			return $hookArg;
 		} else {
@@ -197,58 +175,54 @@ EOF;
 
 	public static function GeneraFacturaElectronica($hookArg) {
 		$Sesion = new Sesion();
-		$Factura = $hookArg['Factura'];
-		if (!empty($Factura->fields['dte_url_pdf'])) {
-			$hookArg['InvoiceURL'] = $Factura->fields['dte_url_pdf'];
+		$factura = $hookArg['Factura'];
+
+		if (!empty($factura->fields['dte_url_pdf'])) {
+			$hookArg['InvoiceURL'] = $factura->fields['dte_url_pdf'];
 		} else {
 			$Estudio = new PrmEstudio($Sesion);
-			$Estudio->Load($Factura->fields['id_estudio']);
-			$rut = $Estudio->GetMetaData('rut');
-			$usuario = $Estudio->GetMetadata('facturacion_electronica_cl.usuario');
-			$password = $Estudio->GetMetadata('facturacion_electronica_cl.password');
-			$WsFacturacionCl = new WsFacturacionCl;
-			$WsFacturacionCl->setLogin($rut, $usuario, $password);
-			if ($WsFacturacionCl->hasError()) {
-				$hookArg['Error'] = self::ParseError($WsFacturacionCl, $WsFacturacionCl->getErrorCode());
+			$Estudio->Load($factura->fields['id_estudio']);
+
+			$factura->fields['RucEmisor'] = $Estudio->GetMetaData('facturacion_electronica_satcom.RucEmisor');
+			$factura->fields['ClaveAcceso'] = $Estudio->GetMetadata('facturacion_electronica_satcom.ClaveAcceso');
+			$factura->fields['Establecimiento'] = $Estudio->GetMetadata('facturacion_electronica_satcom.Establecimiento');
+			$factura->fields['Punto'] = $Estudio->GetMetadata('facturacion_electronica_satcom.Punto');
+
+			$WsFacturacionSatcom = new WsFacturacionSatcom;
+			$documento = $WsFacturacionSatcom->emitirFactura($factura);
+
+			if ($WsFacturacionSatcom->hasError()) {
+				$hookArg['Error'] = self::parseError($WsFacturacionSatcom, 'BuildingInvoiceError');
 			} else {
-				$arrayDocumento = self::FacturaToArray($Sesion, $Factura ,$Estudio);
-				$hookArg['ExtraData'] = $arrayDocumento;
 				try {
-					$result = $WsFacturacionCl->emitirFactura($arrayDocumento);
-					if (!$WsFacturacionCl->hasError()) {
-						try {
-							$Factura->Edit('dte_xml', $result['Detalle']['Documento']['xmlDTE']);
-							$Factura->Edit('dte_fecha_creacion', date('Y-m-d H:i:s'));
-							$file_url = $result['Detalle']['Documento']['urlPDF'];
-							$Factura->Edit('dte_url_pdf', $file_url);
-							if ($Factura->Write()) {
-								$hookArg['InvoiceURL'] = $file_url;
-							}
-						} catch (Exception $ex) {
-							$hookArg['Error'] = self::ParseError($ex, 'BuildingInvoiceError');
-						}
-					} else {
-						$hookArg['Error'] = self::ParseError($WsFacturacionCl, 'BuildingInvoiceError');
+					$factura->Edit('dte_fecha_creacion', date('Y-m-d H:i:s'));
+					$factura->Edit('dte_url_pdf', $documento);
+					if ($factura->Write()) {
+						$hookArg['InvoiceURL'] = $documento;
 					}
-				} catch  (Exception $ex) {
-					$hookArg['Error'] = self::ParseError($ex, 'BuildingInvoiceError');
+				} catch (Exception $ex) {
+					$hookArg['Error'] = self::parseError($ex, 'BuildingInvoiceError');
 				}
 			}
 		}
+
 		return $hookArg;
 	}
 
-	public static function ParseError($result, $error_code) {
+	public static function parseError($result, $error_code) {
 		$error_description = null;
+
 		if (is_a($result, 'Exception')) {
 			$error_log = $result->__toString();
 		} else {
 			$error_description = utf8_decode($result->getErrorMessage());
 			$error_log = $error_description;
 		}
-		Log::write($error_log, "FacturacionElectronicaCl");
+
+		Log::write($error_log, "FacturacionElectronicaSatcom");
+
 		return array(
-			'Code' =>  $error_code,
+			'Code' => $error_code,
 			'Message' => $error_description
 		);
 	}
@@ -286,10 +260,10 @@ EOF;
 					$Factura->Edit('dte_estado_descripcion', __(Factura::$estados_dte_desc[$estado_dte]));
 					$Factura->Write();
 				} catch (Exception $ex) {
-					$hookArg['Error'] = self::ParseError($ex, 'SaveCanceledInvoiceError');
+					$hookArg['Error'] = self::parseError($ex, 'SaveCanceledInvoiceError');
 				}
 			} else {
-				$hookArg['Error'] = self::ParseError($WsFacturacionCl, 'CancelGeneratedInvoiceError');
+				$hookArg['Error'] = self::parseError($WsFacturacionCl, 'CancelGeneratedInvoiceError');
 				$mensaje = "Usted ha solicitado anular un Documento Tributario. Este proceso puede tardar y mientras esto ocurre, anularemos la factura en Time Billing para que usted pueda volver a generar el documento correctamente.";
 				$estado_dte = Factura::$estados_dte['ProcesoAnular'];
 				$Factura->Edit('dte_estado', $estado_dte);
@@ -298,168 +272,6 @@ EOF;
 			}
 		}
 		return $hookArg;
-	}
-
-	/**
-	 * Genera array de datos de la factura para enviar a Facturacion.cl
-	 * @param Sesion $Sesion
-	 * @param Factura $Factura
-	 * @return array
-	 */
-	public static function FacturaToArray(Sesion $Sesion, Factura $Factura, PrmEstudio $Estudio) {
-		$subtotal_factura = $Factura->fields['subtotal'] + $Factura->fields['subtotal_gastos'] + $Factura->fields['subtotal_gastos_sin_impuesto'];
-
-		$PrmDocumentoLegal = new PrmDocumentoLegal($Sesion);
-		$PrmDocumentoLegal->Load($Factura->fields['id_documento_legal']);
-		$tipoDTE = $PrmDocumentoLegal->fields['codigo_dte'];
-		$afecto = $PrmDocumentoLegal->fields['documento_afecto'];
-
-		$Contrato = new Contrato($Sesion);
-		$Contrato->Load($Factura->fields['id_contrato']);
-
-		if ($tipoDTE == 39 || $tipoDTE == 41) {
-			$Cobro = new Cobro($Sesion);
-			$Cobro->Load($Factura->fields['id_cobro'], array('id_cobro', 'fecha_ini', 'fecha_fin'));
-			$fecha_desde = $Cobro->fields['fecha_ini'];
-			if (empty($fecha_desde) || $fecha_desde == '0000-00-00') {
-				$fecha_desde = $Cobro->fechaPrimerTrabajo();
-			}
-			$fecha_hasta = $Cobro->fields['fecha_fin'];
-		}
-		$arrayFactura = array(
-			'tipo_dte' => $tipoDTE,
-			'afecto' => $afecto,
-			'fecha_emision' => Utiles::sql2date($Factura->fields['fecha'], '%Y-%m-%d'),
-			'fecha_vencimiento' => Utiles::sql2date($Factura->fields['fecha_vencimiento'], '%Y-%m-%d'),
-			'fecha_desde' => $fecha_desde,
-			'fecha_hasta' => $fecha_hasta,
-			'folio' => $Factura->fields['numero'],
-			'monto_neto' => intval($subtotal_factura),
-			'tasa_iva' => intval($Factura->fields['porcentaje_impuesto']),
-			'monto_iva' => intval($Factura->fields['iva']),
-			'monto_total' => intval($Factura->fields['total']),
-			'condicion_pago' => $Factura->ObtieneGlosaCondicionPago(),
-			'emisor' => array(
-				'rut' => $Estudio->GetMetaData('rut'),
-				'razon_social' => $Estudio->GetMetaData('razon_social'),
-				'giro' => $Estudio->GetMetaData('giro'),
-				'correo' => $Contrato->fields['email_contacto'],
-				'codigo_actividad' => $Estudio->GetMetaData('codigo_actividad'),
-				'direccion' => $Estudio->GetMetaData('direccion'),
-				'comuna' => $Estudio->GetMetaData('comuna'),
-				'cuidad' => $Estudio->GetMetaData('cuidad')
-			),
-			'receptor' => array(
-				'rut' => $Factura->fields['RUT_cliente'],
-				'codigo' => $Factura->fields['codigo_cliente'],
-				'razon_social' => UtilesApp::transliteration($Factura->fields['cliente']),
-				'giro' => UtilesApp::transliteration($Factura->fields['giro_cliente']),
-				'direccion' => UtilesApp::transliteration($Factura->fields['direccion_cliente']),
-				'comuna' => UtilesApp::transliteration($Factura->fields['comuna_cliente']),
-				'cuidad' => UtilesApp::transliteration($Factura->fields['ciudad_cliente']),
-				'contacto' => $Contrato->ObtenerSolicitante()
-			),
-			'detalle' => array()
-		);
-
-		if ($Factura->fields['subtotal'] > 0) {
-			$arrayFactura['detalle'][] = array(
-				'descripcion' => $Factura->fields['descripcion'],
-				'cantidad' => 1,
-				'precio_unitario' => (int) number_format($Factura->fields['subtotal'], 2, '.', '')
-			);
-		}
-
-		if ($Factura->fields['subtotal_gastos'] > 0) {
-			$arrayFactura['detalle'][] = array(
-				'descripcion' => $Factura->fields['descripcion_subtotal_gastos'],
-				'cantidad' => 1,
-				'precio_unitario' => (int) number_format($Factura->fields['subtotal_gastos'], 2, '.', '')
-			);
-		}
-
-		if ($Factura->fields['subtotal_gastos_sin_impuesto'] > 0) {
-			$arrayFactura['detalle'][] = array(
-				'descripcion' => $Factura->fields['descripcion_subtotal_gastos_sin_impuesto'],
-				'cantidad' => 1,
-				'precio_unitario' => (int) number_format($Factura->fields['subtotal_gastos_sin_impuesto'], 2, '.', '')
-			);
-		}
-
-		if ($Factura->fields['id_factura_padre'] > 0) {
-			$FacturaPadre = new Factura($Sesion);
-			$FacturaPadre->Load($Factura->fields['id_factura_padre']);
-			$arrayPadre = self::FacturaToArray($Sesion, $FacturaPadre, $Estudio);
-
-			$PrmDocumentoLegal->Load($FacturaPadre->fields['id_documento_legal']);
-			$tipoDTE = $PrmDocumentoLegal->fields['codigo_dte'];
-
-			$referenciaId = intval($Factura->fields['dte_codigo_referencia']);
-			$Referencia = new PrmCodigo($Sesion);
-			$Referencia->LoadById($referenciaId);
-			$codigoReferencia = $Referencia->Loaded() ? $Referencia->fields['codigo'] : 1;
-			$arrayFactura['referencia'] = array(
-				'tipo_dte'	=> $tipoDTE,
-				'folio'	=> $FacturaPadre->fields['numero'],
-				'fecha_emision'	=> Utiles::sql2date($FacturaPadre->fields['fecha'], '%Y-%m-%d'),
-				/**
-				 * CodRef: Indica los distintos casos de referencia, los cuales pueden ser:
-				 * a) Nota de Crédito que elimina documento de referencia en forma completa
-				 *    (Factura de venta, Nota de débito, o Factura de compra)
-				 * b) Nota de crédito que corrige un texto del documento de referencia
-				 * c) Nota de Débito que elimina una Nota de Crédito en la referencia en forma completa
-				 * d) Notas de crédito o débito que corrigen montos de otro documento
-				 * Casos a) b) y c) deben tener un único documento de referencia, es decir una sola línea de referencia.
-				 * Sus valores pueden ser:
-				 *   1: Anula Documento de Referencia.
-				 *   2: Corrige Texto Documento de Referencia.
-				 *   3: Corrige Montos.
-				 */
-				'codigo'	=> $codigoReferencia,
-				/**
-				 * RazonRef: Explicitar razon. Ejemplo una Nota de Credito que hacer referencia a una factura,
-				 * indica "descuento por pronto pago", "error en precio" o "anula factura", etc.
-				 * El campo tiene un largo maximo de 90 caracteres.
-				 */
-				'razon'	=> $Factura->fields['dte_razon_referencia'],
-			);
-		} else if ($Factura->fields['id_documento_referencia'] > 0) {
-			$FacturaPadre = new Factura($Sesion);
-			$FacturaPadre->Load($Factura->fields['id_factura_padre']);
-			$arrayPadre = self::FacturaToArray($Sesion, $FacturaPadre, $Estudio);
-
-			$referenciaId = intval($Factura->fields['id_documento_referencia']);
-			$Referencia = new PrmCodigo($Sesion);
-			$Referencia->LoadById($referenciaId);
-			$codigoReferencia = $Referencia->Loaded() ? $Referencia->fields['codigo'] : 1;
-			$arrayFactura['referencia'] = array(
-				'tipo_dte'	=> $referenciaId,
-				'folio'	=> $Factura->fields['folio_documento_referencia'],
-				'fecha_emision'	=> Utiles::sql2date($Factura->fields['fecha_documento_referencia'], '%Y-%m-%d'),
-				/**
-				 * CodRef: Indica los distintos casos de referencia, los cuales pueden ser:
-				 * a) Nota de Crédito que elimina documento de referencia en forma completa
-				 *    (Factura de venta, Nota de débito, o Factura de compra)
-				 * b) Nota de crédito que corrige un texto del documento de referencia
-				 * c) Nota de Débito que elimina una Nota de Crédito en la referencia en forma completa
-				 * d) Notas de crédito o débito que corrigen montos de otro documento
-				 * Casos a) b) y c) deben tener un único documento de referencia, es decir una sola línea de referencia.
-				 * Sus valores pueden ser:
-				 *   1: Anula Documento de Referencia.
-				 *   2: Corrige Texto Documento de Referencia.
-				 *   3: Corrige Montos.
-				 */
-				'codigo'	=> $codigoReferencia,
-				/**
-				 * RazonRef: Explicitar razon. Ejemplo una Nota de Credito que hacer referencia a una factura,
-				 * indica "descuento por pronto pago", "error en precio" o "anula factura", etc.
-				 * El campo tiene un largo maximo de 90 caracteres.
-				 */
-				'razon'	=> $Factura->fields['dte_razon_referencia'],
-			);
-		}
-
-		return $arrayFactura;
 	}
 
 }
--- app/classes/WsFacturacionSatcom.php
+++ app/classes/WsFacturacionSatcom.php
@@ -5,6 +5,7 @@ ini_set('soap.wsdl_cache_enabled', 0);
 
 class WsFacturacionSatcom extends WsFacturacion {
 	protected $url = 'http://190.108.68.38:9010/Bridge/WcfBridge.svc';
+	protected $action = 'http://tempuri.org/IBridge/ProcesarComprobante';
 
 	public function __construct() {
 		$this->Client = new SoapClient(
@@ -17,35 +18,119 @@ class WsFacturacionSatcom extends WsFacturacion {
 		);
 	}
 
-	public function emitirFactura($dataFactura) {
-		$xml = '<![CDATA[<?xml version="1.0" encoding="utf-8"?><Requerimiento></Requerimiento>]]>';
-		$actionHeader = array();
+	public function emitirFactura($factura) {
+		$documento_xml = $this->crearXML($factura);
+
+		$id_comprobante = 0;
+
+		$this->Client->__setSoapHeaders(
+			array(
+				new SoapHeader('http://www.w3.org/2005/08/addressing', 'Action', $this->action),
+				new SoapHeader('http://www.w3.org/2005/08/addressing', 'To', $this->url)
+			)
+		);
+
+		$ProcesarComprobante = new stdClass();
+		$ProcesarComprobante->strRequerimiento = $documento_xml;
+		$ProcesarComprobante->Comprimido = false;
 
 		try {
-			$actionHeader[] = new SoapHeader(
-				'http://www.w3.org/2005/08/addressing',
-				'Action',
-				'http://tempuri.org/IBridge/ProcesarComprobante'
-			);
+			$respuesta = $this->Client->ProcesarComprobante($ProcesarComprobante);
 
-			$actionHeader[] = new SoapHeader(
-				'http://www.w3.org/2005/08/addressing',
-				'To',
-				$this->url
-			);
-
-			$this->Client->__setSoapHeaders($actionHeader);
-
-			$ProcesarComprobante  = new stdClass();
-			$ProcesarComprobante->strRequerimiento = $xml;
-			$ProcesarComprobante->Comprimido = false;
-
-			$response = $this->Client->ProcesarComprobante($ProcesarComprobante);
+			if ($respuesta->ProcesarComprobanteResult->EstadoProceso != 'Error') {
+				$id_comprobante = $respuesta->ProcesarComprobanteResult->IdComprobanteSAT;
+			} else {
+				Log::write($documento_xml, 'FacturacionElectronicaSatcom');
+				Log::write($respuesta->ProcesarComprobanteResult->MensajeError, 'FacturacionElectronicaSatcom');
+				$this->setError(1, 'Ocurrió un error inesperado en la generación del documento con Satcom');
+			}
 		} catch(SoapFault $fault) {
-			echo $fault;
-			echo $this->Client->__getLastRequest();
+			Log::write($documento_xml, 'FacturacionElectronicaSatcom');
+			Log::write(print_r($fault, true), 'FacturacionElectronicaSatcom');
+			$this->setError(1, 'Ocurrió un error inesperado con Satcom');
 		}
 
-		var_dump($response); exit;
+		return $id_comprobante;
+	}
+
+	private function crearXML($factura) {
+		$requerimiento = new SimpleXMLElement('<Requerimiento/>');
+
+		$requerimiento->addChild('Codigo', '01');
+		$requerimiento->addChild('Descripcion', 'FACTURA');
+		$requerimiento->addChild('NumeroDocumento', $factura->fields['numero']);
+		$requerimiento->addChild('FechaEmision', str_replace(' ', 'T', $factura->fields['fecha_creacion']));
+		// $requerimiento->addChild('PeridodoFiscal');
+
+		$impuestos = $requerimiento->addChild('Impuestos');
+		$impuesto = $impuestos->addChild('Impuesto');
+		$impuesto->addChild('CodigoImpuesto', 2);
+		$impuesto->addChild('Impuesto', 'IVA');
+		$impuesto->addChild('CodigoPorcentaje', 2);
+		$impuesto->addChild('Porcentaje', 12);
+		$impuesto->addChild('BaseImponible', '9.02');
+		$impuesto->addChild('Valor', '1.08');
+
+		$informacion_adicional = $requerimiento->addChild('InformacionAdicional');
+		$campo = $informacion_adicional->addChild('Campo');
+		$campo->addAttribute('id', 1);
+		$campo->addChild('Descripcion', 'CAJERO');
+		$campo->addChild('Valor', '509. Elizabet Garcia');
+
+		// $requerimiento->addChild('Reprocesos', 0);
+		// $requerimiento->addChild('EstadoComprobante', 'Autorizado');
+		$requerimiento->addChild('ClaveAcceso', $factura->fields['ClaveAcceso']);
+		// $requerimiento->addChild('Propina', 0.90);
+		// $requerimiento->addChild('NumeroAutorizacion', 1502201622075405917148990014338182163);
+		// $requerimiento->addChild('FechaAutorizacion', date('Y-m-d\TH:i:s'));
+		// $requerimiento->addChild('IdRequerimiento', 504579351630773);
+		// $requerimiento->addChild('CodigoEmisor', 12);
+		$requerimiento->addChild('RucEmisor', $factura->fields['RucEmisor']);
+		// $requerimiento->addChild('TipoEmision', 1);
+		$requerimiento->addChild('Establecimiento', $factura->fields['Establecimiento']);
+		$requerimiento->addChild('Punto', $factura->fields['Punto']);
+		$requerimiento->addChild('Moneda', 'DOLAR');
+		$requerimiento->addChild('Ambiente', 2);
+		// $requerimiento->addChild('Version', '');
+		$requerimiento->addChild('TotalConImpuestos', '11.00');
+		$requerimiento->addChild('TotalSinImpuestos', '9.02');
+		// $requerimiento->addChild('TotalRetencion', 0);
+
+		$cliente = $requerimiento->addChild('Cliente');
+		$cliente->addChild('RazonSocial', 'CONSUMIDOR FINAL');
+		$cliente->addChild('TipoIdentificacion', '07');
+		$cliente->addChild('NumeroIdentificacion', 9999999999999);
+		// $cliente->addChild('email', '');
+		// $cliente->addChild('Telefono', '');
+
+		$detalles = $requerimiento->addChild('Detalles');
+		$detalle = $detalles->addChild('Detalle');
+		$detalle->addAttribute('id', 1);
+		$detalle->addChild('Cantidad', 1);
+		$detalle->addChild('Descuento', '0.00');
+		$detalle->addChild('SubTotal', '9.02');
+
+		$producto = $detalle->addChild('Producto');
+		$producto->addChild('Codigo', 1008665);
+		$producto->addChild('CodigoAuxiliar', 1);
+		$producto->addChild('Descripcion', 'Piz Jamon');
+		$producto->addChild('ValorUnitario', '9.02');
+
+		$impuestos = $detalle->addChild('Impuestos');
+		$impuesto = $impuestos->addChild('Impuesto');
+		$impuesto->addChild('CodigoImpuesto', 2);
+		$impuesto->addChild('Impuesto', 'IVA');
+		$impuesto->addChild('CodigoPorcentaje', 2);
+		$impuesto->addChild('Porcentaje', 12);
+		$impuesto->addChild('BaseImponible', '9.02');
+		$impuesto->addChild('Valor', '1.08');
+
+		return $requerimiento->asXML();
+	}
+
+	public function obtenerPdf() {
+		$pdf = '';
+
+		return $pdf;
 	}
 }
