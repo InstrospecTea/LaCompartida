62b823eb771fabdca54ce003c08d6331de00486d
cps <cuayocps@gmail.com>
2016-04-25 12:06:08 -0300

    elimina configuraciones utilizadas para pruebas locales

--- backups/config.php
+++ backups/config.php
@@ -7,10 +7,8 @@ Conf::setStatic('AmazonKey', [
 	'default_cache_config' => '/var/www/cache/dynamoDBbackups'
 ]);
 
-Conf::write('dir_temp', '/tmp');
+Conf::write('dir_temp', '/tmp_backups');
 Conf::write('backup_mysql_error', '/var/www/error_logs/backup_mysqlerror.txt');
 Conf::write('alerta_disco_temp', 5); //(GB) si el espacio libre es menos q eso, tira un mensaje (y manda mail)
 Conf::write('alerta_disco_base', 5); //(GB) si el espacio libre es menos q eso, tira un mensaje (y manda mail)
 Conf::write('send_to', 'servidores@lemontech.cl');
-Conf::write('dir_temp', '/tmp');
-Conf::write('backup_mysql_error', '/tmp/backup_mysqlerror.txt');
