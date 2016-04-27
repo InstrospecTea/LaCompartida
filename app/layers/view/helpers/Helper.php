e2ca7ce209df6ca2f8a747dbc0e8ff0a6e7d0135
cps <cuayocps@gmail.com>
2016-04-27 11:43:53 -0300

    Corrige loadHelpers()

--- app/layers/view/helpers/Helper.php
+++ app/layers/view/helpers/Helper.php
@@ -22,16 +22,18 @@ abstract class Helper {
 	}
 
 	protected function loadHelpers() {
+		if (!class_exists('Helper')) {
+			$fileHelper = LAYER_PATH . $this->helpersPath . "/helpers/Helper.php";
+			require_once $fileHelper;
+		}
 		foreach ($this->helpers as $helper) {
-			$file = LAYER_PATH . $this->filePath . "/{$helper}.php";
+			$class_name = is_array($helper) ? $helper[0] : $helper;
+			$alias = is_array($helper) ? $helper[1] : null;
+			$file = LAYER_PATH . $this->helpersPath . "/helpers/{$class_name}.php";
 			if (is_readable($file)) {
 				require_once $file;
 			}
-			if (is_array($helper)) {
-				$this->loadModel($helper[0], $helper[1]);
-			} else {
-				$this->loadModel($helper);
-			}
+			$this->loadModel($class_name, $alias);
 		}
 	}
 }
