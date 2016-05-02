116559b64157ac789d562ff2ab30b58fb6eacfba
Sebastián González <brutalchrist@gmail.com>
2016-05-02 10:49:35 -0300

    Refactorización contrato_datos_incompletos_xls

--- app/interfaces/contrato_datos_incompletos_xls.php
+++ app/interfaces/contrato_datos_incompletos_xls.php
@@ -2,11 +2,12 @@
 	require_once dirname(__FILE__).'/../conf.php';
 
 	//Parámetros generales para los 2 casos de listas a extraer
-	$sesion = new Sesion( array('REV','ADM') );
-	$pagina = new Pagina( $sesion );
+	$sesion = new Sesion(array('REV','ADM'));
+	$pagina = new Pagina($sesion);
 
-	#$key = substr(md5(microtime().posix_getpid()), 0, 8);
-	$wb = new Spreadsheet_Excel_Writer();
+	$Criteria = new Criteria($sesion);
+
+	$wb = new WorkbookMiddleware();
 	$wb->setCustomColor ( 35, 220, 255, 220 );
 	$wb->setCustomColor ( 36, 255, 255, 220 );
 	$encabezado =& $wb->addFormat(array('Size' => 12,
@@ -39,30 +40,26 @@
 	                              'Align' => 'center',
 	                              'Border' => 1,
 	                              'Color' => 'black'));
+
 	$ws1 =& $wb->addWorksheet(__('Tipo de Cambio'));
 	$ws1->setInputEncoding('utf-8');
 	$ws1->fitToPages(1,0);
 	$ws1->setZoom(75);
-	#$ws1->protect( $key );
-	if (method_exists('Conf','GetConf'))
-	{
+
+	if (method_exists('Conf','GetConf')) {
 		$PdfLinea1 = Conf::GetConf($sesion, 'PdfLinea1');
 		$PdfLinea2 = Conf::GetConf($sesion, 'PdfLinea2');
-	}
-	else
-	{
+	} else {
 		$PdfLinea1 = Conf::PdfLinea1();
 		$PdfLinea2 = Conf::PdfLinea2();
 	}
 
-	$where = 1;
-
 	$wb->send('Clientes_datos_incompletos.xls');
-	$ws1->setColumn( 1, 1, 18.14);
-	$ws1->setColumn( 2, 2, 45.00);
-	$ws1->setColumn( 3, 3, 40.00);
-	$ws1->setColumn( 4, 4, 75.00);
-	$ws1->setColumn( 5, 5, 20.00);
+	$ws1->setColumn( 1, 1, 18);
+	$ws1->setColumn( 2, 2, 45);
+	$ws1->setColumn( 3, 3, 40);
+	$ws1->setColumn( 4, 4, 75);
+	$ws1->setColumn( 5, 5, 20);
 	$ws1->write(0, 0, 'Listado de Clientes con datos incompletos', $encabezado);
 	$ws1->mergeCells (0, 0, 0, 8);
 	$info_usr1 = str_replace('<br>',' - ',$PdfLinea1);
@@ -79,115 +76,133 @@
 	$ws1->write($fila_inicial, 3, __('Asunto'), $tit);
 	$ws1->write($fila_inicial, 4, __('Campos Faltantes'), $tit);
 
-	$query = "SELECT
-					c.glosa_cliente, c.id_usuario_encargado,
-					ct.codigo_cliente, ct.rut, ct.factura_razon_social, ct.factura_giro, ct.factura_direccion,
-					ct.cod_factura_telefono, ct.factura_telefono, ct.titulo_contacto, ct.contacto as nombre_contacto,
-					ct.apellido_contacto, ct.fono_contacto, ct.email_contacto, ct.direccion_contacto,
-					ct.id_tarifa, ct.id_moneda, ct.forma_cobro, ct.opc_moneda_total, ct.observaciones,
-					GROUP_CONCAT( glosa_asunto ) as asunto
-				FROM cliente as c
-					JOIN contrato as ct ON ( c.codigo_cliente = ct.codigo_cliente )
-					JOIN asunto as a ON ( ct.id_contrato = a.id_contrato )
-				GROUP BY ct.id_contrato;";
+	$Criteria
+		->add_select('c.glosa_cliente')
+		->add_select('c.id_usuario_encargado')
+		->add_select('ct.codigo_cliente')
+		->add_select('ct.rut')
+		->add_select('ct.factura_razon_social')
+		->add_select('ct.factura_giro')
+		->add_select('ct.factura_direccion')
+		->add_select('ct.cod_factura_telefono')
+		->add_select('ct.factura_telefono')
+		->add_select('ct.titulo_contacto')
+		->add_select('ct.contacto', 'nombre_contacto')
+		->add_select('ct.apellido_contacto')
+		->add_select('ct.fono_contacto')
+		->add_select('ct.email_contacto')
+		->add_select('ct.direccion_contacto')
+		->add_select('ct.id_tarifa')
+		->add_select('ct.id_moneda')
+		->add_select('ct.forma_cobro')
+		->add_select('ct.opc_moneda_total')
+		->add_select('ct.observaciones')
+		->add_select('GROUP_CONCAT(glosa_asunto)', 'asunto')
+		->add_from('cliente', 'c')
+		->add_inner_join_with(
+				['contrato', 'ct'],
+				CriteriaRestriction::equals('c.codigo_cliente', 'ct.codigo_cliente')
+			)
+		->add_inner_join_with(
+				['asunto', 'a'],
+				CriteriaRestriction::equals('ct.id_contrato', 'a.id_contrato')
+			)
+		->add_grouping('ct.id_contrato');
 
 	$fila_inicial++;
-	$where = "";
-	if( $fecha_ini )
-	{
-		$fecha_ini = Utiles::fecha2sql($fecha_ini);
-		$where .= ( strlen( $where ) > 0 ? " AND " : " WHERE " );
-		$where .= " DATE_FORMAT(fecha, '%Y-%m-%d') >= '$fecha_ini' ";
-	}
-
-	if( $fecha_fin )
-	{
-		$fecha_fin = Utiles::fecha2sql($fecha_fin);
-		$where .= ( strlen( $where ) > 0 ? " AND " : " WHERE " );
-		$where .= " DATE_FORMAT(fecha, '%Y-%m-%d') >= '$fecha_fin' ";
-	}
-
-	$user_temp = "";
 	$incompletos = 0;
 	$campos = "";
-	$campos_tmp = "";
-	$head = "";
-	$nombre_campos = array(
-		"Nombre", "Attache Secundario", "Código Cliente",
-		"RUC", "Razón Social", "Giro", "Dirección", "Código Teléfono", "Teléfono",
-		"Título", "Nombre", "Apellido", "Teléfono", "E-mail", "Dirección Envío",
-		"Tarifa Horas", "Tarifa en", "Forma de liquidaciones", "Mostrar en", "Detalle de Cobranza",
-		"Asunto"
+	$datos_cliente = array(
+		'glosa_cliente' => 'Nombre',
+		'id_usuario_encargado' => 'Attache Secundario',
+		'codigo_cliente' => 'Código Cliente',
+		'rut' => "RUC",
 	);
-	$resp = mysql_query($query, $sesion->dbh) or Utiles::errorSQL($query,__FILE__,__LINE__,$sesion->dbh);
-	while($row = mysql_fetch_array($resp))
-	{
-		for( $i=0; $i<20; $i++)
-		{
-			if( strlen( $row[$i] ) == 0  )
-			{
-				$campos_tmp .= ( strlen( $campos_tmp ) > 0 ? ", " : "") . $nombre_campos[$i] ;
-			}
-			elseif( $row[$i] == '0' || $row[$i] == '-1' )
-			{
-				$campos_tmp .= ( strlen( $campos_tmp ) > 0 ? ", " : "") . $nombre_campos[$i] ;
-			}
 
-			if( $i >= 3 && $i < 9 )
-			{
-				$head = "Datos Facturacion: ";
-				if( $i == 8 )
-				{
-					$campos .= ( strlen( $campos_tmp ) > 0 ? $head . $campos_tmp . "; \n" : "" );
-					$campos_tmp = "";
-				}
-			}
-			elseif( $i>=9 && $i < 15 )
-			{
-				$head = "Solicitante: ";
-				if( $i == 14 )
-				{
-					$campos .= ( strlen( $campos_tmp ) > 0 ? $head . $campos_tmp . "; \n" : "" );
-					$campos_tmp = "";
-				}
-			}
-			elseif( $i>=15 && $i < 20 )
-			{
-				$head = "Tarificación: ";
-				if( $i == 19 )
-				{
-					$campos .= ( strlen( $campos_tmp ) > 0 ? $head . $campos_tmp . ";" : "" );
-					$campos_tmp = "";
-				}
-			}
-			else
-			{
-				$head = "Datos Cliente: ";
-				if( $i == 2 )
-				{
-					$campos .= ( strlen( $campos_tmp ) > 0 ? $head . $campos_tmp . "; \n" : "" );
-					$campos_tmp = "";
-				}
+	$datos_facturacion = array(
+		'factura_razon_social' => 'Razón Social',
+		'factura_giro' => 'Giro',
+		'factura_direccion' => 'Dirección',
+		'cod_factura_telefono' => 'Código Teléfono',
+		'factura_telefono' => 'Teléfono',
+	);
+
+	$datos_solicitante = array(
+		'titulo_contacto' => 'Título',
+		'nombre_contacto' => 'Nombre',
+		'apellido_contacto' => 'Apellido',
+		'fono_contacto' => 'Teléfono',
+		'email_contacto' => 'E-mail',
+		'direccion_contacto' => 'Dirección Envío',
+	);
+
+	$datos_tarificacion = array(
+		'id_tarifa' => 'Tarifa Horas',
+		'id_moneda' => 'Tarifa en',
+		'forma_cobro' => 'Forma de liquidaciones',
+		'opc_moneda_total' => 'Mostrar en',
+		'observaciones' => 'Detalle de Cobranza',
+		'asunto' => 'Asunto'
+	);
+
+	try{
+		$respuesta = $Criteria->run();
+	} catch(Exception $e) {
+		error_log('Error al ejecutar la SQL');
+	}
+
+	foreach ($respuesta as $key => $row) {
+		$campos_tmp = '';
+		foreach ($datos_cliente as $key => $value) {
+			if (strlen($row[$key]) == 0 || $row[$key] == '0' || $row[$key] == '-1') {
+				$campos_tmp .= (strlen($campos_tmp) > 0 ? ', ' : '') . $value;
 			}
 		}
 
-		if( strlen( $campos ) > 0 )
-		{
-			$ws1->write($fila_inicial, 1, $row["codigo_cliente"], $f5);
-			$ws1->write($fila_inicial, 2, $row["glosa_cliente"], $f4);
-			$ws1->write($fila_inicial, 3, $row["asunto"], $f4);
+		$campos .= strlen($campos_tmp) > 0 ? 'Datos Cliente: ' . $campos_tmp . "\n" : '';
+		$campos_tmp = '';
+
+		foreach ($datos_facturacion as $key => $value) {
+			if (strlen($row[$key]) == 0 || $row[$key] == '0' || $row[$key] == '-1') {
+				$campos_tmp .= (strlen($campos_tmp) > 0 ? ', ' : '') . $value;
+			}
+		}
+
+		$campos .= strlen($campos_tmp) > 0 ? 'Datos Facturacion: ' . $campos_tmp . "\n" : '';
+		$campos_tmp = '';
+
+		foreach ($datos_solicitante as $key => $value) {
+			if (strlen($row[$key]) == 0 || $row[$key] == '0' || $row[$key] == '-1') {
+				$campos_tmp .= (strlen($campos_tmp) > 0 ? ', ' : '') . $value;
+			}
+		}
+
+		$campos .= strlen($campos_tmp) > 0 ? 'Solicitante: ' . $campos_tmp . "\n" : '';
+		$campos_tmp = '';
+
+		foreach ($datos_tarificacion as $key => $value) {
+			if (strlen($row[$key]) == 0 || $row[$key] == '0' || $row[$key] == '-1') {
+				$campos_tmp .= (strlen($campos_tmp) > 0 ? ', ' : '') . $value;
+			}
+		}
+
+		$campos .= strlen($campos_tmp) > 0 ? 'Tarificación: ' . $campos_tmp : '';
+		$campos_tmp = '';
+
+		if (strlen($campos) > 0) {
+			$ws1->write($fila_inicial, 1, $row['codigo_cliente'], $f5);
+			$ws1->write($fila_inicial, 2, $row['glosa_cliente'], $f4);
+			$ws1->write($fila_inicial, 3, $row['asunto'], $f4);
 			$ws1->write($fila_inicial, 4, $campos, $f4);
 			$incompletos++;
 			$fila_inicial++;
-			$campos = "";
+			$campos = '';
 		}
 	}
 
-	if( $incompletos == 0)
-	{
-		$ws1->write($fila_inicial, 1, "No se encontraron clientes con datos incompletos", $f5);
+	if ($incompletos == 0) {
+		$ws1->write($fila_inicial, 1, 'No se encontraron clientes con datos incompletos', $f5);
 	}
 
   $wb->close();
   exit;
-?>
