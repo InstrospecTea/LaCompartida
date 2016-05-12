b7413c7425c9f586c32129836aa387bd2da43ef4
Sebastián González <brutalchrist@gmail.com>
2016-05-12 13:50:14 -0300

    PrmExcelCobro sin conflictos

--- app/classes/PrmExcelCobro.php
+++ app/classes/PrmExcelCobro.php
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-<?php
-require_once dirname(__FILE__).'/../conf.php';
-require_once Conf::ServerDir().'/../fw/classes/Lista.php';
-require_once Conf::ServerDir().'/../fw/classes/Objeto.php';
-require_once Conf::ServerDir().'/../app/classes/Debug.php';
-
-class PrmExcelCobro extends Objeto
-{
-	function PrmExcelCobro($sesion, $fields = "", $params = "")
-	{
-		$this->tabla = "prm_excel_cobro";
-		$this->campo_id = "id_prm_excel_cobro";
-		$this->sesion = $sesion;
-		$this->fields = $fields;
-		$this->guardar_fecha = false;
-	}
-}
-
-class ListaPrmExcelCobro extends Lista
-{
-    function ListaPrmExcelCobro($sesion, $params, $query)
-    {
-        $this->Lista($sesion, 'PrmExcelCobro', $params, $query);
-    }
-}
-=======
 <?php
 
 class PrmExcelCobro extends Objeto {
@@ -95,4 +68,3 @@ class PrmExcelCobro extends Objeto {
 	}
 
 }
->>>>>>> develop
