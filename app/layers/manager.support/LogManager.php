611fe7a050948c25e2c92ace832c08a3711df88c
Sebastián González <brutalchrist@gmail.com>
2016-05-23 16:18:35 -0300

    Se añade capa Manager de LogDatabase. TTBC-4057

--- app/layers/manager.support/LogManager.php
+++ app/layers/manager.support/LogManager.php
@@ -2,4 +2,15 @@
 
 class LogManager extends AbstractManager implements ILogManager {
 
+	public function getLogs($table_title, $id_field) {
+		$this->loadService('LogDatabase');
+
+		$restrictions = CriteriaRestriction::and_clause(
+			CriteriaRestriction::equals('titulo_tabla', "'{$table_title}'"),
+			CriteriaRestriction::equals('id_field', $id_field)
+		);
+
+		// Antes de retornar se debe humanizar.
+		return $this->LogDatabaseService->findAll($restrictions, null, array('log_db.fecha DESC'));
+	}
 }
--- app/layers/manager/ILogManager.php
+++ app/layers/manager/ILogManager.php
@@ -1,5 +1,11 @@
 <?php
 
 interface ILogManager extends BaseManager {
-
+	/**
+	 * Obtiene la bitácora de una tabla en cierto movimiento
+	 * @param 	string $table_title
+	 * @param 	string $id_field
+	 * @return 	Array humanizado
+	 */
+	public function getLogs($table_title, $id_field)
 }
