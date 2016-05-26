9f8ff3a1b5fccdab46917925fcc4cbc71f64ef1d
cps <cuayocps@gmail.com>
2016-05-26 18:53:14 -0400

    Premite crear criterias de actualización. Cambia los nombres de los métodos, manteniento retrocompatibilidad.

--- app/classes/InsertCriteria.php
+++ app/classes/InsertCriteria.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
-*
-*/
-class InsertCriteria
-{
+ *
+ */
+class InsertCriteria {
+
 	/**
 	 * Permite generar una conexión con el medio persistente.
 	 * @var [type]
@@ -12,14 +12,17 @@ class InsertCriteria
 	private $sesion;
 
 	/*
-		CRITERIA QUERY BUILDER PARAMS.
+	  CRITERIA QUERY BUILDER PARAMS.
 	 */
-	private $insert = 'INSERT INTO ';
+	private $action = 'INSERT INTO';
 	private $table_name = '';
 	private $select_criteria = null;
+	private $is_update = false;
+	private $restrictions = array();
+	private $forced = false;
 
 	/*
-		CRITERIA SCOPE ENVELOPERS.
+	  CRITERIA SCOPE ENVELOPERS.
 	 */
 	private $insert_value_clause = array();
 
@@ -33,21 +36,29 @@ class InsertCriteria
 
 	/**
 	 * Ejecuta una query en base a PDO, considerando los criterios definidos en este Criteria.
-	 * @return Array asociativo de resultados.
+	 * @param type $forced Fuerza la ejecución de UPDATE si no tiene restricciones
+	 * @throws Exception
 	 */
-	public function run() {
+	public function run($forced = false) {
 		if ($this->sesion == null) {
 			throw new Exception('Criteria dice: No hay una sesión definida para Criteria, no es posible ejecutar.');
 		}
+		$this->forced = $forced;
 		$statement = $this->sesion->pdodbh->prepare($this->get_plain_query());
 		$statement->execute();
 	}
 
-	public function add_pivot_with_value($column_key, $value, $default = false) {
-		if (array_key_exists($column_key, $this->insert_value_clause)){
-			throw new Exception("Criteria dice: Usted está sobrescribiendo un valor para la columna $column_key que ya está definido");
+	public function update() {
+		$this->action = 'UPDATE';
+		$this->is_update = true;
+		return $this;
+	}
+
+	public function addPivotWithValue($column_key, $value, $default = false) {
+		if (array_key_exists($column_key, $this->insert_value_clause)) {
+			throw new Exception("Criteria dice: Usted está sobrescribiendo un valor para la columna '$column_key' que ya está definido");
 		} else {
-			if (is_null($value) || $value == 'NULL' ) {
+			if (is_null($value) || $value == 'NULL') {
 				$this->insert_value_clause[$column_key] = 'NULL';
 			} else if ($default) {
 				$this->insert_value_clause[$column_key] = $value;
@@ -58,48 +69,124 @@ class InsertCriteria
 		}
 	}
 
-	public function add_pivot($column_key) {
+	public function addPivot($column_key) {
 		if (array_key_exists($column_key, $this->insert_value_clause)) {
-			throw new Exception("Criteria dice: Usted está declarando nuevamente la columna $column_key");
+			throw new Exception("Criteria dice: Usted está declarando nuevamente la columna '$column_key'");
 		} else {
 			$this->insert_value_clause[$column_key] = '';
 			return $this;
 		}
 	}
 
-	public function set_into($table) {
+	public function addRestriction(CriteriaRestriction $restriction) {
+		$this->restrictions[] = $restriction;
+		return $this;
+	}
+
+	public function setTable($table) {
 		if (is_string($table)) {
 			$this->table_name = $table;
 			return $this;
 		} else {
-			throw new Exception("Criteria dice: El nombre de la tabla en la que se persistirán los registros debe ser String");
+			throw new Exception('Criteria dice: El nombre de la tabla en la que se persistirán los registros debe ser String');
 		}
 	}
 
-	public function values_criteria(Criteria $criteria){
+	public function valuesCriteria(Criteria $criteria) {
 		$this->select_criteria = $criteria;
 		return $this;
 	}
 
-	private function get_pivotes_fragment() {
+	public function getPlainQuery() {
+		if (empty($this->table_name)) {
+			throw new Exception('Criteria dice: No ha especificado una tabla para la consulta.');
+		}
+		if ($this->is_update) {
+			return $this->updatePlainQuery();
+		}
+		return $this->action . ' ' . $this->table_name . ' '
+			. $this->getPivotesFragment() . ' VALUES ' . $this->getValuesFragment();
+	}
+
+	private function getPivotesFragment() {
 		$pivotes = array_keys($this->insert_value_clause);
 
-		return '('.implode(',',$pivotes).')';
-
+		return '(' . implode(',', $pivotes) . ')';
 	}
 
-	private function get_values_fragment() {
+	private function getValuesFragment() {
 		$pivotes = array_values($this->insert_value_clause);
 
-		return '('.implode(',',$pivotes).')';
+		return '(' . implode(',', $pivotes) . ')';
 	}
 
-	public function get_plain_query() {
-		return $this->insert . $this->table_name . ' ' . $this->get_pivotes_fragment() . ' VALUES ' . $this->get_values_fragment();
+	private function updatePlainQuery() {
+		$plain_query = $this->action . ' ' . $this->table_name . ' SET '
+			. $this->getSets() . ' ' . $this->getRestrictions();
+		return $plain_query;
 
 	}
 
-	public function get_plain_query_from_criteria(){
+	private function getSets() {
+		$pivotes = $this->insert_value_clause;
+		array_walk($pivotes, function(&$value, $key) {
+			$value = "{$key} = $value";
+		});
+		return implode(', ', $pivotes);
+	}
+
+	private function getRestrictions() {
+		if (empty($this->restrictions)) {
+			if ($this->forced) {
+				return '';
+			}
+			throw new Exception("Criteria dice: No se puede actualizar sin restricciones");
+		}
+		return 'WHERE ' . CriteriaRestriction::and_clause($this->restrictions);
+	}
+
+	public function get_plain_query_from_criteria() {
 		throw new Exception("No implementado aún.");
 	}
+
+	/**
+	 * @param Criteria $criteria
+	 * @return type
+	 * @deprecated use valuesCriteria()
+	 */
+	public function values_criteria(Criteria $criteria) {
+		return $this->valuesCriteria($criteria);
+	}
+	/**
+	 * @param type $column_key
+	 * @param type $value
+	 * @param type $default
+	 * @return type
+	 * @deprecated use addPivotWithValue()
+	 */
+	public function add_pivot_with_value($column_key, $value, $default = false) {
+		return $this->addPivotWithValue($column_key, $value, $default);
+	}
+	/**
+	 * @param type $column_key
+	 * @deprecated use addPivot()
+	 */
+	public function add_pivot($column_key) {
+	$this->addPivot($column_key);
+	}
+	/**
+	 * @param type $table
+	 * @return type
+	 * @deprecated use setTable()
+	 */
+	public function set_into($table) {
+		return $this->setTable($table);
+	}
+	/**
+	 * @deprecated use getPlainQuery()
+	 */
+	public function get_plain_query() {
+		return $this->getPlainQuery();
+	}
+
 }
