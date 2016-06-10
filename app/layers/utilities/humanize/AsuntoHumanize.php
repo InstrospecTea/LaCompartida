5b2f6f5e9adf486426c4f846c87fbc5c714b46d1
brutalchrist <brutalchrist@gmail.com>
2016-06-10 16:37:55 -0400

    Espacios shuer locos

--- app/layers/utilities/humanize/AsuntoHumanize.php
+++ app/layers/utilities/humanize/AsuntoHumanize.php
@@ -2,27 +2,27 @@
 
 abstract Class AsuntoHumanize {
 	public static $rules = array(
-		'id_asunto' =>  'literalMessage',
-		'id_tipo_asunto' =>  'valueForValue',
-		'id_contrato_indep' =>  'contractToggle',
-		'id_area_proyecto' =>  'valueForValue',
-		'id_encargado' =>  'valueForValue',
-		'id_idioma' =>  'valueForValue',
-		'codigo_asunto' =>  'valueForValue',
-		'codigo_cliente' =>  'valueForValue',
-		'glosa_asunto' =>  'valueForValue',
-		'descripcion_asunto' =>  'valueForValue',
-		'codigo_asunto_secundario' =>  'valueForValue',
-		'cobrable' =>  'chargeable',
-		'activo' =>  'activeInactive',
+		'id_asunto' => 'literalMessage',
+		'id_tipo_asunto' => 'valueForValue',
+		'id_contrato_indep' => 'contractToggle',
+		'id_area_proyecto' => 'valueForValue',
+		'id_encargado' => 'valueForValue',
+		'id_idioma' => 'valueForValue',
+		'codigo_asunto' => 'valueForValue',
+		'codigo_cliente' => 'valueForValue',
+		'glosa_asunto' => 'valueForValue',
+		'descripcion_asunto' => 'valueForValue',
+		'codigo_asunto_secundario' => 'valueForValue',
+		'cobrable' => 'chargeable',
+		'activo' => 'activeInactive',
 		'actividades_obligatorias' => 'mandatoryActivities',
-		'contraparte' =>  'valueForValue',
-		'cotizado_con' =>  'valueForValue',
-		'contacto' =>  'valueForValue',
-		'email_contacto' =>  'valueForValue',
-		'fono_contacto' =>  'valueForValue',
-		'limite_hh' =>  'valueForValue',
-		'limite_monto' =>  'valueForValue'
+		'contraparte' => 'valueForValue',
+		'cotizado_con' => 'valueForValue',
+		'contacto' => 'valueForValue',
+		'email_contacto' => 'valueForValue',
+		'fono_contacto' => 'valueForValue',
+		'limite_hh' => 'valueForValue',
+		'limite_monto' => 'valueForValue'
 	);
 
 	public static $relations = array(
@@ -50,24 +50,24 @@ abstract Class AsuntoHumanize {
 	);
 
 	public static $dictionary = array(
-		'id_contrato' =>  'el identificador del Contrato',
-		'id_area_proyecto' =>  'el área del proyecto',
-		'id_contrato_indep' =>  'el indentificador del Contrato independiente',
-		'id_encargado' =>  'el encargado',
-		'id_idioma' =>  'el idioma',
-		'id_tipo_asunto' =>  'el tipo de Asunto',
-		'codigo_asunto' =>  'el código',
-		'codigo_cliente' =>  'el cliente',
-		'glosa_asunto' =>  'la glosa',
-		'descripcion_asunto' =>  'la descripción',
-		'codigo_asunto_secundario' =>  'el código secundario',
-		'contraparte' =>  'la contraparte',
-		'cotizado_con' =>  'el cotizador conjunto',
-		'contacto' =>  'el contacto',
-		'email_contacto' =>  'el email de contacto',
-		'fono_contacto' =>  'el fono de contacto',
-		'limite_hh' =>  'el límite de horas',
-		'limite_monto' =>  'el límite del monto'
+		'id_contrato' => 'el identificador del Contrato',
+		'id_area_proyecto' => 'el área del proyecto',
+		'id_contrato_indep' => 'el indentificador del Contrato independiente',
+		'id_encargado' => 'el encargado',
+		'id_idioma' => 'el idioma',
+		'id_tipo_asunto' => 'el tipo de Asunto',
+		'codigo_asunto' => 'el código',
+		'codigo_cliente' => 'el cliente',
+		'glosa_asunto' => 'la glosa',
+		'descripcion_asunto' => 'la descripción',
+		'codigo_asunto_secundario' => 'el código secundario',
+		'contraparte' => 'la contraparte',
+		'cotizado_con' => 'el cotizador conjunto',
+		'contacto' => 'el contacto',
+		'email_contacto' => 'el email de contacto',
+		'fono_contacto' => 'el fono de contacto',
+		'limite_hh' => 'el límite de horas',
+		'limite_monto' => 'el límite del monto'
 	);
 
 	public static $black_list = array(
