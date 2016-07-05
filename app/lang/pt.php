b898265bbee2a5b9014c19c321a9139be1008075
brutalchrist <brutalchrist@gmail.com>
2016-07-05 12:24:04 -0400

    Se genera descripción del pago. TTBC-4118

--- app/lang/pt.php
+++ app/lang/pt.php
@@ -1183,3 +1183,4 @@ $_LANG['Se produjo un error generando el documento, por favor intente nuevamente
 $_LANG['No existen movimientos para este Asunto'] = 'Não existem movimentos para esse assunto';
 $_LANG['Ha ocurrido un error al consultar el historial. Por favor inicie sesión nuevamente.'] = 'Ocorreu um erro ao consultar o histórico. Por favor inicie a sessão novamente.';
 $_LANG['Profesionales sin trabajos'] = 'Profissionais sem trabalhos';
+$_LANG['Pago de'] = 'Pagamento de';
