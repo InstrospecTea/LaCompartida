ff8e78828df9fa5260c6813bc901e58f37ce87e1
Nicolás Torres <ntorresj@gmail.com>
2016-07-15 13:11:00 -0400

    lang inglés para careyallende

---
+++ app/lang/en_careyallende.php
@@ -0,0 +1,10 @@
+<?php
+require_once dirname(__FILE__) . '/en.php';
+
+$_LANG['Nombre'] = "Biller";
+$_LANG['Tarifa'] = "Fee";
+$_LANG['Hr. Trabajadas'] = "Hours Worked";
+$_LANG['Hrs. Trabajadas'] = "Hours Worked";
+$_LANG['Hrs. Rebajadas'] = "Discounted Hours";
+$_LANG['Hrs. No Tarificadas'] = "Hours not Billed";
+$_LANG['Hrs. Tarificadas'] = "Billed Hours";
