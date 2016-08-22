12df2c358f03070b56fd85badae851bb5bac0818
brionescl <brionescl@gmail.com>
2016-08-22 13:01:01 -0300

    corregir carga de helpers

--- app/layers/utilities/ViewRenderer.php
+++ app/layers/utilities/ViewRenderer.php
@@ -88,17 +88,9 @@ class ViewRenderer {
 	}
 
 	protected function loadHelpers() {
-		if (!class_exists('Helper')) {
-			$fileHelper = LAYER_PATH . $this->helpersPath . "/helpers/Helper.php";
-			require_once $fileHelper;
-		}
 		foreach ($this->helpers as $helper) {
 			$class_name = is_array($helper) ? $helper[0] : $helper;
 			$alias = is_array($helper) ? $helper[1] : null;
-			$file = LAYER_PATH . $this->helpersPath . "/helpers/{$class_name}.php";
-			if (is_readable($file)) {
-				require_once $file;
-			}
 			$this->loadModel($class_name, $alias);
 		}
 	}
