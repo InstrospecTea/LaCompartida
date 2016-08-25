8a84e4d1b20650fa4c7ff64b879d0931e847c9b8
brutalchrist <brutalchrist@gmail.com>
2016-08-25 13:56:40 -0300

    Se agrega patron de color hover a los graficos de torta

--- app/classes/Graficos/DatasetPie.php
+++ app/classes/Graficos/DatasetPie.php
@@ -7,9 +7,13 @@ class DatasetPie extends Dataset {
 	public function setData(array $data) {
 		$background_color = [];
 		foreach ($data as $d) {
-			$background_color[] = $this->getRandomColor();
+			$color = $this->getRandomColor(0.8);
+			$background_color[] = $color;
+			$color = str_replace('0.80', '0.50', $color);
+			$hover_background_color[] = $color;
 		}
 		$this->backgroundColor = $background_color;
+		$this->hoverBackgroundColor = $hover_background_color;
 		return parent::setData($data);
 	}
 }
