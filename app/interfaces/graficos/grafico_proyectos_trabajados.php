b69e438660f3af137f4488494649ff1c9e1c3098
Claudio Peralta <cuayocps@gmail.com>
2016-08-26 10:42:18 -0300

    implementa chart 2.0

--- app/interfaces/graficos/grafico_proyectos_trabajados.php
+++ app/interfaces/graficos/grafico_proyectos_trabajados.php
@@ -9,37 +9,73 @@
 		->add_select('SUM(TIME_TO_SEC(duracion))/3600', 'tiempo')
 		->add_from('asunto')
 		->add_left_join_with('trabajo',
-				CriteriaRestriction::equals('trabajo.codigo_asunto', 'asunto.codigo_asunto')
-			)
+			CriteriaRestriction::equals('trabajo.codigo_asunto', 'asunto.codigo_asunto')
+		)
 		->add_restriction(
-				CriteriaRestriction::equals('trabajo.id_usuario', "'$id_usuario'")
-			)
+			CriteriaRestriction::equals('trabajo.id_usuario', "'$id_usuario'")
+		)
 		->add_restriction(
-				CriteriaRestriction::between('fecha', "'" . Utiles::fecha2sql($fecha1) . "'", "'" . Utiles::fecha2sql($fecha2) . "'")
-			)
+			CriteriaRestriction::between('fecha', "'" . Utiles::fecha2sql($fecha1) . "'", "'" . Utiles::fecha2sql($fecha2) . "'")
+		)
 		->add_grouping('asunto.codigo_asunto')
 		->add_ordering('tiempo', 'DESC')
 		->add_limit(14, 0);
 
-	try{
+	try {
 		$respuesta = $Criteria->run();
 	} catch(Exception $e) {
 		error_log('Error al ejecutar la SQL');
 	}
 
 	foreach ($respuesta as $i => $fila) {
-		$asunto[] = $fila['glosa_asunto'];
-		$tiempo[] = $fila['tiempo'];
+		$user_data[$fila['glosa_asunto']] = $fila['tiempo'];
 	}
 
-	$grafico = new TTB\Graficos\GraficoBarra();
-	$dataset = new TTB\Graficos\GraficoDataset();
+	$grafico = new TTB\Graficos\Grafico();
+	$dataset = new TTB\Graficos\Dataset();
 
-	$dataset->addLabel(__('Horas trabajadas'))
-		->addData($tiempo);
+	$options = [
+		'responsive' => true,
+		'tooltips' => [
+			'mode' => 'label'
+		],
+		'scales' => [
+			'xAxes' => [[
+				'display' => true,
+				'gridLines' => [
+					'display' => false
+				],
+				'labels' => [
+					'show' => true,
+				]
+			]],
+			'yAxes' => [
+				[
+					'type' => 'linear',
+					'display' => true,
+					'position' => 'left',
+					'id' => 'y-axis-1',
+					'gridLines' => [
+						'display' => false
+					],
+					'labels' => [
+						'show' => true
+					],
+					'ticks' => [
+						'beginAtZero' => true
+					]
+				]
+			]
+		]
+	];
 
-	$grafico->addDataSets($dataset)
-		->addNameChart('Horas trabajadas' . (!empty($nombre_usuario) ? ' - ' . $nombre_usuario : ''))
-		->addLabels($asunto);
+	$dataset->setYAxisID('y-axis-1')
+		->setLabel(__('Horas trabajadas'))
+		->setData(array_values($user_data));
+
+	$grafico->setNameChart('Horas trabajadas' . (!empty($nombre_usuario) ? ' - ' . $nombre_usuario : ''))
+		->addDataset($dataset)
+		->setOptions($options)
+		->addLabels(array_keys($user_data));
 
 	echo $grafico->getJson();
