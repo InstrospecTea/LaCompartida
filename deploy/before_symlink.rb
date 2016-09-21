65ba88da260a262b04b5de417819a30776112968
brutalchrist <brutalchrist@gmail.com>
2016-09-21 14:51:45 -0300

    V por W

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,5 @@
 Chef::Log.info("Running deploy/before_symlink.rb...")
 Chef::Log.info("Release path: #{release_path}")
 
-composer_command="/usr/bin/php56 /srw/www/thetimebilling/shared/composer.phar install --no-dev- --no-interaction"
+composer_command="/usr/bin/php56 /srv/www/thetimebilling/shared/composer.phar install --no-dev- --no-interaction"
 run "cd #{release_path} && #{composer_command}"
