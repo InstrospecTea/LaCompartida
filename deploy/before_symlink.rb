87cd781c634f1e175f1904de4c151d94272ceb83
Abraham (El Dude) Barrera <abarrerac@gmail.com>
2016-09-21 16:14:59 -0300

    terceras cositas

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -4,8 +4,8 @@ Chef::Log.info("Release path: #{release_path}")
 composer_command="/usr/bin/php56 /usr/local/bin/composer.phar install --no-dev --no-interaction"
 run "cd #{release_path} && #{composer_command}"
 
-bower_command="/usr/local/bin/bower install"
+bower_command="/usr/local/bin/bower --allow-root install"
 run "cd #{release_path} && #{bower_command}"
 
-bower_update="/usr/local/bin/bower update"
+bower_update="/usr/local/bin/bower --allow-root update"
 run "cd #{release_path} && #{bower_update}"
