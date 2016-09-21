87c0b68f05975cc4d331b9f8af4e2925c48aa27d
brionescl <brionescl@gmail.com>
2016-09-21 16:41:19 -0300

    eliminar método

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,7 +1,2 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 Chef::Log.info("Release path: #{release_path}")
-
-execute "do the bartman" do
-    command "touch /tmp/12345666"
-    user "root"
-end
