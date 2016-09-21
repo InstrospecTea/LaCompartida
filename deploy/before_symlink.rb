ec9ea1024d1aef8218d184f3dcaf429cfcffc1fa
Abraham (El Dude) Barrera <abarrerac@gmail.com>
2016-09-21 15:55:01 -0300

    Cositas

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,11 @@
 Chef::Log.info("Running deploy/before_symlink.rb...")
 Chef::Log.info("Release path: #{release_path}")
 
-composer_command="/usr/bin/php56 /srv/www/thetimebilling/shared/composer.phar install --no-dev --no-interaction"
+composer_command="/usr/bin/php56 #{release_path}/composer.phar install --no-dev --no-interaction"
 run "cd #{release_path} && #{composer_command}"
+
+bower_command="/usr/local/bin/bower install"
+run "cd #{release_path} && #{bower_command}"
+
+bower_update="/usr/local/bin/bower update"
+run "cd #{release_path} && #{bower_update}"
