f287251e09e40031293be2558d0ca6b66e3b74ba
vladzur <vladzur@gmail.com>
2016-09-27 15:25:50 -0300

    Deshacer commit anterior (patch)

--- app/interfaces/graficos/grafico_resumen_actividades.php
+++ app/interfaces/graficos/grafico_resumen_actividades.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__) . '/../../conf.php';
 
 $Sesion = new Sesion();
 
-$data = json_decode(base64_decode($datos), true);
+$data = UtilesApp::utf8izar(json_decode(base64_decode($datos), true), false);
 $datos = array_combine($data['nombres'], $data['tiempo']);
 
 function dort_desc($a, $b) {
