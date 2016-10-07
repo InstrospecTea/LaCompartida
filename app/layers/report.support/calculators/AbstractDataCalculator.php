7b8b5ed9d449550e3732115a7604d4ef9c86d5d7
vladzur <vladzur@gmail.com>
2016-10-07 17:44:48 -0300

    Simplificar lógica

--- app/layers/report.support/calculators/AbstractDataCalculator.php
+++ app/layers/report.support/calculators/AbstractDataCalculator.php
@@ -496,12 +496,8 @@ abstract class AbstractDataCalculator implements IDataCalculator {
 	 * Factor to apply to heach sum element
 	 * @return factor
 	 */
- 	public function getFactor() {
-		$factor = 1;
-		if (Conf::getConf($this->Session, 'NuevoModuloFactura')) {
-			$factor = $this->invoiceFactor();
-		}
- 		return $factor;
+ 	public function getFactor() {	
+ 		return Conf::getConf($this->Session, 'NuevoModuloFactura') ? $this->invoiceFactor() : 1;
  	}
 
 	/**
