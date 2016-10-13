f9b16b7058983ad495f163a083a28bfcdb5ab413
Claudio Peralta <cuayocps@gmail.com>
2016-10-13 16:58:11 -0300

    elimina lineas de debug

--- app/layers/report.support/calculators/AbstractDataCalculator.php
+++ app/layers/report.support/calculators/AbstractDataCalculator.php
@@ -146,17 +146,14 @@ abstract class AbstractDataCalculator implements IDataCalculator {
 		$results = array();
 
 		if (!empty($this->WorksCriteria)) {
-			// Debug::pr($this->WorksCriteria->get_plain_query());
 			$results = array_merge($results, $this->WorksCriteria->run());
 		}
 
 		if (!empty($this->ErrandsCriteria)) {
-			// Debug::pr($this->ErrandsCriteria->get_plain_query());
 			$results = array_merge($results, $this->ErrandsCriteria->run());
 		}
 
 		if (!empty($this->ChargesCriteria)) {
-			// Debug::pr($this->ChargesCriteria->get_plain_query());
 			$results = array_merge($results, $this->ChargesCriteria->run());
 		}
 		return $results;
@@ -496,7 +493,7 @@ abstract class AbstractDataCalculator implements IDataCalculator {
 	 * Factor to apply to heach sum element
 	 * @return factor
 	 */
- 	public function getFactor() {	
+ 	public function getFactor() {
  		return Conf::getConf($this->Session, 'NuevoModuloFactura') ? $this->invoiceFactor() : 1;
  	}
 
