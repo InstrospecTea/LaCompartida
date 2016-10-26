9825eaa93069a698deb2a5be390599608b86068f
gwurman <gwurman@lemontech.cl>
2016-10-26 17:15:41 -0300

    Refactor some SQL queries using Criteria

--- app/classes/Documento.php
+++ app/classes/Documento.php
@@ -467,28 +467,45 @@ class Documento extends Objeto {
 	}
 
 	function ListaPagos() {
+		$Criteria = new Criteria($this->sesion);
 		$out = '';
+		$pagos;
 		if (Conf::read('NuevoModuloFactura')) {
 			// Todos los neteos en donde 'this' figura como documento_cobro
-			$query = "SELECT n.id_documento_pago AS id, n.valor_cobro_honorarios as honorarios, n.valor_cobro_gastos as gastos, d.pago_retencion, d.es_adelanto
-								FROM neteo_documento AS n
-								JOIN documento AS d ON d.id_documento = n.id_documento_pago
-								WHERE n.id_documento_cobro ='" . $this->fields['id_documento'] . "'";
+			$pagos = $Criteria
+				->add_select('neteo_documento.id_documento_pago', 'id')
+				->add_select('neteo_documento.valor_cobro_honorarios', 'honorarios')
+				->add_select('neteo_documento.valor_cobro_gastos', 'gastos')
+				->add_select('documento.pago_retencion')
+				->add_select('documento.es_adelanto')
+				->add_from('neteo_documento')
+				->add_custom_join_with('documento', 'documento.id_documento = neteo_documento.id_documento_pago', '')
+				->add_restriction(CriteriaRestriction::equals('neteo_documento.id_documento_cobro', $this->fields['id_documento']))
+				->run();
 		} else {
-			// Todos los documentos asociados al cobro de 'this', sin neteo
-			$query = "SELECT d.id_documento AS id, d.honorarios, d.subtotal_gastos AS gastos, d.pago_retencion, d.es_adelanto
-								FROM documento AS d
-								LEFT JOIN neteo_documento AS n ON d.id_documento = n.id_documento_pago
-								WHERE  d.tipo_doc != 'N'
-								AND n.id_neteo_documento IS NULL
-								AND d.id_cobro ='" . $this->fields['id_cobro'] . "'";
+			// Todos los documentos sin neteo asociados al cobro de 'this',
+			$pagos = $Criteria
+				->add_select('documento.id_documento', 'id')
+				->add_select('documento.honorarios')
+				->add_select('documento.subtotal_gastos', 'gastos')
+				->add_select('documento.pago_retencion')
+				->add_select('documento.es_adelanto')
+				->add_from('documento')
+				->add_left_join_with('neteo_documento', 'documento.id_documento = neteo_documento.id_documento_pago')
+				->add_restriction(
+					CriteriaRestriction::and_clause(
+						CriteriaRestriction::not_equal('documento.tipo_doc', '"N"'),
+						CriteriaRestriction::is_null('neteo_documento.id_neteo_documento'),
+						CriteriaRestriction::equals('documento.id_cobro', $this->fields['id_cobro'])
+					)
+				)
+				->run();
 		}
-
-		$resp = mysql_query($query, $this->sesion->dbh) or Utiles::errorSQL($query, __FILE__, __LINE__, $this->sesion->dbh);
 
 		$Form = new Form();
 		$Html = &$Form->Html;
-		while (list($id, $honorarios, $gastos, $pago_retencion, $es_adelanto) = mysql_fetch_array($resp)) {
+		foreach ($pagos as &$pago) {
+			list($id, $honorarios, $gastos, $pago_retencion, $es_adelanto) = array_values($pago);
 			if (!$id) {
 				continue;
 			}
