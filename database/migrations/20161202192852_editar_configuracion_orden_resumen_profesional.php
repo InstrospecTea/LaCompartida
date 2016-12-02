02789d9e4d28fa7548cea2a55fea9d878666fbc3
gwurman <gwurman@lemontech.cl>
2016-12-02 16:51:23 -0300

    BUGFIX: Migraciones

--- database/migrations/20161202192852_editar_configuracion_orden_resumen_profesional.php
+++ database/migrations/20161202192852_editar_configuracion_orden_resumen_profesional.php
@@ -9,12 +9,9 @@ class EditarConfiguracionOrdenResumenProfesional extends \Database\Migration imp
    * @return void
    */
 	function up() {
-		$this->addQueryUp("UPDATE `configuracion`
-		 SET `valor_opcion` = 'orden_categoria ASC, tarifa DESC', WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='prm_categoria_usuario.orden ASC, trabajo.tarifa_hh DESC'");
-		$this->addQueryUp("UPDATE `configuracion`
-		 SET `valor_opcion` = 'id_categoria_usuario ASC, id_usuario ASC', WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='usuario.id_categoria_usuario ASC, usuario.id_usuario ASC'");
-		$this->addQueryUp("UPDATE `configuracion`
-		 SET `valor_opcion` = 'fecha ASC, descripcion ASC', WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='trabajo.fecha ASC, trabajo.descripcion'");
+		$this->addQueryUp("UPDATE `configuracion` SET `valor_opcion` = 'orden_categoria ASC, tarifa DESC' WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='prm_categoria_usuario.orden ASC, trabajo.tarifa_hh DESC'");
+		$this->addQueryUp("UPDATE `configuracion` SET `valor_opcion` = 'id_categoria_usuario ASC, id_usuario ASC' WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='usuario.id_categoria_usuario ASC, usuario.id_usuario ASC'");
+		$this->addQueryUp("UPDATE `configuracion` SET `valor_opcion` = 'fecha ASC, descripcion ASC' WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='trabajo.fecha ASC, trabajo.descripcion'");
 	}
 
 	/**
@@ -22,11 +19,8 @@ class EditarConfiguracionOrdenResumenProfesional extends \Database\Migration imp
    * @return void
    */
 	function down() {
-		$this->addQuerydDown("UPDATE `configuracion`
-		 SET `valor_opcion` = 'prm_categoria_usuario.orden ASC, trabajo.tarifa_hh DESC', WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='orden_categoria ASC, tarifa DESC'");
-		$this->addQueryDown("UPDATE `configuracion`
-		 SET `valor_opcion` = 'usuario.id_categoria_usuario ASC, usuario.id_usuario ASC', WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='id_categoria_usuario ASC, id_usuario ASC'");
-		$this->addQueryDown("UPDATE `configuracion`
-		 SET `valor_opcion` = 'trabajo.fecha ASC, trabajo.descripcion', WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='fecha ASC, descripcion ASC'");
+		$this->addQuerydDown("UPDATE `configuracion` SET `valor_opcion` = 'prm_categoria_usuario.orden ASC, trabajo.tarifa_hh DESC' WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='orden_categoria ASC, tarifa DESC'");
+		$this->addQueryDown("UPDATE `configuracion` SET `valor_opcion` = 'usuario.id_categoria_usuario ASC, usuario.id_usuario ASC' WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='id_categoria_usuario ASC, id_usuario ASC'");
+		$this->addQueryDown("UPDATE `configuracion` SET `valor_opcion` = 'trabajo.fecha ASC, trabajo.descripcion' WHERE glosa_opcion='OrdenResumenProfesional' AND valor_opcion='fecha ASC, descripcion ASC'");
 	}
 }
